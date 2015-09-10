4e081f76a7a6d06df9c81642daa6fef87ea262a1
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2015-09-10 13:16:44 +0000

    [doc] format: again longer example

--- antinet/src/antinet_sim/style_tab.cpp
+++ antinet/src/antinet_sim/style_tab.cpp
@@ -1,33 +1,50 @@
 #include <algorithm>
 #include <list>
+#include <iostream>
 
-namespace test {
+namespace test
+{
 
-int foo(int a, int b) {
-    std::list<int> t;
+int foo(int a, int b)
+{
+	std::list<int> t;
+	if (a>b) { // 1 tab
 
-    if (a>b) { // 1 tab
-        int x = a+b;
-        int y = a*b // 2 tab
-              + a // 2 tab, plus 6 space to align
-              + b
-              + 42
-              + std::count_if(
-                t.begin(), // 2 tab, 6 space, again 1 tab
-                t.end(),
-                [](int p) {
-                    if (p<0) { // 2 tab, 6 space, again 2 tab
-                        return p%2; // 2 tab, 6 space, again 3 tab
-                    }
-                    return p%4;
-                } // lambda
-              ) // count
-              +1000;
-        int z=x+y;
-        if (z) return z;
-    } // a>b
+		std::cout << "This is "
+			<< "a test "
+			<< "variable is: "
+			<< a
+		;
 
-    return a+b;
+		// rare problems:
+
+			std::cout << "Hello " // 2 tab
+			          << "world! " // 2 tab, and hand allign with spaces
+			          << a
+			          ;
+
+			int x = a+b;
+			int y = a*b // 2 tab
+			        + a // 2 tab, plus 6 space to align
+			        + b
+			        + 42
+			        + std::count_if(
+			          	t.begin(), // 2 tab, 6 space, again 1 tab
+			          	t.end(),
+			          	[](int p) {
+			          		int d=p*2;
+			          		if (d>10) {
+			          			return p%2;
+			          		}
+			          		return p%4;
+			          	} // lambda
+			        ) // count
+			        +1000;
+			int z=x+y;
+			if (z) return z;
+		} // a>b
+
+	return a+b;
 }
 
 }
