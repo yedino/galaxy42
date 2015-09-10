b21d7681bbf1f6a12d6d9b8c9b648aa53f57e5e9
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2015-09-10 12:18:07 +0000

    [doc] updated style info (tabs)

--- antinet/src/antinet_sim/style_tab.cpp
+++ antinet/src/antinet_sim/style_tab.cpp
@@ -1,32 +1,32 @@
 #include <algorithm>
 #include <set>
 
-namespace test { 
+namespace test {
 
 int foo(int a, int b) {
-	std::set<int> t;
+    std::set<int> t;
 
-	if (a>b) { // 1 tab
-		int x = a+b;
-		int y = a*b // 2 tab a // 2 tab + 7 space
-		      + a
-		      + b
-		      + 42
-		      + std::count(
-		      	t.begin(), 
-		      	t.end(), 
-		      	[](int p) -> bool {
-		      		if (a<0) {
-		      			return a%2;
-		      		}
-		      		return a%4;
-		      	} // lambda
-		      ) // count
-		      +1000;
-		int z=x+y;
-	} // a>b
+    if (a>b) { // 1 tab
+        int x = a+b;
+        int y = a*b // 2 tab
+              + a // 2 tab, plus 6 space to align
+              + b
+              + 42
+              + std::count(
+                t.begin(), // 2 tab, 6 space, again 1 tab
+                t.end(),
+                [](int p) -> bool {
+                    if (a<0) { // 2 tab, 6 space, again 2 tab
+                        return a%2; // 2 tab, 6 space, again 3 tab
+                    }
+                    return a%4;
+                } // lambda
+              ) // count
+              +1000;
+        int z=x+y;
+    } // a>b
 
-	return a,b;
+    return a,b;
 }
 
 }
