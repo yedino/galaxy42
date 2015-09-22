ce5f5919323cb7459e372570468369330cb67125
drewka <ev-dev@tigusoft.pl>
2015-09-22 16:47:29 +0200

    change from emplace to [] operator

--- antinet/src/antinet_sim/c_network.cpp
+++ antinet/src/antinet_sim/c_network.cpp
@@ -3,7 +3,8 @@
 void c_network::add_node (std::shared_ptr<c_cjddev> node) {
 	//node->set_network(shared_from_this()); TODO
 	t_nym_id node_address = node->get_address();
-	m_node_map.emplace(node_address, std::move(node));
+    //m_node_map.emplace(node_address, std::move(node));
+    m_node_map[node_address] = std::move(node);
 
 }
 #if defined USE_API_TR
