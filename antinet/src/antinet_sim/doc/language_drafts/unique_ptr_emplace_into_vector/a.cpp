6fd96f14d0c58b5b054fc6d58b3a0b8c8a4782b8
woskov <woskov@tigusoft.pl>
2015-09-25 17:20:04 +0200

    testing

--- antinet/src/antinet_sim/doc/language_drafts/unique_ptr_emplace_into_vector/a.cpp
+++ antinet/src/antinet_sim/doc/language_drafts/unique_ptr_emplace_into_vector/a.cpp
@@ -2,9 +2,18 @@
 #include <vector>
 #include <string>
 #include <memory>
-
+#include <chrono>
 
 // --- intro ---
+/*  
+		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+//		stringEater3(std::move(tmpString));
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Printing took "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << "us.\n";
+*/
+
 
 namespace std{
 // http://stackoverflow.com/questions/7038357/make-unique-and-perfect-forwarding
@@ -39,24 +48,24 @@ struct animal {
 	virtual ~animal()=default; 
 
 
-	animal(const std::string & name): m_name(name)  { _info(this<<" T("<<m_name<<")"); } 
-	animal(std::string && name): m_name(name)  { _info(this<<" T("<<m_name<<")"); } 
+	animal(const std::string & name): m_name(name)  { /*   _info(this<<" T("<<m_name<<")");*/ } 
+	animal(std::string && name): m_name(name)  { /* _info(this<<" T("<<m_name<<")");*/ } 
 
-	animal(const animal & obj) : age(obj.age), mana(obj.mana), m_name(obj.m_name) { _info(this<<" T() copy"); }
-	animal(animal && obj) {  _info(this<<" T() move"); 
+	animal(const animal & obj) : age(obj.age), mana(obj.mana), m_name(obj.m_name) { /*_info(this<<" T() copy")*/; }
+	animal(animal && obj) { // _info(this<<" T() move"); 
 		std::swap(age, obj.age);
 		std::swap(mana, obj.mana);
 		std::swap(m_name, obj.m_name);
 	} 
 
-	animal& operator=(const animal & obj) { _info(this<<" op= copy"); 
+	animal& operator=(const animal & obj) { /*  _info(this<<" op= copy")*/; 
 		age=obj.age; 
 		mana=obj.mana;
 		m_name=obj.m_name;
 		return *this; 
 	}
 
-	animal& operator=(animal && obj) { _info(this<<" op= move"); 
+	animal& operator=(animal && obj) {/* _info(this<<" op= move")*/; 
 		age=obj.age; 
 		std::swap(age, obj.age);
 		std::swap(mana, obj.mana);
@@ -73,6 +82,8 @@ struct cat  : public animal { int lives=9; };
 struct world {
 	std::vector< std::unique_ptr< animal > > m_animal_ptr;
 	std::vector< animal > m_animal_obj;
+	world(){m_animal_ptr.reserve(5000);m_animal_obj.reserve(5000);}
+
 
 	void add_ptr(std::unique_ptr<animal> && ptr) {
 		m_animal_ptr.emplace_back( std::move(ptr) ); // move the unique_ptr to the animal into our array
@@ -99,6 +110,7 @@ int main_test() {
 	long int number=5000; 
 	long int tries=5000;
 
+		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 	for (long int t=0; t<tries; ++t)
 	{
 		world w;
@@ -107,40 +119,65 @@ int main_test() {
 			w.add_ptr( std::move(a) );
 		}
 	}
-	_mark("add_ptr: ");
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "add_ptr took "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+              << "us.\n";
+
+//	_mark("add_ptr: ");
 
 
+		 start = std::chrono::steady_clock::now();
 	for (long int t=0; t<tries; ++t)
 	{
 		world w;
 		for (long int n=0; n<number; ++n) {
-			_info("Test2");
+		//	_info("Test2");
 			animal a(name);
 			w.add_obj( std::move(a) );
 		}
 	}
-	_mark("add_obj: ");
+
+     end = std::chrono::steady_clock::now();
+    std::cout << "add_obj took "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << "us.\n";
+
+//	_mark("add_obj: ");
 
 
+		 start = std::chrono::steady_clock::now();
 	for (long int t=0; t<tries; ++t)
 	{
 		world w;
 		for (long int n=0; n<number; ++n) {
-			_info("Test3");
+//			_info("Test3");
 			w.new_obj( );
 		}
 	}
-	_mark("new_obj: ");
+     end = std::chrono::steady_clock::now();
+    std::cout << "new_obj took "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << "us.\n";
+//	_mark("new_obj: ");
 
+
+
+		 start = std::chrono::steady_clock::now();
 	for (long int t=0; t<tries; ++t)
 	{
 		world w;
 		for (long int n=0; n<number; ++n) {
-			_info("Test new_ptr");
+//			_info("Test new_ptr");
 			w.new_ptr( );
 		}
 	}
-	_mark("new_ptr: ");
+     end = std::chrono::steady_clock::now();
+    std::cout << "new_ptr took "
+              << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << "us.\n";
+
+//	_mark("new_ptr: ");
 
 
 }
