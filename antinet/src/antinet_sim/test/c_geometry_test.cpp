c59ab6eb5665d60cef071dc0eeb3e0ffebbd2551
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2015-11-13 11:43:50 +0100

    [fix] gtest, cmake

--- antinet/src/antinet_sim/test/c_geometry_test.cpp
+++ antinet/src/antinet_sim/test/c_geometry_test.cpp
@@ -1,5 +1,13 @@
 #include "gtest/gtest.h"
+#include "c_geometry.hpp"
 
 TEST(c_geometry_test, Test) {
-	EXPECT_EQ(1, 0);
+
+    //t_geo_r x1,y1;
+    //t_geo_r x2,y2;
+
+    t_geo_point A(100,100);
+    t_geo_point B(200,500);
+    t_geo_point C = c_geometry::point_on_line_between_distance(A,B,0.5);
+    EXPECT_TRUE(1);
 }
