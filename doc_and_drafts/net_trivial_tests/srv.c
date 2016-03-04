73831a76aa0d8dff791d5fdaa34b1c1b26ff3cc4
linuxhowtos.org <info@spsn.net>
2016-03-04 14:14:04 +0000

    External code: trivial example linuxhowtos.org

---
+++ doc_and_drafts/net_trivial_tests/srv.c
@@ -0,0 +1,61 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 256
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[BUFFER_SIZE];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     while (1) {
+	bzero(buffer, BUFFER_SIZE);
+	n = read(newsockfd, buffer, BUFFER_SIZE -1);
+	//printf("read %d bytes\n", n);
+     	if (n < 0) error("ERROR reading from socket");
+	//printf("Here is the message: %s\n", buffer);
+	n = write(newsockfd, buffer, n);
+	if (n < 0) error("ERROR writing to socket");
+	//sleep(1);
+     }
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
+}
