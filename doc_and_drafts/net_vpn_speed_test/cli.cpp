7a1107f696cdcec2dfb0ca3d17b33e5b94c526cf
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-03-04 12:10:10 +0000

    starting net_vpn_speed_test

---
+++ doc_and_drafts/net_vpn_speed_test/cli.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main_cli(int argc, const char **argv)  {
+
+	return 0;
+
+}
+
---
+++ doc_and_drafts/net_vpn_speed_test/srv.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main_srv(int argc, const char **argv)  {
+
+	return 0;
+
+}
+
