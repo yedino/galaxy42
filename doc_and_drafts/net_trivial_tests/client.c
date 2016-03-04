63a713a8bf73574b93ec443658f9f68fbc3cd6d3
Robert Oleksy <robertoleksy6@gmail.com>
2016-03-04 15:02:06 +0000

    use ipv6 in client

--- doc_and_drafts/net_trivial_tests/client.c
+++ doc_and_drafts/net_trivial_tests/client.c
@@ -17,6 +17,8 @@ A trivial socket client sending data.
 #include <netinet/in.h>
 #include <netdb.h> 
 
+#define BUFFER_SIZE 10000
+
 void error(const char *msg)
 {
     perror(msg);
@@ -26,14 +28,43 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+    char buffer[BUFFER_SIZE];
+
+     if (argc < 4) {
+         fprintf(stderr,"ERROR wrong params. Usage: program ip portnumber ipfamily\nexample:\nprogram 127.0.0.1 9000 ipv4\nprogram  [ipv6] 9000 ipv6\n");
+         exit(1);
+     }
+
+	int mode_ipv6 = 0==strcmp("ipv6",argv[3]);  // ***
+	fprintf("IPv6 mode: %s" , (mode_ipv6 ? "YES, IPv6" : "no, ipv4"));
+
+    	if (mode_ipv6) {
+    struct sockaddr_in6	serv_addr;
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname2(argv[1], AF_INET6);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
     }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin6_family = AF_INET6;
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin6_addr.s6_addr,
+         server->h_length);
+    serv_addr.sin6_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+ 
+
+    	}
+        else
+        {
+    struct sockaddr_in serv_addr;
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
@@ -51,17 +82,15 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+        }
+
+    memset(buffer, 'a', 256);
+    buffer[BUFFER_SIZE - 1] = '\0';
+    while (1) {
+    	n = write(sockfd, buffer, strlen(buffer));
+	    if (n < 0) 
+        	 error("ERROR writing to socket");
+    }
     close(sockfd);
     return 0;
 }
