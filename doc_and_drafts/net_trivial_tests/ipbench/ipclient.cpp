e637b6b648f1b801e16315dcbaf9c1987fe9568c
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-03-11 17:54:27 +0000

    Our network test has working stats in new client.

--- doc_and_drafts/net_trivial_tests/ipbench/ipclient.cpp
+++ doc_and_drafts/net_trivial_tests/ipbench/ipclient.cpp
@@ -2,11 +2,14 @@
 See LICENCE.txt
 */
 
+const char * disclaimer = "*** WARNING: This is a work in progress, do NOT use this code, it has bugs, vulns, and 'typpos' everywhere. ***"; // XXX
+
 #include <iostream>
 #include <stdexcept>
 #include <vector>
 #include <string>
-
+#include <iomanip>
+#include <algorithm>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,6 +29,109 @@ See LICENCE.txt
 #include <thread>
 
 #include <cstring>
+
+
+// ------------------------------------------------------------------ XXX move me
+
+class c_counter {
+	public:
+		typedef long long int t_count;
+
+		c_counter(c_counter::t_count tick_len, bool is_main); ///< tick_len - how often should we fire (print stats, and restart window)
+
+		void add(c_counter::t_count bytes); ///< general type for integrals (number of packets, of bytes)
+		bool tick(c_counter::t_count bytes, std::ostream &out); ///< a tick, can look at clock (in reasonable way), can call print(out); returns: should we print new line
+		void print(std::ostream &out) const; ///< prints now the statistics (better instead call tick)
+
+	private:
+		const t_count m_tick_len; ///< how often should I tick - it's both the window size, and the rate of e.g. print()
+		bool m_is_main; ///< is this the main counter (then show global stats and so on)
+
+		t_count m_pck_all, m_pck_w; ///< packets count: all, and in current window
+		t_count m_bytes_all, m_bytes_w; ///< the bytes (in current windoow)
+		t_count m_time_first; ///< when I was started at first actually (time in unix time)
+		t_count m_time_ws; ///< when when current window started (time in unix time)
+		t_count m_time_last; ///< current last time
+
+		bool tick_restarts;
+};
+
+c_counter::c_counter(c_counter::t_count tick_len, bool is_main)
+	: m_tick_len(tick_len), m_is_main(is_main),
+	m_pck_all(0), m_pck_w(0), m_bytes_all(0), m_bytes_w(0)
+{
+	m_time_first=time(NULL);
+	m_time_ws=time(NULL);
+}
+
+void c_counter::add(c_counter::t_count bytes) { ///< general type for integrals (number of packets, of bytes)
+	m_pck_all += 1;
+	m_pck_w += 1;
+
+	m_bytes_all += bytes;
+	m_bytes_w += bytes;
+}
+
+bool c_counter::tick(c_counter::t_count bytes, std::ostream &out) { ///< a tick, can look at clock (in reasonable way), can call print(out)
+	add(bytes);
+
+	bool do_print=0;
+	bool do_reset=0;
+	if (m_pck_all==1) { do_print=1; do_reset=1; }
+	if (0 == (m_pck_all%1000)) {
+		// std::cerr<<m_bytes_all<<std::endl; std::cout << m_time_last << " >? " << m_time_ws << std::endl;
+		m_time_last = std::time(NULL);
+		if (m_time_last >= m_time_ws + m_tick_len) { do_reset=1; do_print=1; }
+	}
+	if (do_print)	print(out);
+	if (do_reset) {
+		m_time_last = std::time(NULL);
+		m_time_ws = m_time_last;
+		m_pck_w=0;
+		m_bytes_w=0;
+	}
+	return do_print;
+}
+
+void c_counter::print(std::ostream &out) const { ///< prints now the statistics (better instead call tick)
+	using std::setw;
+
+	double time_all = m_time_last - m_time_first;
+	double time_w = m_time_last - m_time_ws;
+	const double epsilon = 1;
+	time_all = std::max(epsilon, time_all);
+	time_w   = std::max(epsilon, time_w);
+
+	double avg_bytes_all = m_bytes_all / time_all;
+	double avg_pck_all   = m_pck_all   / time_all;
+	double avg_bytes_w = m_bytes_w / time_w;
+	double avg_pck_w   = m_pck_w   / time_w;
+
+	// formatting
+	int p1=3; // digits after dot
+	int w1=p1+1+5; // width
+
+	double K=1000, Mi = 1024*1024, Gi = 1024*Mi; // units
+	out	<< std::setprecision(3) << std::fixed;
+
+	if (m_is_main) {
+		out << setw(6) << m_bytes_all/Gi << "GiB; "
+		    << "Speed: "
+	             << setw(w1) << (avg_pck_all / K) << " Kpck/s "
+		    << ",  " << setw(w1) << (avg_bytes_all*8 / Mi) << " Mib/s "
+		    << " = " << setw(w1) << (avg_bytes_all   / Mi) << " MiB/s " << "; "
+		    ;
+	}
+	out << "Window " << time_w << "s: "
+	             << setw(w1) << (avg_pck_w   / 1000) << " Kpck/s "
+	    << ",  " << setw(w1) << (avg_bytes_w  *8 / Mi) << " Mib/s "
+	    << " = " << setw(w1) << (avg_bytes_w     / Mi) << " MiB/s " << ") ";
+	out << std::endl;
+}
+
+
+
+// ------------------------------------------------------------------ XXX move me
 
 
 // --- to library --- TODO ------------------------------------------
@@ -139,12 +245,17 @@ void c_ipbench::prepare_socket() {
 
 void c_ipbench::event_loop() {
 	vector<char> buffer(m_blocksize,'a');
+	c_counter counter(2,true);
+	c_counter counter_big(10,false);
 	while (1) {
 		sockaddr_in6 * sockaddr6_ptr = & m_sockaddr6.get();
 		sockaddr * addr_ptr = reinterpret_cast<sockaddr*>(sockaddr6_ptr); // guaranteed by Linux. TODO
 		auto sent = sendto(m_sock, static_cast<void*>(buffer.data()), buffer.size(),  0,
 			addr_ptr,	sizeof(m_sockaddr6));
 		if (sent<0) error("Sent failed");
+
+		counter.tick(sent, std::cout);
+		counter_big.tick(sent, std::cout);
 	}
 }
 
@@ -158,6 +269,7 @@ void c_ipbench::run() {
 
 
 int main(int argc, char **argv) {
+	std::cerr << disclaimer << std::endl;
 	c_ipbench bench;
 	vector <string> args;
 	for (int i=0; i<argc; ++i) args.push_back(argv[i]);
