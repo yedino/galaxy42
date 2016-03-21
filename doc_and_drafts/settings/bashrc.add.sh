9d1b83834501559bcc29b245c6fb46480b330078
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-03-21 20:55:01 +0000

    Bash rc to show current interface IP

--- doc_and_drafts/settings/bashrc.add.sh
+++ doc_and_drafts/settings/bashrc.add.sh
@@ -4,6 +4,8 @@
 
 # --- tigusoft hacks for bashrc ---
 
+# ----> EDIT the lines marked [CONFIG] if you want to <----
+
 export PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"
 
 export TZ="/usr/share/zoneinfo/UTC"
@@ -33,16 +35,37 @@ function edit_kws() {
 xset r rate 160 60
 
 
-# TODO make this hacked code nicer
-# prva2xG@if8:
-netif_guest=$( ip a | egrep 'prv.+x?@if' | sed -e 's/.*prv\(.\+\).G@.*/\1/g' )
-netif_guest_count=$( ip a | egrep 'prv.+x?@if' | sed -e 's/.*prv\(.\+\).G@.*/\1/g' | wc -l )
-[[ $netif_guest_count == "1" ]] && {
-	echo "You are in guest network [$netif_guest]"
-	PS1='${debian_chroot:+($debian_chroot)}\[\033[01;30m\][$(date +%Y-%m-%d_%H:%M:%S)] \[\033[01;38m\]\u@\[\033[01;34m\]\h\[\033[00m\]:\[\033[01;32m\]\w\[\033[01;33m\]($(__git_ps1 "%s"))'
-	PS1="${PS1}"'\[\033[01;35m\]'
-	PS1="${PS1}<guest $netif_guest>"
-	PS1="${PS1}"'\[\033[00m\]\$ '
+function __ps_netif_glaxy {
+	# 17: galaxy0: <POINTOPOINT,
+	netif_galaxy=$( ip a | egrep 'galaxy[0-9]+:' | sed -e 's/.*galaxy\([0-9]\+\):.*/\1/g' )
+	netif_galaxy_count=$( ip a | egrep 'galaxy[0-9]+:' | sed -e 's/.*galaxy\([0-9]\+\):.*/\1/g' | wc -l )
+	[[ $netif_galaxy_count == "1" ]] && {
+		echo -n -e '\e[44m\e[33m'
+		echo -n " galaxy $netif_galaxy "
+		# lame? way to extract ipv6 address: 
+		# ip a | grep galaxy -A 10 | grep inet6 | head -n 1 | sed 's/[ ]*inet6 \([0-9a-f:]\+\)\/.*$/\1/g'
+		addr=$( ip a | grep galaxy -A 10 | grep inet6 | head -n 1 | sed 's/[ ]*inet6 \([0-9a-f:]\+\)\/.*$/\1/g' )
+		echo -n -e '\e[32m'
+		echo -n "$addr "
+	}
 }
+
+function __ps_netif_privnet {
+	# TODO make this hacked code nicer
+	# prva2xG@if8:
+	netif_guest=$( ip a | egrep 'prv.+x?@if' | sed -e 's/.*prv\(.\+\).G@.*/\1/g' )
+	netif_guest_count=$( ip a | egrep 'prv.+x?@if' | sed -e 's/.*prv\(.\+\).G@.*/\1/g' | wc -l )
+	[[ $netif_guest_count == "1" ]] && {
+		# echo "You are in guest network [$netif_guest]"
+		echo -n -e '\e[35m'
+		echo "<guest $netif_guest>"
+	}
+}
+
+# [CONFIG] remove the ending $ from your default PS above, or use this one instead (uncomment next 1 line):
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;30m\][$(date +%Y-%m-%d_%H:%M:%S)] \[\033[01;38m\]\u@\[\033[01;34m\]\h\[\033[00m\]:\[\033[01;32m\]\w\[\033[01;33m\]($(__git_ps1 "%s"))'
+
+PS1="${PS1}\$(__ps_netif_privnet)\$(__ps_netif_glaxy)"
+PS1="${PS1}"'\[\033[00m\]\$ '
 
 
