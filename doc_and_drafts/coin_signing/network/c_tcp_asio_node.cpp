3b6292d1b30e34b28e061abf828840106f911710
Robert Oleksy <robertoleksy6@gmail.com>
2016-04-05 14:08:15 +0000

    [wip] accept

--- doc_and_drafts/coin_signing/network/c_tcp_asio_node.cpp
+++ doc_and_drafts/coin_signing/network/c_tcp_asio_node.cpp
@@ -9,7 +9,9 @@ c_tcp_asio_node::c_tcp_asio_node()
 	m_asio_threads(),
 	m_stop_flag(false),
 	m_ioservice(),
-	m_recv_queue()
+	m_recv_queue(),
+	m_acceptor(m_ioservice, ip::tcp::endpoint(ip::tcp::v4(), 19000)), // TODO port
+	m_socket_accept(m_ioservice)
 {
 	unsigned int number_of_threads = std::thread::hardware_concurrency();
 	if (number_of_threads == 0) number_of_threads = 1;
@@ -22,6 +24,8 @@ c_tcp_asio_node::c_tcp_asio_node()
 	for(unsigned int i = 0; i < number_of_threads; ++i) {
 		m_asio_threads.emplace_back(new std::thread(thread_lambda)); // TODO make_unique
 	}
+
+	m_acceptor.async_accept(m_socket_accept, std::bind(&c_tcp_asio_node::accept_handler, this, std::placeholders::_1));
 }
 
 c_tcp_asio_node::~c_tcp_asio_node() {
@@ -51,6 +55,14 @@ c_network_message c_tcp_asio_node::receive() {
 }
 
 
+void c_tcp_asio_node::accept_handler(const boost::system::error_code &error) {
+	if (error) return;
+	auto endpoint = m_socket_accept.remote_endpoint();
+	std::lock_guard<std::mutex> lg(m_connection_map_mtx);
+	m_connection_map.emplace(endpoint, std::make_shared<c_connection>(*this, std::move(m_socket_accept)));
+}
+
+
 /************************************************************/
 
 c_connection::c_connection(c_tcp_asio_node &node, const boost::asio::ip::tcp::endpoint &endpoint)
@@ -62,6 +74,16 @@ c_connection::c_connection(c_tcp_asio_node &node, const boost::asio::ip::tcp::en
 {
 	m_socket.connect(endpoint); // TODO throw if error
 }
+
+c_connection::c_connection(c_tcp_asio_node &node, ip::tcp::socket && socket)
+:
+	m_tcp_node(node),
+	m_socket(std::move(socket)),
+	m_streambuff(),
+	m_ostream(&m_streambuff)
+{
+}
+
 
 void c_connection::send(std::string && message) {
 	std::unique_lock<std::mutex> lg(m_streambuff_mtx);
--- doc_and_drafts/coin_signing/network/c_tcp_asio_node.hpp
+++ doc_and_drafts/coin_signing/network/c_tcp_asio_node.hpp
@@ -18,23 +18,28 @@ class c_tcp_asio_node final : public c_connection_base
 	public:
 		c_tcp_asio_node();
 		~c_tcp_asio_node();
-		void send(c_network_message && message) override; // TODO
+		void send(c_network_message && message) override;
 		c_network_message receive() override; // TODO
 	private:
 		std::vector<std::unique_ptr<std::thread>> m_asio_threads;
 		std::atomic<bool> m_stop_flag;
-
 		boost::asio::io_service m_ioservice;
 		c_locked_queue<c_network_message> m_recv_queue;
 
 		std::mutex m_connection_map_mtx;
 		std::map<boost::asio::ip::tcp::endpoint, std::shared_ptr<c_connection>> m_connection_map; ///< always use m_connection_map_mtx !!!
+
+		boost::asio::ip::tcp::acceptor m_acceptor;
+		boost::asio::ip::tcp::socket m_socket_accept;
+
+		void accept_handler(const boost::system::error_code& error); // TODO
 };
 
 class c_connection {
 	public:
-		c_connection(c_tcp_asio_node &node, const boost::asio::ip::tcp::endpoint &endpoint);
-		void send(std::string && message); // TODO
+		c_connection(c_tcp_asio_node &node, const boost::asio::ip::tcp::endpoint &endpoint); ///< connect constructor
+		c_connection(c_tcp_asio_node &node, boost::asio::ip::tcp::socket &&socket); ///< accept constructor // TODO
+		void send(std::string && message);
 		std::string receive(); // TODO
 
 	private:
@@ -42,8 +47,8 @@ class c_connection {
 		boost::asio::ip::tcp::socket m_socket;
 
 		std::mutex m_streambuff_mtx;
-		boost::asio::streambuf m_streambuff;
-		std::ostream m_ostream;
+		boost::asio::streambuf m_streambuff; ///< always lock m_streambuff_mtx before use
+		std::ostream m_ostream; ///< always lock m_streambuff_mtx before use
 
 		void write_handler(const boost::system::error_code &e, std::size_t length);
 
