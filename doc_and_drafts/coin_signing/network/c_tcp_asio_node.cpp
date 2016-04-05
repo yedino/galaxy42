a7debc65b5c3995616999574eedde8276da1071f
Robert Oleksy <robertoleksy6@gmail.com>
2016-04-05 07:44:59 +0000

    [wip] c_tcp_asio_node

--- doc_and_drafts/coin_signing/network/c_tcp_asio_node.cpp
+++ doc_and_drafts/coin_signing/network/c_tcp_asio_node.cpp
@@ -4,7 +4,8 @@ c_tcp_asio_node::c_tcp_asio_node()
 :
 	m_asio_threads(),
 	m_stop_flag(false),
-	m_ioservice()
+	m_ioservice(),
+	m_recv_queue_ptr(std::make_shared<c_locked_queue<c_network_message>>())
 {
 	unsigned int number_of_threads = std::thread::hardware_concurrency();
 	if (number_of_threads == 0) number_of_threads = 1;
@@ -15,7 +16,7 @@ c_tcp_asio_node::c_tcp_asio_node()
 		}
 	};
 	for(unsigned int i = 0; i < number_of_threads; ++i) {
-		m_asio_threads.emplace_back(new std::thread(thread_lambda));
+		m_asio_threads.emplace_back(new std::thread(thread_lambda)); // TODO make_unique
 	}
 }
 
@@ -33,4 +34,20 @@ void c_tcp_asio_node::send(c_network_message && message) {
 
 c_network_message c_tcp_asio_node::receive() {
 
+}
+
+
+/************************************************************/
+
+c_tcp_asio_node::connection::connection(boost::asio::io_service &io_service, const std::shared_ptr<c_locked_queue<c_network_message>> &msg_queue)
+:
+	m_socket(io_service)
+{
+}
+
+void c_tcp_asio_node::connection::connect(const boost::asio::ip::tcp::endpoint &endpoint) {
+	//m_socket.async_connect(endpoint, []()) // TODO
+}
+
+void c_tcp_asio_node::connection::send(std::string && message) {
 }
--- doc_and_drafts/coin_signing/network/c_tcp_asio_node.hpp
+++ doc_and_drafts/coin_signing/network/c_tcp_asio_node.hpp
@@ -2,8 +2,11 @@
 #define C_TCP_ASIO_NODE_H
 
 #include "c_connection_base.hpp"
+#include "c_locked_queue.hpp"
 #include <atomic>
 #include <boost/asio.hpp>
+#include <condition_variable>
+#include <map>
 #include <memory>
 #include <thread>
 #include <vector>
@@ -12,13 +15,30 @@ class c_tcp_asio_node final : public c_connection_base
 {
 	public:
 		c_tcp_asio_node();
-		virtual ~c_tcp_asio_node();
-		void send(c_network_message && message) override;
-		c_network_message receive() override;
+		~c_tcp_asio_node();
+		void send(c_network_message && message) override; // TODO
+		c_network_message receive() override; // TODO
 	private:
 		std::vector<std::unique_ptr<std::thread>> m_asio_threads;
 		std::atomic<bool> m_stop_flag;
 		boost::asio::io_service m_ioservice;
+		std::shared_ptr<c_locked_queue<c_network_message>> m_recv_queue_ptr;
+
+		class connection {
+			public:
+				connection(boost::asio::io_service &io_service, const std::shared_ptr<c_locked_queue<c_network_message>> &msg_queue);
+				void send(std::string && message); // TODO
+				std::string receive(); // TODO
+
+			private:
+				boost::asio::ip::tcp::socket m_socket;
+				std::mutex m_streambuff_mtx;
+				boost::asio::streambuf m_streambuff;
+
+				void connect(const boost::asio::ip::tcp::endpoint &endpoint); // TODO
+		};
+
+		std::map<boost::asio::ip::tcp::endpoint, connection> m_connection_map;
 };
 
 #endif // C_TCP_ASIO_NODE_H
