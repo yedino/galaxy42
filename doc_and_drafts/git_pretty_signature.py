92fb0249d21a3e6b2aa7bc0116f8e911db1f712c
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-04-11 14:43:51 +0200

    [add] deque buffer for printing single lines, get terminal height

--- doc_and_drafts/git_pretty_signature.py
+++ doc_and_drafts/git_pretty_signature.py
@@ -4,7 +4,9 @@ import select
 import tty
 import termios
 import argparse
-from subprocess import check_output, call, STDOUT
+from collections import deque
+from subprocess import check_output, call, CalledProcessError, STDOUT
+import locale
 
 class bcolors:
     HEADER = '\033[95m'
@@ -23,6 +25,32 @@ class bcolors:
         self.ENDC = ''
 
 
+##Returns the terminal size WxH
+#Found on http://stackoverflow.com/a/566752/2646228
+def getTerminalSize():
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+    return int(cr[1]), int(cr[0])
+
+
 def readchar(wait_for_char=True):
     old_settings = termios.tcgetattr(sys.stdin)
     tty.setcbreak(sys.stdin.fileno())
@@ -35,6 +63,7 @@ def readchar(wait_for_char=True):
 
 
 def print_totag_stats(check_mode=False):
+    encoding = locale.getdefaultlocale()[1]
     i = 0
     ch = 't'
     G_totag = 0
@@ -43,7 +72,13 @@ def print_totag_stats(check_mode=False):
     N_totag = 0
     while (ch != 'q'):
         data_com = 'git log -1 --skip='+str(i)+' --pretty=format:"%G?%d"'
-        data = check_output(data_com.split())
+
+        try:
+            data = check_output(data_com.split()).decode(encoding)
+        except CalledProcessError as e:
+            print("exit(1)")
+            exit(1)
+
         sig_stat = data[1:2]
         if sig_stat == 'G':
             G_totag+=1
@@ -54,9 +89,9 @@ def print_totag_stats(check_mode=False):
         elif sig_stat == 'N':
             N_totag+=1
 
-        if data.find("tag") != -1:
+        if data.find("tag") != -1 or not data:
             tag_info = data[2:-1]
-            col_str = bcolors.OKGREEN
+            col_tag = bcolors.OKGREEN
             col_end = bcolors.ENDC
             # last tag checking
             if sig_stat == 'G':
@@ -96,13 +131,52 @@ def print_totag_stats(check_mode=False):
                 print(col_coms+"WARNING: At leas one not signed commit!!"+col_end)
                 if check_mode:
                     exit(1)
-            info_str = col_coms+"Signatures status after "+col_tag+tag_info+"\n"+col_coms+str(G_totag)+": GOOD\n"+str(U_totag)+": UNTRUSTED\n"+str(B_totag)+": BAD SIGN\n"+str(N_totag)+": NOT SIGNED"+col_end
+            info_head=""
+            if not data:
+                info_head = col_coms+"Signatures status for all commits:\n"
+            else:
+                info_head = col_coms+"Signatures status after "+col_tag+tag_info+"\n"
+            info_str = info_head+col_coms+str(G_totag)+": GOOD\n"+str(U_totag)+": UNTRUSTED\n"+str(B_totag)+": BAD SIGN\n"+str(N_totag)+": NOT SIGNED"+col_end
             print(info_str)
             return
         i+=1
 
 def is_git_directory(path = '.'):
     return call(['git', '-C', path, 'branch'], stderr=STDOUT, stdout=open(os.devnull, 'w')) == 0
+
+def buffor_popleft_n(buffor, lines):
+    for line in range(lines):
+        print(buffor.popleft())
+
+def buffor_popleft_all(buffor):
+    while buffor:
+        print(buffor.popleft())
+
+def buffor_popleft_singly(buffor):
+    ch = 't'
+    while buffor:
+        print(buffor.popleft())
+        sys.stdout.write(':')
+        sys.stdout.flush()
+        ch = readchar()
+        sys.stdout.write('\b')
+        sys.stdout.write("\033[K")
+        sys.stdout.flush()
+        if ch == 'q':
+            return 'q'
+
+def wait_for_quit():
+    ch = 't'
+    while ch != 'q':
+        sys.stdout.write('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b')
+        sys.stdout.write("\033[K") # Clear to the end of line
+        # sys.stdout.write("\033[F") # Cursor up one line
+        sys.stdout.write("(END) type q for quit")
+        sys.stdout.flush()
+        ch = readchar()
+    sys.stdout.write('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b')
+    sys.stdout.write("\033[K")
+
 
 if __name__ == "__main__":
 
@@ -119,20 +193,58 @@ if __name__ == "__main__":
     else:
         print_totag_stats()
 
+    encoding = locale.getdefaultlocale()[1]
     i=0
     ch='t'
 
+    teminal_height = 20
+    if (sys.version_info > (3, 0)):
+        terminal_height = os.get_terminal_size().lines
+    else:
+        terminal_height = getTerminalSize()[1]
+
+    terminal_height -= 9 # for status // TODO Load status to buffer too
+    first_lines_printed = False
+    buffor = deque([])
     while (ch != 'q'):
         data_com = 'git log -1 --skip='+str(i)+' --pretty=format:"%H:%G?%d"'
-        data = check_output(data_com.split())
+
+        try:
+            data = check_output(data_com.split()).decode(encoding)
+        except CalledProcessError as e:
+            print("exit(1)")
+            exit(1)
+
+        if not first_lines_printed and len(buffor) >= terminal_height:
+            buffor_popleft_n(buffor,terminal_height)
+            first_lines_printed = True
+        if first_lines_printed and buffor and buffor_popleft_singly(buffor) == 'q':
+            break
+
+        if not data:
+            if len(buffor) <= terminal_height and not first_lines_printed:
+                print("printed?:" + str(first_lines_printed))
+                buffor_popleft_all(buffor)
+            elif len(buffor) >= terminal_height and not first_lines_printed:
+                buffor_popleft_n(buffor,terminal_height)
+
+            if buffor_popleft_singly(buffor) != 'q':
+                wait_for_quit()
+
+            break     # end of commits success exit
 
         del_pos = data.find(':')
         com_hash = data[1:del_pos]
         sig_stat = data[del_pos+1:del_pos+2]
 
-        com = "git log -1 "+com_hash+" --pretty=format:\"%C(yellow)%H%C(auto)%d\n"
+        com = "git||log||-1||"+com_hash+"||--pretty=format:\"%C(yellow)%H%C(auto)%d\n"
         sig_info_com = "git log "+com_hash+" -1 --pretty=format:\"%GG\""
-        sig_info = check_output(sig_info_com.split())
+
+        if (sys.version_info > (3, 0)):
+            sig_info = check_output(sig_info_com.split()).decode(encoding)
+        else:
+            sig_info = check_output(sig_info_com.split())
+
         sig_info = repr(sig_info).replace('"','\\"')[2:-2]
         sig_info = sig_info.replace('\\n','\n')
         if sig_stat == 'G':
@@ -148,10 +260,29 @@ if __name__ == "__main__":
             com += "%C(red)! WARNING !\n%C(red)SIGNATURE - NOT SIGNED\n%C(red)! WARNING !\n"
             com += sig_info.replace('\n','\n%C(red)')[1:-1] # empty
         com += "%C(white)Author: %aN <%aE>\n%C(white)Date: %aD\n\n    %B\n\""
-        os.system(com)
-        sys.stdout.write(':')
-        sys.stdout.flush()
-        ch = readchar()
-        sys.stdout.write('\b')
+
+        try:
+            #print(com.split("||"))
+            if (sys.version_info > (3, 0)):
+                data = check_output(com.split("||")).decode(encoding)
+                data = data[1:-2]
+            else:
+                data = check_output(com.split("||"))
+                data = data[1:-2]
+
+            data_lines = data.split('\n')
+            for line in data_lines:
+                buffor.append(line)
+        except CalledProcessError as e:
+            print("exit(1)")
+            exit(1)
+
+        #sys.stdout.write(':')
+        #sys.stdout.flush()
+        #ch = readchar()
+        #sys.stdout.write('\b')
         i+=1
 
+    #print("end buffor_size: "+str(len(buffor)));
+    #for line in buffor:
+    #    print(line)
