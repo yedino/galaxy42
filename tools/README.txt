61629ad90c0eaad2761185dd20dbb7dc73f2aa43
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-04-12 09:58:54 +0000

    Connecting the git check tool to top

---
+++ tools/README.txt
@@ -0,0 +1,4 @@
+
+Tools for entire project in general.
+
+
---
+++ tools/git_check.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+projecttop=$( git rev-parse --show-toplevel 2>/dev/null )
+
+cd "$projecttop"
+
+python3 tools/git_pretty_signature.py | less -FRSX
+:w
+
