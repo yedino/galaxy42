d58eae14f765c653ab40ce1320d2aa293bd43b86
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-04-13 13:56:22 +0200

    usage: python3 git_pretty_signatures.py
    less working now as subprocess with stdin=PIPE

--- tools/git_pretty_signature.py
+++ tools/git_pretty_signature.py
@@ -5,8 +5,9 @@ import tty
 import termios
 import argparse
 from collections import deque
-from subprocess import check_output, call, CalledProcessError, STDOUT
+from subprocess import check_output, call, Popen, PIPE, CalledProcessError, STDOUT
 import locale
+import subprocess
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,44 +26,7 @@ class bcolors:
         self.ENDC = ''
 
 
-##Returns the terminal size WxH
-#Found on http://stackoverflow.com/a/566752/2646228
-def getTerminalSize():
-    import os
-    env = os.environ
-    def ioctl_GWINSZ(fd):
-        try:
-            import fcntl, termios, struct, os
-            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
-        '1234'))
-        except:
-            return
-        return cr
-    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
-    if not cr:
-        try:
-            fd = os.open(os.ctermid(), os.O_RDONLY)
-            cr = ioctl_GWINSZ(fd)
-            os.close(fd)
-        except:
-            pass
-    if not cr:
-        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
-    return int(cr[1]), int(cr[0])
-
-
-def readchar(wait_for_char=True):
-    old_settings = termios.tcgetattr(sys.stdin)
-    tty.setcbreak(sys.stdin.fileno())
-    try:
-        if wait_for_char or select.select([sys.stdin, ], [], [], 0.0)[0]:
-            char = os.read(sys.stdin.fileno(), 1)
-            return char if type(char) is str else char.decode()
-    finally:
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-
-
-def make_totag_stats(buffor, check_mode = False):
+def totag_stats(pager, encoding, check_mode = False):
     encoding = locale.getdefaultlocale()[1]
     i = 0
     ch = 't'
@@ -76,7 +40,7 @@ def make_totag_stats(buffor, check_mode = False):
         try:
             data = check_output(data_com.split()).decode(encoding)
         except CalledProcessError as e:
-            print("exit(1)")
+            pager.stdin.write('exit(1)'.encode(encoding))
             exit(1)
 
         sig_stat = data[1:2]
@@ -89,167 +53,185 @@ def make_totag_stats(buffor, check_mode = False):
         elif sig_stat == 'N':
             N_totag+=1
 
-        if data.find("tag") != -1 or not data:
-            tag_info = data[2:-1]
-            col_tag = bcolors.OKGREEN
-            col_end = bcolors.ENDC
-            # last tag checking
-            if sig_stat == 'G':
-                G_totag-=1
-            if sig_stat == 'U':
-                U_totag-=1
-                col_tag = bcolors.WARNING
-                buffor.append(col_tag+"WARNING: Last tag have untrusted sign!"+col_end)
-                if check_mode:
-                    exit(1)
-            if sig_stat == 'B':
-                B_totag-=1
-                col_tag = bcolors.FAIL
-                buffor.append(col_tag+"ERROR: Last tag have bad sign!!"+col_end)
-                if check_mode:
-                    exit(1)
-            if sig_stat == 'N':
-                N_totag-=1
-                col_tag = bcolors.FAIL
-                buffor.append(col_tag+"ERROR: Last tag is not signed!!"+col_end)
-                if check_mode:
-                    exit(1)
+        tag_pos = data.find("tag")
+        if tag_pos != -1 or not data:
+            # getting tag from %d ref names
+            if tag_pos != -1:
+                tag_info = data[tag_pos:-2]
+                tag_end_pos = tag_info.find(',')
+                tag_info = tag_info[:tag_end_pos]
+
+                col_tag = bcolors.OKGREEN
+                col_end = bcolors.ENDC
+                # last tag checking
+                if sig_stat == 'G':
+                    G_totag-=1
+                if sig_stat == 'U':
+                    U_totag-=1
+                    col_tag = bcolors.WARNING
+                    string = col_tag+"WARNING: Last tag have untrusted sign!"+col_end
+                    pager.stdin.write(string.encode(encoding))
+                    if check_mode:
+                        exit(1)
+                if sig_stat == 'B':
+                    B_totag-=1
+                    col_tag = bcolors.FAIL
+                    string = col_tag+"ERROR: Last tag have bad sign!!"+col_end
+                    pager.stdin.write(string.encode(encoding))
+                    if check_mode:
+                        exit(1)
+                if sig_stat == 'N':
+                    N_totag-=1
+                    col_tag = bcolors.FAIL
+                    string = col_tag+"ERROR: Last tag is not signed!!"+col_end
+                    pager.stdin.write(string.encode(encoding))
+                    if check_mode:
+                        exit(1)
+
             col_coms = bcolors.OKGREEN
             # commits checking
             if U_totag:
                 col_coms = bcolors.OKBLUE
-                buffor.append(col_coms+"NOTICE: At least one commit with untrusted sign!"+col_end)
+                string = col_coms+"NOTICE: At least one commit with untrusted sign!"+col_end
+                pager.stdin.write(string.encode(encoding))
                 if check_mode:
                     exit(1)
             if B_totag:
                 col_coms = bcolors.FAIL
-                buffor.append(col_coms+"ERROR: At least one commit with bad sign!"+col_end)
+                string = col_coms+"ERROR: At least one commit with bad sign!"+col_end
+                pager.stdin.write(string.encode(encoding))
                 if check_mode:
                     exit(1)
             if N_totag:
                 col_coms = bcolors.WARNING
-                buffor.append(col_coms+"WARNING: At leas one not signed commit!!"+col_end)
+                string = col_coms+"WARNING: At leas one not signed commit!!"+col_end
+                pager.stdin.write(string.encode(encoding))
                 if check_mode:
                     exit(1)
+
             info_head=""
-            if not data:
-                info_head = col_coms+"Signatures status for all commits:\n"
+            info_ft=""
+            if tag_pos != -1:
+                info_head = col_coms+"Signatures status after "+col_tag+tag_info+":\n"
+                if col_tag == bcolors.OKGREEN and (col_coms == bcolors.OKGREEN or col_coms == bcolors.OKBLUE):
+                    info_ft = col_coms+"Everything SINCE LAST TAG "+tag_info[5:]+" seems correctly signed\n"
+                else:
+                    info_ft = col_coms+"Occur at least one problem with signatures SINCE LAST TAG "+tag_info[5:]+'\n'
             else:
-                info_head = col_coms+"Signatures status after "+col_tag+tag_info+"\n"
-            info_str = info_head+col_coms+str(G_totag)+": GOOD\n"+str(U_totag)+": UNTRUSTED\n"+str(B_totag)+": BAD SIGN\n"+str(N_totag)+": NOT SIGNED"+col_end
-            buffor.append(info_str)
+                info_head = col_coms+"Signatures status for all commits:\n"
+                info_ft = bcolors.WARNING+"no tag found in this repository\n"
+
+            info_str = info_head                                    \
+                        +col_coms+str(G_totag)+": GOOD\n"           \
+                        +col_coms+str(U_totag)+": UNTRUSTED\n"      \
+                        +col_coms+str(B_totag)+": BAD SIGN\n"       \
+                        +col_coms+str(N_totag)+": NOT SIGNED\n"     \
+                        +col_coms+info_ft+col_end+'\n'
+            pager.stdin.write(info_str.encode(encoding))
             return
         i+=1
 
 def is_git_directory(path = '.'):
     return call(['git', '-C', path, 'branch'], stderr=STDOUT, stdout=open(os.devnull, 'w')) == 0
 
-def buffor_popleft_n(buffor, lines):
-    for line in range(lines):
-        print(buffor.popleft())
-
-def buffor_popleft_all(buffor):
-    while buffor:
-        print(buffor.popleft())
-
-def buffor_popleft_singly(buffor):
-    while buffor:
-        try:
-            print(buffor.popleft())
-        except:
-            exit(0)     # catch exit from less
-
 if __name__ == "__main__":
-
-    teminal_height = 20
-    terminal_height = getTerminalSize()[1]
 
     if not is_git_directory():
         print("Exit: This directory does not contain a git repository!")
         exit(1)
 
-    buffor = deque([])
+    try:
+        encoding = locale.getdefaultlocale()[1]
+        pager = Popen(['less', '-F', '-R', '-S', '-X', '-K'], stdin=PIPE, stdout=sys.stdout)
 
-    parser = argparse.ArgumentParser(description='Display git log with gpg signatures in pretty colored format')
-    parser.add_argument('-c','--check',action="store_true",
-                        help='running check mode, exit with 1 status if occurs warning or error')
-    args = parser.parse_args()
-    if args.check:
-        make_totag_stats(buffor, True)
-    else:
-        make_totag_stats(buffor)
+        parser = argparse.ArgumentParser(description='Display git log with gpg signatures in pretty colored format')
+        parser.add_argument('-c','--check',action="store_true",
+                            help='running check mode, exit with 1 status if occurs warning or error')
 
-    encoding = locale.getdefaultlocale()[1]
-    first_lines_printed = False
-
-    i=0
-    while (True):
-        data_com = 'git log -1 --skip='+str(i)+' --pretty=format:"%H:%G?%d"'
-
-        try:
-            data = check_output(data_com.split()).decode(encoding)
-        except CalledProcessError as e:
-            print("exit(1)")
-            exit(1)
-
-        if not first_lines_printed and len(buffor) >= terminal_height:
-            buffor_popleft_n(buffor,terminal_height)
-            first_lines_printed = True
-
-        if first_lines_printed and buffor:
-            buffor_popleft_singly(buffor)
-
-        if not data:
-            if len(buffor) <= terminal_height and not first_lines_printed:
-                buffor_popleft_all(buffor)
-            elif len(buffor) >= terminal_height and not first_lines_printed:
-                buffor_popleft_n(buffor,terminal_height)
-
-            buffor_popleft_singly(buffor)
-            break     # end of commits success exit
-
-        del_pos = data.find(':')
-        com_hash = data[1:del_pos]
-        sig_stat = data[del_pos+1:del_pos+2]
-
-        com = "git||log||-1||"+com_hash+"||--pretty=format:\"%C(yellow)%H%C(auto)%d\n"
-        sig_info_com = "git log "+com_hash+" -1 --pretty=format:\"%GG\""
-
-        if (sys.version_info > (3, 0)):
-            sig_info = check_output(sig_info_com.split()).decode(encoding)
+        args = parser.parse_args()
+        if args.check:
+            totag_stats(pager, encoding, True)
         else:
-            sig_info = check_output(sig_info_com.split())
+            totag_stats(pager, encoding)
 
-        sig_info = repr(sig_info).replace('"','\\"')[2:-2]
-        sig_info = sig_info.replace('\\n','\n')
-        sig_info = sig_info.replace('\\\"','\"')
-        if sig_stat == 'G':
-            com += "%C(green)SIGNATURE - GOOD\n%C(green)"
-            com += sig_info.replace('\n','\n%C(green)')[1:-1]
-        elif sig_stat == 'U':
-            com += "%C(cyan)SIGNATURE - UNTRUSTED\n%C(cyan)"
-            com += sig_info.replace('\n','\n%C(cyan)')[1:-1]
-        elif sig_stat == 'B':
-            com += "%C(magneta)SIGNATURE - BAD SIGN\n%C(magneta)"
-            com += sig_info.replace('\n','\n%C(magneta)')[1:-1]
-        elif sig_stat == 'N':
-            com += "%C(red)! WARNING !\n%C(red)SIGNATURE - NOT SIGNED\n%C(red)! WARNING !\n"
-            com += sig_info.replace('\n','\n%C(red)')[1:-1] # empty
-        com += "%C(white)Author: %aN <%aE>\n%C(white)Date: %aD\n\n    %B\n\""
+        i=0
+        while (True):
+            data_com = 'git log -1 --skip='+str(i)+' --pretty=format:"%H:%G?%d"'
 
-        try:
+            try:
+                data = check_output(data_com.split()).decode(encoding)
+            except CalledProcessError as e:
+                pager.stdin.write('exit(1)'.encode(encoding))
+                exit(1)
+
+
+            if not data:
+                break     # end of commits success exit
+
+            del_pos = data.find(':')
+            com_hash = data[1:del_pos]
+            sig_stat = data[del_pos+1:del_pos+2]
+
+            com = "git||log||-1||"+com_hash+"||--pretty=format:\"%C(yellow)%H%C(auto)%d\n"
+            sig_info_com = "git log "+com_hash+" -1 --pretty=format:\"%GG\""
+
+            tag_pos = data.find("tag")
+            # getting tag from %d ref names
+            if tag_pos != -1:
+                tag_info = data[tag_pos:-2]
+                info_len = len(tag_info) + len(com_hash) - 1
+                tag_end_pos = tag_info.find(',')
+                tag_info = tag_info[:tag_end_pos]
+                com += "%C(yellow reverse)=== "+tag_info+" is here " + '='*info_len+'\n'    #TODO bad nr of =
+
             if (sys.version_info > (3, 0)):
-                data = check_output(com.split("||")).decode(encoding)
-                data = data[1:-2]
+                sig_info = check_output(sig_info_com.split()).decode(encoding)
             else:
-                data = check_output(com.split("||"))
-                data = data[1:-2]
+                sig_info = check_output(sig_info_com.split())
 
-            data_lines = data.split('\n')
-            for line in data_lines:
-                buffor.append(line)
-        except CalledProcessError as e:
-            print("exit(1)")
-            exit(1)
+            sig_info = repr(sig_info).replace('"','\\"')[2:-2]
+            sig_info = sig_info.replace('\\n','\n')
+            sig_info = sig_info.replace('\\\"','\"')
+            if sig_stat == 'G':
+                com += "%C(green)SIGNATURE - GOOD\n%C(green)"
+                com += sig_info.replace('\n','\n%C(green)')[1:-1]
+            elif sig_stat == 'U':
+                com += "%C(cyan)SIGNATURE - UNTRUSTED\n%C(cyan)"
+                com += sig_info.replace('\n','\n%C(cyan)')[1:-1]
+            elif sig_stat == 'B':
+                com += "%C(magneta)SIGNATURE - BAD SIGN\n%C(magneta)"
+                com += sig_info.replace('\n','\n%C(magneta)')[1:-1]
+            elif sig_stat == 'N':
+                com += "%C(red)! WARNING !\n%C(red)SIGNATURE - NOT SIGNED\n%C(red)! WARNING !\n"
+                com += sig_info.replace('\n','\n%C(red)')[1:-1] # empty
+            com += "%C(white)Author: %aN <%aE>\n%C(white)Date: %aD\n\n    %B\n\""
 
-        i+=1
+            try:
+                if (sys.version_info > (3, 0)):
+                    data = check_output(com.split("||")).decode(encoding)
+                    data = data[1:-1]
+                else:
+                    data = check_output(com.split("||"))
+                    data = data[1:-1]
+
+                pager.stdin.write(data.encode(encoding))
+
+            except CalledProcessError as e:
+                pager.stdin.write('exit(1)'.encode(encoding))
+                exit(1)
+
+            i+=1
+
+        pager.stdin.close()
+        pager.wait()
+
+    except IOError as e:
+        if e.errno == 32:  # catch q quit from less
+            pass
+        else:
+            print("Unexpected error: " + os.strerror(e.errno))
+            raise
+
+    except KeyboardInterrupt:
+        # less should handle this
+        pass
