78eb98bb2f409ac8b04518942ccb08a3f46ffb90
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-04-20 11:21:56 +0200

    [wip] load json conffile

--- galaxy42/loadjson.hpp
+++ galaxy42/loadjson.hpp
@@ -1,5 +1,6 @@
 
 #include <jsoncpp/json/json.h>
+#include <libs1.hpp>
 
 struct c_peer {
     uint32_t m_nr;		///< peer number (could be usefull in routing)
@@ -9,14 +10,69 @@ struct c_peer {
 
 class c_json_file_parser {
   public:
-    c_json_file_parser (const std::string &filename);
+	c_json_file_parser (const std::string &filename);
 
     c_json_file_parser () = delete;
     c_json_file_parser (const c_json_file_parser &) = delete;
 
-    std::vector<c_peer> get_peers();
+	std::vector<c_peer> get_peers();
+	Json::Value get_root () {
+		return root;
+	}
 
   private:
     bool parse_file(const std::string &filename);
     Json::Value root;   // will contains the root value after parsing.
+};
+
+
+class c_auth_password_load {
+	c_auth_password_load (const std::string &filename) {
+  try {
+		c_json_file_parser parser(filename);
+
+
+  } catch (std::invalid_argument &err) {
+		std::cout << "Fail to load galaxy configuration file" << std::endl;
+		std::cout << err.what() << std::endl;
+  }
+	}
+
+	c_auth_password_load (const c_auth_password_load &) = delete;
+};
+
+class c_connect_to_load {
+	c_connect_to_load (const std::string &filename) {
+  try {
+		c_json_file_parser parser(filename);
+
+
+  } catch (std::invalid_argument &err) {
+		std::cout << "Fail to load galaxy configuration file" << std::endl;
+		std::cout << err.what() << std::endl;
+  }
+	}
+
+	c_connect_to_load (const c_connect_to_load &) = delete;
+};
+
+
+class c_galaxyconf_load {
+  public:
+	c_galaxyconf_load (const std::string &filename = "galaxy.conf") {
+  try {
+		c_json_file_parser parser(filename);
+
+
+  } catch (std::invalid_argument &err) {
+		std::cout << "Fail to load galaxy configuration file" << std::endl;
+		std::cout << err.what() << std::endl;
+  }
+	}
+
+	c_galaxyconf_load (const c_galaxyconf_load &) = delete;
+
+  private:
+
+
 };
