e288175b584fc40b4a8329f74ab9acf0a2815d94
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-04-21 10:52:05 +0000

    Crypto: removed old tests.

--- galaxy42/crypto.hpp
+++ galaxy42/crypto.hpp
@@ -55,67 +55,6 @@ class c_symhash_state final : public c_crypto_state {
 
 
 
-
-// ... 
-// For given CA (form me, to given recipient) - and should be permanent
-
-// ==================================================================
-
-/***
-
-DHDH CryptoAuth
-
-
-On ID creation:
-Ali: ap,AP = Generte_DH // make permanent keys: ap = Ali_DH_Perm_priv , AT = Ali_DH_Perm_Pub
-
-On ID creation:
-Bob: bp,BP = Generte_DH // same for Bob
-
-
-On CA establishing from Alice to Bob:
-Alice: 
-  at,AT = Generte_DH()  // make temporary keys: at = Ali_DH_Tmp_priv , AT = Ali_DH_Tmp_Pub
-  r1 = credentials_given_by_b || ';' || secure_random
-  mt = "AT,r1"; // message with temporary key and temporary random
-
-  KP = complete_DH( ap , BP ) , () // prepare permanent-key based AuthEncr
-  uap=secure_random() ; ua = uap || encrypt_symm( KP , uap || mt )  // authenticate temp message
-  deltaua = encrypt_symm ( HASHED_SYM_STATE.get_password() , uap )  // anti-QC!
-
-Ali -> Bob: "deltaua" // NETWORK send
-
-Bob: 
-  decrypts/checks auth: ua 
-  has AT, r1
-
-  bt,BT = Generte_DH()  // make temporary keys: at = Ali_DH_Tmp_priv , AT = Ali_DH_Tmp_Pub
-  r2 = secure_random() 
-  mt = "BT,r2"; // message with temporary key and temporary random
-
-  KP = complete_DH( bp , AP ) , // prepare permanent-key AuthEncr  
-  ubp=secure_random() ; ub = ubp || encrypt_symm( KP , ubp || mt )  // authenticate temp message
-  ... or ub = encrypt_symm( KP , ubp || ubp || mt )
-  ... or ub = encrypt_symm( KP , 'all_is_fine' || mt )
-  deltaub = encrypt_symm ( HASHED_SYM_STATE.get_password() , uab )  // anti-QC!
-
-Bob -> Alice: "deltaub" // NETWORK send
-
-Alice (and Bob similar)
-  decrypts/checks auth: ub (and decrypts with HASHED_SYM_STATE)
-  has BT, r2
-
-Now both sides calculate the same:
-  TSK = complete_DH( at , BT)  or  = complete_DH( bt , AT) // Temporary Shared Key
-  FSSSK = H( H( TSK ) || H( r1 ) || H( r2 ) )   // Forward-Secrecy Session Shared Key
-  nonce=1
-
-*** r1,r2 help MAYBE(?) when:
-attack hardness:   badluck-DH << DH-tmp , DH-perm 
-
-*/
-
-
 // For given CA (form me, to given recipient) - and given session
 class c_dhdh_state final : public c_crypto_state {
 	public:
