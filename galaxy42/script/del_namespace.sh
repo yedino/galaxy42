33b4057ad05f7a3ce123f6f2b11ba6326c6da889
Tigusoft Admin <admin@tigusoft.pl>
2016-04-25 16:27:05 +0100

    [review] of the sudo script netpriv/setcap (a bit)

--- galaxy42/script/del_namespace.sh
+++ galaxy42/script/del_namespace.sh
@@ -2,6 +2,13 @@
 group_name="$1"
 group_size="$2"
 
+# *** review *** this program was PARTIALLY reviewed and probably does not have critical flaws
+
+echo "This program will try to delete previously created namespaces of netpriv."
+echo "It will create (overwrite!) a log file here called out.log" # <--- log name
+echo "Press ENTER to continue or abort (e.g. with Ctrl-C)"
+read _
+
 if [ $# -lt 2 ]
 then
 echo "---------------------
@@ -20,7 +27,7 @@ else
 	for i in $(seq 1 $group_size)
 	do
 		
-		(sudo ip link delete "prv${group_name}${i}xH") > out.log
+		(sudo ip link delete "prv${group_name}${i}xH") > out.log # see log name above too
 		wc -l out.log
 	done
 fi
