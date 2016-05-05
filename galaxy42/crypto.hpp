a824c99937399e53fde9c8bf16c48ad4115adb91
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-05-05 12:09:54 +0000

    [doc] crypto cleanup, glossary; hash operator>

--- galaxy42/crypto.hpp
+++ galaxy42/crypto.hpp
@@ -8,6 +8,35 @@
 
 #include <sodiumpp/sodiumpp.h>
 
+/***
+Glossary:
+
+IDP - ID of user of this system (usually a multikey of many crypto public keys), that is Permanently his (see HIP).
+IDC - ID of user of this system (usually a multikey of many crypto public keys), that is Current in use by him.
+It is authorized by his IDP. In Cjdns, IDP == IDC (there is no separation).
+IDCFP - ID Current From Permanent - full chain of crypto signatures, pubkeys, etc to prove ownership from HIP and IDP to given IDC.
+
+HIP - Hash IP - the IP (here IPv6 always), that is a hash of some public key(s) - of his IDP.
+
+KCT - Key for CryptoTunnel - the most low-level, current, low-level key that is directly used for given CT crypto (authentication, and also encryption is present).
+Currently alwyas this is some Symmetrical Key.
+Created between two IDC.
+
+CT   - CryptoTunnel. Similar to CA (CryptoAuth) from Cjdns. Tunnel that is encrypted: possibly encrypted, and always authenticated.
+CTE  - CryptoTunnel Encrypted - a CT that in addition always is encrypted (and of course is authenticated)
+CTNE - CryptoTunnel NOT Encrypted - a CT that is NOT encrypted (and of course is authenticated)
+
+CT-E2E - a CT established end-to-end for transmission of data, it is of type CTE.
+CT-P2P - a CT established peer-to-peer for forwarding of data further, so it is only CTNE.
+
+HIP == IDP-h --> IDP-pub --> ID*-pub -- ... --> IDC-pub
+
+*pub - public key
+*PRIV - private key
+*PAIR - pair of pub + PRIV
+
+*/
+
 
 using namespace std;
 
@@ -25,7 +54,7 @@ Types: uses own t_types - that are probably std::string
 =====================================================================
 */
 
-// must match: t_crypto_system_type_to_name() 
+// must match: t_crypto_system_type_to_name()
 enum t_crypto_system_type : unsigned char {
 	// 0 is reserved
 	e_crypto_system_type_invalid = 0,
@@ -106,11 +135,21 @@ class c_multikeys_pub : public c_crypto_system {
 		t_cryptolists_pubkey m_cryptolists_pubkey; //< A "map" of public keys of given type, organized by their crypto type.
 		//< example use: to get 50-th of our Ed25519 keys: m_cryptolists_pubkey[ e_crypto_system_type_Ed25519 ].at(50);
 
+		mutable string m_hash_cached; ///< Hash of all my public keys (a cache, auto calculated by getters/cleared by setters)
+		// empty "" means that it needs calculation.
+		// ^- TODO invalidate by setters
+
+		void update_hash(); ///< calculate the current m_hash and save it
+
 	public:
 		void add_public(t_crypto_system_type crypto_type, const t_pubkey & pubkey);
 		t_pubkey get_public(t_crypto_system_type crypto_type, size_t number_of_key) const;
 		virtual t_crypto_system_type get_system_type() const;
-		bool operator > (const c_multikeys_pub &rhs) const;
+
+		string get_hash() const; ///< const, though it is allowed to update mutable field with cache of current hash
+
+		bool operator>(const c_multikeys_pub &rhs) const; ///< some sorting order, e.g. using the get_hash(). Used e.g. to
+		/// pick even/odd nonce depending on comparing keys.
 };
 
 /*** All PRIVATE keys of given identity */
@@ -120,8 +159,8 @@ class c_multikeys_PRIV : public c_crypto_system {
 		friend class c_crypto_system;
 
 		typedef std::array< vector< t_PRIVkey > , e_crypto_system_type_END	> t_cryptolists_PRIVkey;
-		t_cryptolists_PRIVkey m_cryptolists_PRIVkey; //< A "map" of PRIVATE keys of given type, organized by their crypto type.
-		//< example use: to get 50-th of our Ed25519 keys: m_cryptolists_PRIVkey[ e_crypto_system_type_Ed25519 ].at(50);
+		t_cryptolists_PRIVkey m_cryptolists_PRIVkey; ///< A "map" of PRIVATE keys of given type, organized by their crypto type.
+		///< example use: to get 50-th of our Ed25519 keys: m_cryptolists_PRIVkey[ e_crypto_system_type_Ed25519 ].at(50);
 
 	public:
 		void add_PRIVATE(t_crypto_system_type crypto_type,const t_PRIVkey & PRIVkey);
@@ -147,12 +186,12 @@ class c_multikeys_PAIR {
 
 
 /*** The finall crypto of the stream */
-class c_stream_crypto final /* because strange ctor init list functions */ 
-: public c_crypto_system 
+class c_stream_crypto final /* because strange ctor init list functions */
+: public c_crypto_system
 {
 	private:
-		t_symkey m_usable_key; //< UK (a.k.a. "K") is the finall key to be used on the streams
-		bool m_nonce_odd; //< is our key uneven (odd) as used in sodiumpp to decide nonce for us
+		t_symkey m_usable_key; ///< UK (a.k.a. "K") is the finall key to be used on the streams
+		bool m_nonce_odd; ///< is our key uneven (odd) as used in sodiumpp to decide nonce for us
 		// TODO lock it's memory before setting it!!!
 
 		sodiumpp::boxer< t_crypto_nonce > m_boxer;
@@ -161,28 +200,28 @@ class c_stream_crypto final /* because strange ctor init list functions */
 	public:
 		virtual t_crypto_system_type get_system_type() const;
 
-		c_stream_crypto(const c_multikeys_PAIR & self,  const c_multikeys_pub & them);
-	
+		c_stream_crypto(const c_multikeys_PAIR & IDC_self,  const c_multikeys_pub & IDC_them);
+
 	private:
 		static t_symkey calculate_usable_key(const c_multikeys_PAIR & self,  const c_multikeys_pub & them);
 		static bool calculate_nonce_odd(const c_multikeys_PAIR & self,  const c_multikeys_pub & them);
 
 };
 
-/*** Tool ready to process the stream as CryptoTunnel (like CryptoAuth from Cjdns naming) */
-class c_crypto_tunnel {
-	protected:
+/*** A CT, can be used to send data in both directions. */
+class c_crypto_tunnel final {
+	private:
 		unique_ptr<c_stream_crypto> m_stream_crypto;
 
 	public:
 		c_crypto_tunnel()=default;
-		c_crypto_tunnel(const c_multikeys_PAIR & self,  const c_multikeys_pub & them);
+		c_crypto_tunnel(const c_multikeys_PAIR & IDC_self,  const c_multikeys_pub & IDC_them);
 };
 
 c_crypto_tunnel create_crypto_tunnel(c_multikeys_PAIR & self, c_multikeys_pub & other);
 
 
-/** For given CA (form me, to given recipient) - and given session */
+/** For given CT (form me, to given recipient) - and given session */
 class c_dhdh_state final : public c_crypto_system {
 	public:
 		typedef string_as_bin t_pubkey;
