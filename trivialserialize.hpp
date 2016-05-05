1e3367a533066ef3b82049f5fb13c9379cc046c3
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-05-05 17:07:57 +0200

    Doxygen
    [add] Doxyfile, [fix] doxygen comments
    Modules, grouping examples in trivialserialize.hpp

--- trivialserialize.hpp
+++ trivialserialize.hpp
@@ -1,54 +1,70 @@
+/** @file
+ *  trivialserialice.hpp -- docs for this file
+ */
+
 #ifndef include_trivialserialize_hppaaa
 #define include_trivialserialize_hppaaa
 
 #include "libs1.hpp"
-
 // yeap defines are not so nice but also remove some problems with global constants re init ordering for example
 // TODO constexpr?
 #define SANE_MAX_SIZE_FOR_STRING 50000000
 
 using namespace std;
 
+/**
+ * @defgroup trivialserialize Trivial Serialize
+ * Description: ...
+ */
 namespace trivialserialize {
 
 void test_trivialserialize();
 
 // ---
+/** @defgroup format_error The Format Error
+ *  Detailed Description: one description for all members of this group
+ *  @{
+ */
 
+/** @page format_err_page1 This is a first section in format_error group
+ *  Text of the first section
+ */
+
+/** @page format_err_page2 This is a second section in format_error group
+ *  Text of the second section
+ */
 
 class format_error : public std::exception { public:	const char * what() const noexcept override; };
-
 class format_error_read : public format_error { public:	const char * what() const noexcept override; };
 
 class format_error_read_badformat : public format_error { public:	const char * what() const noexcept override; };
 
 class format_error_read_delimiter : public format_error { public:	const char * what() const noexcept override; };
 
-
 class format_error_write : public format_error { public:	const char * what() const noexcept override; };
 
 class format_error_write_too_long : public format_error_write { public:	const char * what() const noexcept override; };
 
 class format_error_write_value_too_big : public format_error { public:	const char * what() const noexcept override; };
+/** @} */	// end of format_error group
 
 
-/***
-@brief Generates the serialized data string, from input data like bytes and strings of various types, there are lower and higher
-level APIs in groups of functions.
-@note About string types, following are possible:
-- conststr: strings of constant length of N characters (during read, and during write, the caller knows the size is exactyl N)
-- octetsvarstr: We support strings that length is variable, but it is known (during read, and write, the caller tells us) to which string-length the string belongs:
--- is it a string with length <= 2^(8*S) -1 for S=1 (using 1 octet to encode own width)
--- is it a string with length <= 2^(8*S) -1 for S=2
--- is it a string with length <= 2^(8*S) -1 for S=3
--- is it a string with length <= 2^(8*S) -1 for S=4
--- If the program tells the string has S=3 but actuall data is always shorter then e.g. 256, so would fit in S=1, then this is fine of course, though.
-always there is wasted space (e.g. of 1 byte) in the serialized format.
-- varstring: see push_varstring() - can save string of any length, and the width is stored in dynamic format (but
-for tiny stings especially of widths <=255 other options are slghtly better)
-- cstring: we COULD have null-delimited string. This option probably will not be implemented (as the string would need to be
-escaped or at least asserted on save)
-*/
+/**
+ * @ingroup trivialserialize
+ * @nosubgrouping
+ * @brief Generates the serialized data string, from input data like bytes and strings of various types.
+ * @note About string types, following are possible:
+ * - conststr: strings of constant length of N characters (during read, and during write, the caller knows the size is exactyl N)
+ * - octetsvarstr: We support strings that length is variable, but it is known (during read, and write, the caller tells us) to which string-length the string belongs:
+ * -- is it a string with length <= 2^(8*S) -1 for S=1 (using 1 octet to encode own width)
+ * -- is it a string with length <= 2^(8*S) -1 for S=2
+ * -- is it a string with length <= 2^(8*S) -1 for S=3
+ * -- is it a string with length <= 2^(8*S) -1 for S=4
+ * -- If the program tells the string has S=3 but actuall data is always shorter then e.g. 256, so would fit in S=1, then this is fine of course, though.
+ * always there is wasted space (e.g. of 1 byte) in the serialized format.
+ * - varstring: we COULD have one that uses 4 bytes to encode length when it is long, but uses just 1 byte when it is e.g. 250; bitcoin uses such mechanism. Not implemented (yet?).
+ * - cstring: we COULD have null-delimited string. Not implemented (yet?).
+ */
 class generator {
 	protected:
 		std::string m_str; ///< the generated data so far
@@ -63,50 +79,64 @@ class generator {
 	public:
 		generator(size_t suggested_size);
 
-		/// === static interface - you need to specify exact (or maximum) data size youself ===
-
+		/** @name Static Interface
+		 * Description: you need to specify exact (or maximum) data size youself
+		 */
+		///@{
 		void push_byte_u(unsigned char c);
 		void push_byte_s(signed char c);
 
 		template <int S, typename T> void push_integer_u(T value); ///< Saves some unsigned integer-type S, into field that is S octets wide.
 		template <int S, typename T> void push_integer_s(T value); ///< Saves some   signed integer-type S, into field that is S octets wide.
 
-		/***
-		@brief writes conststr (see class notes).
-		@note Supported data size is 0 to 2^32 -1.
-		@note Serialized size will be <= the data size.
-		@param size - the defined exact size of data. We require (and assert) that it == data.size(). It is used ONLY for asserting.
-		@param data - the string of binary data
-		*/
+		/**
+		 * @brief writes conststr (see class notes).
+		 * @note Supported data size is 0 to 2^32 -1.
+		 * @note Serialized size will be <= the data size.
+		 * @param size - the defined exact size of data. We require (and assert) that it == data.size(). It is used ONLY for asserting.
+		 * @param data - the string of binary data
+		 */
 		void push_bytes_n(size_t size, const std::string & data);
 
-		/***
-		@brief writes octetsvarstr (see class notes).
-		@note Supported data size is 0 to 2^32 -1 in general, and for given S it is 2^(8*S)-1
-		@note Serialized size will be <= the data size + S
-		@param S - the numbers of octets needed to express the maximum possible size of string. String can have at most (2^(8*S)-1) characters.
-		@param data - the string of binary data.
-		@param max_size_for_assert - the expected maximum size of data. It can be lower, e.g. S=1 but we want to assert string is <=100 characters. Is asserted.
-		*/
+		/**
+		 * @brief writes octetsvarstr (see class notes).
+		 * @note Supported data size is 0 to 2^32 -1 in general, and for given S it is 2^(8*S)-1
+		 * @note Serialized size will be <= the data size + S
+		 * @param S - the numbers of octets needed to express the maximum possible size of string. String can have at most (2^(8*S)-1) characters.
+		 * @param data - the string of binary data.
+		 * @param max_size_for_assert - the expected maximum size of data. It can be lower, e.g. S=1 but we want to assert string is <=100 characters. Is asserted.
+		 */
 		template <int S> void push_bytes_sizeoctets(const std::string & data, size_t max_size_for_assert);
-		template <int S> void push_bytes_sizeoctets(const std::string & data); //< same as push_bytes_sizeoctets with max_size_for_assert, except that argument is not used (and this additional assert is not done)
+		/// same as push_bytes_sizeoctets with max_size_for_assert, except that argument is not used (and this additional assert is not done)
+		template <int S> void push_bytes_sizeoctets(const std::string & data);
+		///@}
 
-		/// === dynamic interface - easy handling data of any runtime size without thinking about it yourself ===
-
+		/** @name Dynamic Interface
+		 * Description: easy handling data of any runtime size without thinking about it yourself
+		 */
+		///@{
 		void push_integer_uvarint(uint64_t val); ///< Encode unsigned int dynamically on 1,3,5,9 octets like Bitcoin's CompactSize
 
 		void push_varstring(const std::string &data); ///< Encode entire string of any length (but < max uint64) in dynamic format.
-		
-		/// === high level interface - handling data structures ===
+		///@}
 
+		/** @name High level Interface
+		 * Description: handling data structures
+		 */
+		///@{
 		void push_vector_string(const vector<string> & data); ///< Save vector<string>
+		///@}
 
-		/// === export the result ===
-
+		/** @name Export The Result
+		 * Description: export the result
+		 */
+		///@{
 		const std::string & str() const; ///< get the generated string. This can be INVALIDATED by any non-const operations of this object (and ofc. after object expires)!
+		///@}
 
 	protected:
-		void push_bytes_octets_and_size(unsigned char octets, size_t max_size, const std::string & data); ///< give number of octets of actuall-data-size, give the max_size that is just asserted, and the data
+		/// give number of octets of actuall-data-size, give the max_size that is just asserted, and the data
+		void push_bytes_octets_and_size(unsigned char octets, size_t max_size, const std::string & data);
 };
 
 template <int S, typename T> void generator::push_integer_u(T value) {
@@ -151,14 +181,14 @@ template <int S> void generator::push_bytes_sizeoctets(const std::string & data)
 }
 
 
-
-
-/***
-@brief For prasing serialized data. It must ncelly THROW in case if input is in format other then expected, e.g. trying to read more data then possible.
-It absolutelly must at least assert all possible internal errors resulting from input that is logically invalid in any way (though of course
-it must be technically valid, e.g. not nullptr string).
-And this asserts must be always-on, because this is for parsing unsafe external data. TODO(r) abort() that can not be disabled
-*/
+/**
+ * @ingroup trivialserialize
+ * @nosubgrouping
+ * @brief For prasing serialized data. It must ncelly THROW in case if input is in format other then expected, e.g. trying to read more data then possible.
+ * It absolutelly must at least assert all possible internal errors resulting from input that is logically invalid in any way (though of course
+ * it must be technically valid, e.g. not nullptr string).
+ * And this asserts must be always-on, because this is for parsing unsafe external data. TODO(r) abort() that can not be disabled
+ */
 class parser {
 	public:
 		struct tag_caller_must_keep_this_string_valid {} ;
@@ -172,8 +202,10 @@ class parser {
 		parser( tag_caller_must_keep_this_string_valid x , const std::string & data_str);
 		parser( tag_caller_must_keep_this_buffer_valid x , const char * buf , size_t size);
 
-		/// === static interface - you need to specify exact (or maximum) data size youself ===
-
+		/**	@name Static Interface
+		 * Description: you need to specify exact (or maximum) data size youself
+		 */
+		///@{
 		unsigned char pop_byte_u();
 		signed char pop_byte_s();
 		void pop_byte_skip(char c); // read expected character (e.g. a delimiter)
@@ -182,20 +214,25 @@ class parser {
 		template <int S, typename T> T pop_integer_u(); ///< Reads some unsigned integer-type S, into field that is S octets wide, as value of type T.
 		template <int S, typename T> T pop_integer_s(); ///< Reads some   signed integer-type S, into field that is S octets wide, as value of type T.
 
-		std::string pop_bytes_n(size_t size); //< Read and return binary string of exactly N characters always, 
-		//< that was saved using push_bytes_n(). N can be 0.
+		std::string pop_bytes_n(size_t size); ///< Read and return binary string of exactly N characters always, that was saved using push_bytes_n()
 
 		template <int S> std::string pop_bytes_sizeoctets();
-		
-		/// === dynamic interface - easy handling data of any runtime size without thinking about it yourself ===
+		///@}
 
+		/** @name Dynamic Interface
+		 *  Description: easy handling data of any runtime size without thinking about it yourself
+		 */
+		///@{
 		uint64_t pop_integer_uvarint(); ///< Decode unsigned int of 1,3,5,9 octets saved by push_integer_uvarint
 		std::string pop_varstring(); ///< Decode string of any length saved by push_varstring()
+		///@}
 
-		/// === high level interface - handling data structures ===
-
+		/** @name High Level Interface
+		 * Description: handling data structures
+		 */
+		///@{
 		vector<string> pop_vector_string(); ///< Decode a vector<string> object saved with push_vector_string
-
+		///@}
 };
 
 
