3272206e8d8b3057a4a4aed0d4399d9f9c9e47be
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-05-16 17:05:50 +0000

    [wip] (serialization template problem)

--- galaxy42/glue_lockedstring_trivialserialize.hpp
+++ galaxy42/glue_lockedstring_trivialserialize.hpp
@@ -12,8 +12,16 @@ void memlock(const trivialserialize::generator & gen) {
 	sodiumpp::mlock( const_cast<std::string&>(gen.get_buffer()) ); // TODO(rob) const string memlock
 }
 
-template <> void trivialserialize::serialize(const sodiumpp::locked_string & data, trivialserialize::generator & gen);
-template <> sodiumpp::locked_string trivialserialize::deserialize<sodiumpp::locked_string>(trivialserialize::parser & parser);
+template <> void trivialserialize::serialize(const sodiumpp::locked_string & data, trivialserialize::generator & gen) {
+	UNUSED(data); UNUSED(gen);
+
+}
+
+template <> sodiumpp::locked_string trivialserialize::deserialize<sodiumpp::locked_string>(trivialserialize::parser & parser) {
+	UNUSED(parser);
+	sodiumpp::locked_string ret;
+	return ret;
+}
 
 #endif
 
