d1d9d70a92682c0931e69432e1a3b2cdbe8619eb
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-05-26 10:07:43 +0200

    [wip] tnrupp wrapper

--- ntrucpp.cpp
+++ ntrucpp.cpp
@@ -1,6 +1,1 @@
 #include "ntrucpp.hpp"
-
-ntrucpp::ntrucpp()
-{
-
-}
--- ntrucpp.hpp
+++ ntrucpp.hpp
@@ -16,7 +16,7 @@ extern "C" {
 namespace ntrupp {
 
 	/// @return pair of <private key, public_key + hash_sha512(private_key) >
-	/// pricate_key hash at the end of publickey is necessary for verifying signatures
+	/// pricate_key hash before publickey is necessary for verifying signatures
 	std::pair<sodiumpp::locked_string,std::string> generate_keypair() {
 
 		// generate key pair
@@ -35,7 +35,7 @@ namespace ntrupp {
 		assert(private_key_len == 659);
 
 		std::string public_key(public_key_len, 0);
-		locked_string private_key(private_key_len);
+		sodiumpp::locked_string private_key(private_key_len);
 
 		NTRU_exec_or_throw(
 			ntru_crypto_ntru_encrypt_keygen(get_DRBG(128), NTRU_EES439EP1,
@@ -51,11 +51,14 @@ namespace ntrupp {
 
 		public_key += std::string(reinterpret_cast<const char>(hash));
 
-		assert(public_key.size() == (public_key_len + HASH_BYTES)
+
+		std::string public_and_hash(public_key + std::string(reinterpret_cast<const char>(hash)),
+									public_key_len + HASH_BYTES);
+
+		assert(public_and_hash.size() == (public_key_len + HASH_BYTES)
 			   && "Ntru public_key + hash: BAD sizes" );
 
-
-		return std::make_pair(std::move(private_key), std::move(public_key));
+		return std::make_pair(std::move(private_key), std::move(public_and_hash));
 	}
 
 	std::string sign(const std::string &msg,
@@ -75,10 +78,10 @@ namespace ntrupp {
 		::sign(hash,
 			   z,
 			   private_key.get_string(),
-			   reinterpret_cast<const unsigned char *>(msg.data()),
+			   reinterpret_cast<const unsigned char *>(msg.c_str()),
 			   msg.size());
 
-		std::string signature(reinterpret_cast<const char *>(z));
+		std::string signature(reinterpret_cast<const char *>(z), PASS_N);
 
 		ntt_cleanup();
 
@@ -87,16 +90,16 @@ namespace ntrupp {
 
 	void verify(const std::string &sign,
 				const std::string &msg,
-				const std::string &pubkey) {
+				const std::string &public_key) {
 
 		if(ntt_setup() == -1) {
 			throw std::runtime_error("ERROR: Could not initialize FFTW. Bad wisdom?");
 		}
 
-		::verify()
+//		std::string public_key = pubkey.substr()
+//		::verify()
 
-
-		ntt_cleanup();
+//		ntt_cleanup();
 	}
 
 }	// namespace ntrucpp
