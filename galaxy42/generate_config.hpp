c4bf69e04adee158708cd2fa5f370ed5f3194b64
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-06-01 19:56:04 +0200

    little changes in filestorage generate_config

--- galaxy42/generate_config.hpp
+++ galaxy42/generate_config.hpp
@@ -1,6 +1,7 @@
 #ifndef GENERATE_CONFIG_HPP
 #define GENERATE_CONFIG_HPP
 
+#include "libs0.hpp"
 #include "filestorage.hpp"
 #include "crypto/crypto.hpp"
 
@@ -17,9 +18,8 @@ enum class e_crypto_set : unsigned char {
 class generate_config {
 public:
 	generate_config() = delete;
-
 	static void crypto_set (e_crypto_set cryptoset);
-
+	static std::string m_crypto_set_name;
 };
 
 #endif // GENERATE_CONFIG_HPP
