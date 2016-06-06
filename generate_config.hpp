eb698498915ef10eb690f50a7f12bcafc24898c6
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-06-06 15:05:00 +0200

    crypto_current crypto_permanent

--- generate_config.hpp
+++ generate_config.hpp
@@ -6,7 +6,7 @@
 #include "crypto/crypto.hpp"
 
 
-enum class e_crypto_set : unsigned char {
+enum class e_crypto_strength : unsigned char {
 	lowest = 0,
 	fast,
 	normal,
@@ -19,8 +19,26 @@ enum class e_crypto_set : unsigned char {
 class generate_config {
 public:
 	generate_config() = delete;
-	static void crypto_set (e_crypto_set cryptoset);
-	static std::string m_crypto_set_name;
+
+	/**
+	 * @brief crypto_permanent Create set of permanent keys with ability to sign
+	 * @param strength
+	 */
+	static void crypto_permanent (e_crypto_strength strength);
+	/**
+	 * @brief crypto_current Create set of current keys with ability to make DN exchange
+	 * @param strength
+	 */
+	static void crypto_current (e_crypto_strength strength);
+
+	static std::string m_crypto_permanent_name;
+	static std::string m_crypto_current_name;
+
+	static std::string get_crypto_permanent_name();
+	static std::string get_crypto_current_name();
+
+	static void set_crypto_permanent_name(const std::string &crypto_permanent_name);
+	static void set_crypto_current_name(const std::string &crypto_current_name);
 };
 
 #endif // GENERATE_CONFIG_HPP
