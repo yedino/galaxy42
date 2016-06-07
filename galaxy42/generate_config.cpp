a89e06250331e0bc406d32205e870236e2e98f1c
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-06-07 14:20:07 +0200

    t_crypto_system_type_from_string, [fix] generate_config

--- galaxy42/generate_config.cpp
+++ galaxy42/generate_config.cpp
@@ -1,6 +1,6 @@
 #include "generate_config.hpp"
 
-void generate_config::crypto_sign(e_crypto_strength strength) {
+void generate_config::crypto_sign(const std::string &filename, e_crypto_strength strength) {
 
 	antinet_crypto::c_multikeys_PAIR keypair;
 
@@ -17,11 +17,10 @@ void generate_config::crypto_sign(e_crypto_strength strength) {
 	case e_crypto_strength::lowest:
 		keypair.generate(antinet_crypto::e_crypto_system_type_Ed25519, 1);
 	}
-	keypair.datastore_save_PRV_and_pub(generate_config::m_crypto_sign_name);
-
+	keypair.datastore_save_PRV_and_pub(filename);
 }
 
-void generate_config::crypto_exchange(e_crypto_strength strength) {
+void generate_config::crypto_exchange(const std::string &filename, e_crypto_strength strength) {
 
 	antinet_crypto::c_multikeys_PAIR keypair;
 
@@ -38,24 +37,16 @@ void generate_config::crypto_exchange(e_crypto_strength strength) {
 	case e_crypto_strength::lowest:
 		keypair.generate(antinet_crypto::e_crypto_system_type_X25519, 1);
 	}
-	keypair.datastore_save_PRV_and_pub(generate_config::m_crypto_exchange_name);
+	keypair.datastore_save_PRV_and_pub(filename);
 }
 
-std::string generate_config::m_crypto_sign_name = "";
-std::string generate_config::m_crypto_exchange_name = "";
+void generate_config::any_crypto_set(const std::string &filename,
+									 const std::vector<std::pair<antinet_crypto::t_crypto_system_type, int> > &keys) {
 
-std::string generate_config::get_crypto_exchange_name() {
-	return m_crypto_exchange_name;
-}
+	antinet_crypto::c_multikeys_PAIR keypair;
 
-void generate_config::set_crypto_exchange_name(const std::string &crypto_exchange_name) {
-	m_crypto_exchange_name = crypto_exchange_name;
-}
-
-std::string generate_config::get_crypto_sign_name() {
-	return m_crypto_sign_name;
-}
-
-void generate_config::set_crypto_sign_name(const std::string &crypto_sign_name) {
-	m_crypto_sign_name = crypto_sign_name;
+	for (auto &type_and_count : keys) {
+		keypair.generate(type_and_count.first, type_and_count.second);
+	}
+	keypair.datastore_save_PRV_and_pub(filename);
 }
