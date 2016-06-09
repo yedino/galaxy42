b0ff01e563f37241cfe3c17f040d4876533dfd6b
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-06-09 15:39:25 +0200

    --gen --sign --verify should works
    examples in: gen-key-demo.sh

--- galaxy42/gen-key-demo.sh
+++ galaxy42/gen-key-demo.sh
@@ -40,24 +40,63 @@
 # new api
 
 # generate
-# ./nocap-tunserver.elf --gen-key --new-key "myself" --key-type "ed25519:x3"
-# ./nocap-tunserver.elf --gen-key --new-key "bank" --key-type "ed25519:x1" "ntru_sign:x2"
-# ./nocap-tunserver.elf --gen-key --new-key "shadyguy" --key-type "ed25519:x1"
-# ./nocap-tunserver.elf --gen-key --new-key-file "myself" --key-type "ed25519:x3"
-# ./nocap-tunserver.elf --gen-key --new-key "tester" --key-type "ed25519:x1" "ntru_sign:x1"
+ ./nocap-tunserver.elf --gen-key --new-key "myself" --key-type "ed25519:x3"
+ ./nocap-tunserver.elf --gen-key --new-key "bank" --key-type "ed25519:x1" "ntru_sign:x2"
+ ./nocap-tunserver.elf --gen-key --new-key "shadyguy" --key-type "ed25519:x1"
+ ./nocap-tunserver.elf --gen-key --new-key-file "myself" --key-type "ed25519:x1"
+ ./nocap-tunserver.elf --gen-key --new-key "tester" --key-type "ed25519:x1" "ntru_sign:x1"
 					# generating keys - means generate pair (two files) newkey.pub and newkey.PRV
-# ./nocap-tunserver.elf --gen-key --new-key-file "newkey" --key-type "ed25519:x1"
+ ./nocap-tunserver.elf --gen-key --new-key-file "newkey" --key-type "ed25519:x1"
 # sign 
-# ./nocap-tunserver.elf --sign --my-key "myself" --sign-key "tester"
-# ./nocap-tunserver.elf --sign --my-key "myself" --sign-key-file "newkey.pub"
+ ./nocap-tunserver.elf --sign --my-key "myself" --sign-key "tester"
+ ./nocap-tunserver.elf --sign --my-key "myself" --sign-key-file "newkey.pub"
 
-# ./nocap-tunserver.elf --sign --my-key-file "myself.PRV" --sign-key-file "newkey.pub"
-# ./nocap-tunserver.elf --sign --my-key "tester" --sign-data-file "tunserver.elf"
+ ./nocap-tunserver.elf --sign --my-key-file "myself.PRV" --sign-key-file "newkey.pub"
+ ./nocap-tunserver.elf --sign --my-key-file "myself.PRV" --sign-data-file "tunserver.elf"
 
 ./nocap-tunserver.elf --sign --my-key "bank" --sign-key "shadyguy"
 
 # verify
 
 ./nocap-tunserver.elf --verify --trusted-key "bank" --toverify-key "shadyguy"
+./nocap-tunserver.elf --verify --trusted-key-file "myself.pub" --toverify-data-file "tunserver.elf"
+# extern signature
 ./nocap-tunserver.elf --verify --trusted-key-file "myself.pub" --toverify-data-file "tunserver.elf" --signature-file "tunserver.elf.sig"
 
+# clean
+echo "Cleaning ..."
+echo "removing: ~/.config/antinet/galaxy42/wallet/bank.PR"
+rm ~/.config/antinet/galaxy42/wallet/bank.PRV
+echo "removing: ~/.config/antinet/galaxy42/wallet/myself.PRV"
+rm ~/.config/antinet/galaxy42/wallet/myself.PRV
+echo "removing: ~/.config/antinet/galaxy42/wallet/shadyguy.PRV"
+rm ~/.config/antinet/galaxy42/wallet/shadyguy.PRV
+echo "removing: ~/.config/antinet/galaxy42/wallet/tester.PRV"
+rm ~/.config/antinet/galaxy42/wallet/tester.PRV
+
+echo "removing: ~/.config/antinet/galaxy42/public/bank.pub"
+rm ~/.config/antinet/galaxy42/public/bank.pub
+echo "removing: ~/.config/antinet/galaxy42/public/myself.pub"
+rm ~/.config/antinet/galaxy42/public/myself.pub
+echo "removing: ~/.config/antinet/galaxy42/public/shadyguy.pub"
+rm ~/.config/antinet/galaxy42/public/shadyguy.pub
+echo "removing: ~/.config/antinet/galaxy42/public/tester.pub"
+rm ~/.config/antinet/galaxy42/public/tester.pub
+
+echo "removing: ~/.config/antinet/galaxy42/public/shadyguy.pub.sig"
+rm ~/.config/antinet/galaxy42/public/shadyguy.pub.sig
+echo "removing: ~/.config/antinet/galaxy42/public/tester.pub.sig"
+rm ~/.config/antinet/galaxy42/public/tester.pub.sig
+
+echo "removing: myself.pub"
+rm myself.pub
+echo "removing: myself.PRV"
+rm myself.PRV
+echo "removing: newkey.pub"
+rm newkey.pub
+echo "removing: newkey.pub.sig"
+rm newkey.pub.sig
+echo "removing: newkey.PRV"
+rm newkey.PRV
+echo "removing: tunserver.elf.sig"
+rm tunserver.elf.sig
