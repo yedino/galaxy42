3ae6c94fd242e1b9f5fd4b14af16853fe04687e9
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-06-22 15:39:42 +0000

    xint: more UTs for incr/decr

--- src/test/xint.cpp
+++ src/test/xint.cpp
@@ -3,10 +3,8 @@
 #include "../libs1.hpp"
 #include <exception>
 #include <cmath>
+#include <type_traits>
 
-//		ASSERT_EQ(parser.pop_integer_uvarint(), i);
-//	ASSERT_THROW(parser.pop_varstring(), std::exception);
-//	EXPECT_NO_THROW(test::test_trivialserialize());
 
 namespace test_xint {
 namespace detail {
@@ -14,6 +12,24 @@ namespace detail {
 
 typedef long double t_correct1;
 
+typedef boost::multiprecision::number<
+	boost::multiprecision::cpp_int_backend<1024, 65536,
+		boost::multiprecision::signed_magnitude, boost::multiprecision::checked, void> >
+t_correct_int;
+
+/*
+template<typename T_INT,
+typename std::enable_if<std::is_signed<T_INT>{}>::type* = nullptr >
+signed long long int round_co(t_correct1 v) {
+	return static_cast<signed long long int>( std::round( v ) );
+}
+
+template<typename T_INT,
+typename std::enable_if<! std::is_signed<T_INT>{}>::type* = nullptr >
+unsigned long long int round_co(t_correct1 v) {
+	return static_cast<unsigned long long int>( std::round( v ) );
+}
+*/
 template<typename T_INT>
 void math_tests_noproblem() {
 	//_mark("Doing tests, on integral with sizeof="<<sizeof(T_INT));
@@ -23,12 +39,9 @@ void math_tests_noproblem() {
 		T_INT a=0;
 		int n=testsize;
 		for (int i=1; i<n; ++i) a = a + i;
-
-		t_correct1 n_co = n;
-		t_correct1 a_co = (((n_co-1)*1)/2) * n_co;
-		EXPECT_EQ(a, a_co);
-		T_INT a_co_round = std::round(a_co);
-		EXPECT_EQ(a, a_co_round);
+		t_correct_int n_corr = n;
+		t_correct_int a_corr = (((n_corr-1)*1)* n_corr) /2;
+		EXPECT_EQ(a, a_corr);
 	}
 }
 
@@ -41,30 +54,95 @@ template <typename T_INT> bool is_safe_int() {
 }
 
 template<typename T_INT>
-void math_tests_overflow(T_INT val) { bool safetype = is_safe_int<T_INT>();
-	T_INT a = val;	t_correct1 a_co = a;
-	EXPECT_NO_THROW( { a++;  a_co+=1; } );  EXPECT_EQ(a,a_co); // this should fit for given starting val
+void math_tests_overflow_incr(T_INT val) { bool safetype = is_safe_int<T_INT>();
+	T_INT a = val;	t_correct_int a_corr = a;
+	auto func = [&]() { a_corr+=1; a++; } ;
+	#define db do { _mark("safe="<<safetype<<"; a="<<a<<" a_corr="<<a_corr); } while(0)
+
+	EXPECT_NO_THROW( func() );
+	EXPECT_EQ(a,a_corr); // this should fit for given starting val
 	// next icrement is problematic:
-	if (safetype) {
-		EXPECT_THROW( { a++;	a_co+=1; } , std::runtime_error );
-	} else { // should not fit for given val
-		EXPECT_NO_THROW( { a++;	a_co+=1; } ); // usafe type fils to throw
-		EXPECT_NE(a,a_co); // unsafe type has mathematically-invalid value
+	if (safetype) { EXPECT_THROW( func() , std::runtime_error ); }
+	else { // should not fit for given val
+		EXPECT_NO_THROW( func() ); // usafe type fils to throw
+		EXPECT_NE(a , a_corr); // unsafe type has mathematically-invalid value
 	}
+	#undef db
+}
+
+template<typename T_INT>
+void math_tests_overflow_decr(T_INT val) { bool safetype = is_safe_int<T_INT>();
+	T_INT a = val;	t_correct_int a_corr = a;
+	auto func = [&]() { a_corr-=1; a--; } ;
+	#define db do { _mark("safe="<<safetype<<"; a="<<a<<" a_corr="<<a_corr); } while(0)
+	db;
+
+	EXPECT_NO_THROW( func() );
+	EXPECT_EQ(a,a_corr); // this should fit for given starting val
+	// next icrement is problematic:
+	if (safetype) { db; EXPECT_THROW( func() , std::runtime_error ); db; }
+	else { // should not fit for given val
+		EXPECT_NO_THROW( func() ); // usafe type fils to throw
+		EXPECT_NE(a , a_corr); // unsafe type has mathematically-invalid value
+	}
+	#undef db
 }
 
 } // namespace
 } // namespace
 
+
+// TODO move to general gtest_examples_test.cpp later (and rename from "xint")
+TEST(xint, gtest_throw) {
+	EXPECT_THROW( { throw std::runtime_error("testerror"); } , std::runtime_error );
+}
+TEST(xint, gtest_nothrow) {
+	EXPECT_NO_THROW( { int a=5; int b=10; int c=55; if (c<a+b) throw std::runtime_error("testerror"); } );
+}
+
+TEST(xint, gtest_throw_lambda) {
+	// EXPECT_NO_THROW( { int a=5, int b=10, int c=55; if (c>a+b) throw std::runtime_error("testerror"); } );
+	auto func = []() { int a=5, b=10, c=55; if (c>a+b) throw std::runtime_error("testerror"); } ;
+	EXPECT_THROW( func() , std::runtime_error );
+}
+TEST(xint, gtest_nothrow_lambda) {
+	// EXPECT_NO_THROW( { int a=5, b=10, c=55; if (c>a+b) throw std::runtime_error("testerror"); } );
+	auto func = []() { int a=5, b=10, c=55; if (c<a+b) throw std::runtime_error("testerror"); } ;
+	EXPECT_NO_THROW( func() );
+}
 
 TEST(xint, math1) {
 	test_xint::detail::math_tests_noproblem<long int>();
 	test_xint::detail::math_tests_noproblem<unsigned long int>();
 	test_xint::detail::math_tests_noproblem<xint>();
+	test_xint::detail::math_tests_noproblem<uxint>();
 }
 
-TEST(xint, overflow_basic) {	test_xint::detail::math_tests_overflow<uint32_t>(0xFFFFFFFE); }
-TEST(xint, overflow_xint) {	test_xint::detail::math_tests_overflow<xint>(0xFFFFFFFE); }
-TEST(xint, overflow_basic_unsign) {	test_xint::detail::math_tests_overflow<int32_t>(0xFFFFFFFF/2-1); }
-TEST(xint, overflow_xint_unsign) {	test_xint::detail::math_tests_overflow<uxint>(0xFFFFFFFF/2-1); }
+//const test_xint::detail::t_correct_int maxni = 0xFFFFFFFFFFFFFFFF; // max "normal integer" on this platform
+#define maxni 0xFFFFFFFFFFFFFFFFULL // max "normal integer" on this platform
+
+
+//static_assert(maxni == std::numeric_limits<uint64_t>::max() , "Unexpected max size of normal integer");
+
+#define generate_tests_for_types(FUNCTION, V1,V2,V3,V4) \
+TEST(xint, FUNCTION ## _u_i) {	test_xint::detail::math_tests_ ## FUNCTION <uint64_t>(V1); } \
+TEST(xint, FUNCTION ## _u_xint) {	test_xint::detail::math_tests_ ## FUNCTION <uxint>(V2); } \
+TEST(xint, FUNCTION ## _s_i) {	test_xint::detail::math_tests_ ## FUNCTION <int64_t>(V3); } \
+TEST(xint, FUNCTION ## _s_xint) {	test_xint::detail::math_tests_ ## FUNCTION <xint>(V4); }
+// we use max_u64-1 for SIGNED xint too, because it can in fact express it it seems?
+
+generate_tests_for_types( overflow_incr , maxni-1, maxni-1, maxni/2-1, maxni-1 )
+// generate_tests_for_types( overflow_decr , +1, +1, -(maxni/2), -maxni )
+
+
+
+#undef maxni
+
+
+
+
+
+
+
+
 
