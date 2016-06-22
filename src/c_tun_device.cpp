47410cd5d5069b665f43fc3badfc4ff05e1dfca4
Robert Oleksy <robertoleksy6@gmail.com>
2016-06-22 11:02:39 +0000

    set tun ip address

--- src/c_tun_device.cpp
+++ src/c_tun_device.cpp
@@ -4,9 +4,10 @@
 #ifdef __linux__
 //#include <sys/types.h>
 //#include <sys/stat.h>
+#include <fcntl.h>
 #include <linux/if_tun.h>
 #include <net/if.h>
-#include <fcntl.h>
+#include <sys/ioctl.h>
 #include "cpputils.hpp"
 #include "cjdns-code/NetPlatform.h"
 c_tun_device_linux::c_tun_device_linux()
@@ -23,6 +24,8 @@ void c_tun_device_linux::set_ipv6_address
 	as_zerofill< ifreq > ifr; // the if request
 	ifr.ifr_flags = IFF_TUN; // || IFF_MULTI_QUEUE; TODO
 	strncpy(ifr.ifr_name, "galaxy%d", IFNAMSIZ);
+	auto errcode_ioctl =  ioctl(m_tun_fd, TUNSETIFF, static_cast<void *>(&ifr));
+	if (errcode_ioctl < 0) throw std::runtime_error("ioctl error");
 	NetPlatform_addAddress(ifr.ifr_name, binary_address.data(), prefixLen, Sockaddr_AF_INET6);
 }
 
