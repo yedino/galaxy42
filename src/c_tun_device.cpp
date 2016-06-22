4e49273512d6cde89a73d76f8f27e02a42f9426d
Robert Oleksy <robertoleksy6@gmail.com>
2016-06-22 10:23:59 +0000

    [wip] c_tun_device_linux

---
+++ src/c_tun_device.cpp
@@ -0,0 +1,34 @@
+#include "c_tun_device.hpp"
+#include <cassert>
+
+#ifdef __linux__
+//#include <sys/types.h>
+//#include <sys/stat.h>
+#include <linux/if_tun.h>
+#include <net/if.h>
+#include <fcntl.h>
+#include "cpputils.hpp"
+#include "cjdns-code/NetPlatform.h"
+c_tun_device_linux::c_tun_device_linux()
+:
+	m_tun_fd(open("/dev/net/tun", O_RDWR))
+{
+	assert(! (m_tun_fd<0) ); // TODO throw?
+}
+
+void c_tun_device_linux::set_ipv6_address
+	(const std::array<uint8_t, 16> &binary_address, int prefixLen) {
+	assert(binary_address[0] == 0xFD);
+	assert(binary_address[1] == 0x42);
+	as_zerofill< ifreq > ifr; // the if request
+	ifr.ifr_flags = IFF_TUN; // || IFF_MULTI_QUEUE; TODO
+	strncpy(ifr.ifr_name, "galaxy%d", IFNAMSIZ);
+	NetPlatform_addAddress(ifr.ifr_name, binary_address.data(), prefixLen, Sockaddr_AF_INET6);
+}
+
+void c_tun_device_linux::set_mtu(uint32_t mtu) {
+	// TODO
+	assert(false);
+}
+
+#endif
---
+++ src/c_tun_device.hpp
@@ -0,0 +1,34 @@
+#ifndef C_TUN_DEVICE_HPP
+#define C_TUN_DEVICE_HPP
+
+#include <array>
+#include <string>
+
+/**
+ * @brief The c_tun_device class
+ * Abstract class to represent tun device
+ */
+class c_tun_device {
+	public:
+		virtual ~c_tun_device() = default;
+		virtual void set_ipv6_address
+			(const std::array<uint8_t, 16> &binary_address, int prefixLen) = 0;
+		virtual void set_mtu(uint32_t mtu) = 0;
+};
+
+#ifdef __linux__
+
+class c_tun_device_linux final : public c_tun_device {
+	public:
+		c_tun_device_linux();
+		void set_ipv6_address
+			(const std::array<uint8_t, 16> &binary_address, int prefixLen) override;
+		void set_mtu(uint32_t mtu);
+
+	private:
+		int m_tun_fd;
+};
+
+#endif
+
+#endif // C_TUN_DEVICE_HPP
