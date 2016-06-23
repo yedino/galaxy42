29abac8352da87e160448659e668947635d41a0a
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-06-23 18:13:05 +0000

    [bad] detect specialized number for op< safer_int

--- src/xint.hpp
+++ src/xint.hpp
@@ -27,10 +27,29 @@ typedef boost::multiprecision::number<
 		boost::multiprecision::unsigned_magnitude, boost::multiprecision::checked, void> >
 uxbigint;
 
+
+
+typedef boost::multiprecision::number<
+	boost::multiprecision::cpp_int_backend<1024, 65536,
+		boost::multiprecision::signed_magnitude, boost::multiprecision::checked, void> >
+t_correct_int;
+
+
+// TODO move to lib
+
+
+// idea from http://stackoverflow.com/questions/16337610/how-to-know-if-a-type-is-a-specialization-of-stdvector
+template <typename T, template<typename...> class R>
+struct is_specialization : std::false_type { };
+template <template<typename...> class R, typename... ARGS>
+struct is_specialization<R<ARGS...>, R> : std::true_type { };
+
+struct safer_int_base { };
+
 template<typename T>
-class safer_int {
-	private:
-		T xi; // the xint implementig my basic type
+class safer_int : public safer_int_base {
+	public: // <-- TODO private
+		T xi; ///< the xint implementig my basic type
 	public:
 		safer_int()
 			: xi( T() )
@@ -73,11 +92,17 @@ class safer_int {
 		template<typename U> bool operator>=(safer_int<U> obj) { return xi>=obj.xi; }
 		template<typename U> bool operator<(safer_int<U> obj) { return xi<obj.xi; }
 		template<typename U> bool operator<=(safer_int<U> obj) { return xi<=obj.xi; }
+		template<typename U> bool operator>(U obj) { return xi>obj; }
+		template<typename U> bool operator>=(U obj) { return xi>=obj; }
+		template<typename U> bool operator<(U obj) { return xi<obj; }
+		template<typename U> bool operator<=(U obj) { return xi<=obj; }
 
 		safer_int<T> & operator++() { xi++; return *this; }
 		safer_int<T> operator++(int) { auto tmp=*this; xi++; return tmp; }
 		safer_int<T> & operator--() { xi--; return *this; }
 		safer_int<T> operator--(int) { auto tmp=*this; xi--; return tmp; }
+
+		safer_int<T> operator-() { auto tmp=*this; tmp.xi = -tmp.xi; return tmp; }
 
 		template<typename U> safer_int<T> & operator+=(safer_int<U> obj) { xi+=obj.xi; return *this; }
 		template<typename U> safer_int<T> & operator-=(safer_int<U> obj) { xi-=obj.xi; return *this; }
@@ -101,6 +126,40 @@ class safer_int {
 		void print(std::ostream& ostr) const { ostr<<xi; }
 };
 
+// intended for comparsions like   int > xint:
+// #define CONDITION typename std::enable_if<! std::is_base_of<safer_int_base, U>{}>::type* = nullptr >
+#define CONDITION typename std::enable_if< std::is_integral<U>{}>::type* = nullptr
+template<typename U, typename T, CONDITION >
+bool operator>(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+
+template<typename U, typename T, CONDITION >
+bool operator>=(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+
+template<typename U, typename T, CONDITION >
+bool operator<(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+
+template<typename U, typename T, CONDITION >
+bool operator<=(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+#undef CONDITION
+
+/*
+// intended for comparsions like boost::number > xint
+#define CONDITION typename std::enable_if< is_specialization<U,boost::multiprecision::number>{}>::type* = nullptr
+template<typename U, typename T, CONDITION >
+bool operator>(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+
+template<typename U, typename T, CONDITION >
+bool operator>=(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+
+template<typename U, typename T, CONDITION >
+bool operator<(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+
+template<typename U, typename T, CONDITION >
+bool operator<=(U obj, safer_int<T> safeint) { return !( (safeint < obj) || (safeint==obj) ); }
+#undef CONDITION
+*/
+// output / streams:
+
 template <typename T>
 std::ostream& operator<<(std::ostream& ostr, safer_int<T> obj) {
 	obj.print(ostr);
@@ -115,6 +174,31 @@ uxint xsize(const T & obj) {
 	return uxint( obj.size() );
 }
 
+
+template <typename T, typename U>
+bool overflow_can_assign(T target, U value) {
+	//UNUSED(target); UNUSED(value);
+
+	//auto max1 = std::numeric_limits<decltype(target)>::max();
+	//if (value > max1 ) return false;
+
+
+	xint aaa;
+	// safer_int<boost::multiprecision::number<boost::multiprecision::backends::cpp_int_backend<64, 64, 1, 1, void>, 0> >
+
+	// boost::multiprecision::number<boost::multiprecision::backends::cpp_int_backend<1024, 65536, 1, 1, void>, 0> >
+	t_correct_int bbb;
+
+	if (aaa > bbb) return true;
+
+	std::cerr << is_specialization< t_correct_int , boost::multiprecision::number >::value << std::endl;
+
+//	if (bbb > aaa) return false; // !
+
+
+	//if (value < std::numeric_limits<decltype(target)>::min()) return false;
+	return true;
+}
 
 
 
