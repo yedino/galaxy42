cb51ff6a9ad59cc638c76672159cdaf6dc9e20f3
Robert Oleksy <robertoleksy6@gmail.com>
2016-06-27 09:38:44 +0000

    [wip] using udp_wrapper

--- src/c_udp_wrapper.hpp
+++ src/c_udp_wrapper.hpp
@@ -8,7 +8,8 @@ class c_udp_wrapper {
 	public:
 		virtual ~c_udp_wrapper() = default;
 		virtual void send_data(const c_ip46_addr &dst_address, const void *data, size_t size_of_data) = 0;
-		virtual void receive_data(void *data_buf, const size_t data_buf_size, c_ip46_addr &from_address) = 0;
+		virtual size_t
+			receive_data(void *data_buf, const size_t data_buf_size, c_ip46_addr &from_address) = 0;
 };
 
 #ifdef __linux__
@@ -16,7 +17,8 @@ class c_udp_wrapper_linux final : public c_udp_wrapper {
 	public:
 		c_udp_wrapper_linux(const int listen_port);
 		void send_data(const c_ip46_addr &dst_address, const void *data, size_t size_of_data) override;
-		void receive_data(void *data_buf, const size_t data_buf_size, c_ip46_addr &from_address) override;
+		size_t receive_data(void *data_buf, const size_t data_buf_size, c_ip46_addr &from_address) override;
+		int get_socket(); // TODO remove this
 	private:
 		int m_socket;
 };
