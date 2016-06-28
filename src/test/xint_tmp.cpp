879197fe2a19a8a524a08e8c10112f1cacded772
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-06-28 17:46:27 +0000

    style: xint: overflow in ctor float almost works

--- src/test/xint_tmp.cpp
+++ src/test/xint_tmp.cpp
@@ -7,29 +7,16 @@
 #include <type_traits>
 
 
-/*
-TEST(xinttmp, simple) {
-	try {
-
-		float x=1000000000,y=100000000,z=999999999;
-		// safer_int<int> aaa( x*y*z );
-		xint bbb( x*y*z );
-
-	} catch(...) { }
-}
-
-
 TEST(xint_tmp, safe_create_float1) {
+	g_dbg_level_set(30,"Details of test");
+
 	auto func = []() { float a=10000, b=10000000000, c=100000000;
 		xint bonus(a*b*c); UNUSED(bonus);	} ;
 	EXPECT_THROW( func()  , boost::numeric::bad_numeric_cast );
 }
-*/
 
 
 TEST(xint_tmp, check_size_of_various_types) {
-//	g_dbg_level_set(30,"Details of test");
-
 	typedef float T;
 	_dbg1(std::numeric_limits<T>::max());
 	_dbg1(std::numeric_limits<T>::digits);
