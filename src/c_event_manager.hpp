c3f509972a90cd28b4a549200588acf4a4406a4a
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-07-11 12:25:04 +0200

    Multiplatform compatibility, c_event_manager_empty

--- src/c_event_manager.hpp
+++ src/c_event_manager.hpp
@@ -17,17 +17,26 @@ class c_tun_device_linux;
 class c_udp_wrapper_linux;
 #include <sys/select.h>
 class c_event_manager_linux final : public c_event_manager {
-	private:
-		const int m_tun_fd;
-		const int m_udp_socket;
-		fd_set m_fd_set_data; ///< select events e.g. wait for UDP peering or TUN input
 	public:
 		c_event_manager_linux(const c_tun_device_linux &tun_device, const c_udp_wrapper_linux &udp_wrapper);
 		void wait_for_event();
 		bool receive_udp_paket();
 		bool get_tun_packet();
+	private:
+		const int m_tun_fd;
+		const int m_udp_socket;
+		fd_set m_fd_set_data; ///< select events e.g. wait for UDP peering or TUN input
 };
-
+#else
+class c_tun_device_empty;
+class c_udp_wrapper_empty;
+class c_event_manager_empty final : public c_event_manager {
+	public:
+		c_event_manager_empty(const c_tun_device_empty &tun_device, const c_udp_wrapper_empty &udp_wrapper);
+		void wait_for_event();
+		bool receive_udp_paket();
+		bool get_tun_packet();
+};
 #endif // __linux__
 
 #endif // C_EVENT_MANAGER_HPP
