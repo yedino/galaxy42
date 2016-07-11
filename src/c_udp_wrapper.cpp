23b9e1b739827cc5e819c117a2ee25430c8c8b75
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-07-11 11:44:53 +0200

    [wip] implement c_udp_wrapper_empty for compilation multiplatform compatibility

--- src/c_udp_wrapper.cpp
+++ src/c_udp_wrapper.cpp
@@ -47,3 +47,21 @@ int c_udp_wrapper_linux::get_socket() {
 	return m_socket;
 }
 #endif // __linux__
+
+c_udp_wrapper_empty::c_udp_wrapper_empty(const int listen_port)
+:
+  m_listen_port(listen_port)
+{ }
+
+void c_udp_wrapper_empty::send_data(const c_ip46_addr &dst_address, const void *data, size_t size_of_data) {
+	_UNUSED(dst_address);
+	_UNUSED(data);
+	_UNUSED(size_of_data);
+}
+
+size_t c_udp_wrapper_empty::receive_data(void *data_buf, const size_t data_buf_size, c_ip46_addr &from_address) {
+	_UNUSED(data_buf);
+	_UNUSED(data_buf_size);
+	_UNUSED(from_address);
+	return 0;
+}
