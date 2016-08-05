aabeadf94d98bed917efd9f43b4e857eeaf5be60
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-08-05 12:30:48 +0200

    add checksum_ipv6_packet for c_ndp

--- src/c_ndp.hpp
+++ src/c_ndp.hpp
@@ -2,7 +2,7 @@
 #define C_NDP_HPP
 
 
-#if defined(_WIN32) || defined(__CYGWIN__)
+//#if defined(_WIN32) || defined(__CYGWIN__)
 
 #include <array>
 #include <cstdint>
@@ -15,8 +15,17 @@ class c_ndp {
 			(const std::array<uint8_t, 9000> &packet_data);
 		static std::array<uint8_t, 94> generate_neighbor_advertisement
 			(const std::array<uint8_t, 9000> &neighbor_solicitation_packet);
+
+		// next_hvalue: 58 icmpv6 and 17 for udpv6
+
+		static uint16_t checksum_ipv6_packet(
+				const uint8_t* source_destination_addr,
+				const uint8_t* header_with_content,
+				uint16_t length,
+				uint32_t next_hvalue);
+
 };
 
-#endif // _WIN32
+//#endif // _WIN32
 
 #endif
