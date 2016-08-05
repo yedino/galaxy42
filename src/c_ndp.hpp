dfc1df681e43dd6be35987703c5bcacf77ed401c
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-08-05 12:46:08 +0200

    add macro for determining endianess

--- src/c_ndp.hpp
+++ src/c_ndp.hpp
@@ -1,8 +1,34 @@
+// order32.h
+// http://stackoverflow.com/questions/2100331/c-macro-definition-to-determine-big-endian-or-little-endian-machine
+
+#ifndef ORDER32_H
+#define ORDER32_H
+
+#include <climits>
+#include <cstdint>
+
+#if CHAR_BIT != 8
+#error "unsupported char size"
+#endif
+
+enum {
+	O32_LITTLE_ENDIAN = 0x03020100ul,
+	O32_BIG_ENDIAN = 0x00010203ul,
+	O32_PDP_ENDIAN = 0x01000302ul
+};
+
+static const union { unsigned char bytes[4]; uint32_t value; } o32_host_order = { { 0, 1, 2, 3 } };
+
+#define O32_HOST_ORDER (o32_host_order.value)
+
+#endif
+
+
 #ifndef C_NDP_HPP
 #define C_NDP_HPP
 
 
-//#if defined(_WIN32) || defined(__CYGWIN__)
+#if defined(_WIN32) || defined(__CYGWIN__)
 
 #include <array>
 #include <cstdint>
@@ -26,6 +52,6 @@ class c_ndp {
 
 };
 
-//#endif // _WIN32
+#endif // _WIN32
 
 #endif
