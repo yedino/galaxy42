e8f2486bbad681035d4f697bda89ec65427d0853
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-08-10 15:12:55 +0000

    [test] gitian helloworld includes

--- qa/gitian-helloworld-src/main.cpp
+++ qa/gitian-helloworld-src/main.cpp
@@ -1,4 +1,42 @@
 
+#include <iostream>
+
+#include <boost/any.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/ip/address.hpp>
+#include <boost/bind.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_int/limits.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <boost/program_options.hpp>
+
+#include <sstream>
+#include <stdexcept>
+#include <stdio.h>
+#include <stdlib.h>
+#include <streambuf>
+#include <string>
+#include <string.h>
+
+#include <fstream>
+#include <functional>
+#include <ifdef.h>
+#include <io.h>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+
+// #include <json/json.h>
+
+
+void foo() {
+	std::cout << "foo" << std::endl;
+}
+
 int main() {
+	foo();
 }
 
