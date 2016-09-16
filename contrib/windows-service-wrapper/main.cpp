0199dacd83e8b7bcd28cfd83c89f612033c52520
Robert Oleksy <robertoleksy6@gmail.com>
2016-09-16 12:14:43 +0200

    convert c_service_wrapper to static class

--- contrib/windows-service-wrapper/main.cpp
+++ contrib/windows-service-wrapper/main.cpp
@@ -1,49 +1,40 @@
-//sc create SampleService binPath= C:\cygwin\home\Tigu_WIN\test\service_wrapper.exe
-//sc delete SampleService
+//sc create ServiceName binPath= absolute path to this wrapper binary
+//sc delete ServiceName
 #define UNICODE
 #include <cwchar>
 #include <functional>
 #include <string>
 #include <windows.h>
 
-class c_service_wrapper {
+class c_service_wrapper final {
 	public:
 		/**
-		 * @param child_binary_name Name of child proces binary. Wrapper and child binaries must be in the same direcrory.
+		 * @param child_binary_name Name of child proces binary. Wrapper and child binaries must be in the same direcrory. The call will block until service is stopped.
 		 */
-		c_service_wrapper(const std::wstring &child_binary_name);
-		void run_child(); ///< blocks
+		static void run_as_service(const std::wstring &child_binary_name);
 	private:
-		std::wstring m_name;
-		SERVICE_TABLE_ENTRY m_service_table[2];
-		SERVICE_STATUS m_service_status;
-		SERVICE_STATUS_HANDLE m_handle_status;
-		PROCESS_INFORMATION m_process_info;
+		static std::wstring m_name;
+		static SERVICE_STATUS m_service_status;
+		static SERVICE_STATUS_HANDLE m_handle_status;
+		static PROCESS_INFORMATION m_process_info;
 
-		void service_main(int argc, char** argv);
-		void control_handler(DWORD request);
-		void run_child_process();
+		static void service_main(long unsigned int argc, wchar_t** argv);
+		static void control_handler(DWORD request);
+		static void run_child_process();
 };
 
-c_service_wrapper::c_service_wrapper(const std::wstring &child_binary_name)
-:
-	m_name(child_binary_name)
-{
+void c_service_wrapper::run_as_service(const std::wstring &child_binary_name) {
+	m_name = child_binary_name;
 	SERVICE_TABLE_ENTRY m_service_table[2];
 	wchar_t service_name[] = L"galaxy42_service";
 	m_service_table[0].lpServiceName = service_name;
-	// function pointer to service_main(int argc, char **argv)
-	std::function<void(int, char**)> srv_main_fn = std::bind(&c_service_wrapper::service_main, *this, std::placeholders::_1, std::placeholders::_2);
-	m_service_table[0].lpServiceProc = reinterpret_cast<LPSERVICE_MAIN_FUNCTION>(srv_main_fn.target<void(int, char**)>());
+	m_service_table[0].lpServiceProc = reinterpret_cast<LPSERVICE_MAIN_FUNCTION>(&c_service_wrapper::service_main);
 	m_service_table[1].lpServiceName = nullptr;
 	m_service_table[1].lpServiceProc = nullptr;
+	StartServiceCtrlDispatcher(m_service_table);
 }
 
-void c_service_wrapper::run_child() {
-	StartServiceCtrlDispatcher(m_service_table); // TODO create function to start
-}
-
-void c_service_wrapper::service_main(int argc, char **argv) {
+void c_service_wrapper::service_main(long unsigned int argc, wchar_t **argv) {
 	m_service_status.dwServiceType              = SERVICE_WIN32_OWN_PROCESS;
 	m_service_status.dwCurrentState             = SERVICE_RUNNING; // TODO rm?
 	m_service_status.dwControlsAccepted         = SERVICE_ACCEPT_STOP;
@@ -52,12 +43,10 @@ void c_service_wrapper::service_main(int argc, char **argv) {
 	m_service_status.dwCheckPoint               = 0;
 	m_service_status.dwWaitHint                 = 0;
 
-	// function pointer to control_handler(DWORD request)
-	std::function<void(DWORD)> handler_proc_fn = std::bind(&c_service_wrapper::control_handler, *this, std::placeholders::_1);
-	m_handle_status = RegisterServiceCtrlHandler(L"galaxy42_service", reinterpret_cast<LPHANDLER_FUNCTION>(handler_proc_fn.target<void(DWORD)>()));
+	m_handle_status = RegisterServiceCtrlHandler(L"galaxy42_service", reinterpret_cast<LPHANDLER_FUNCTION>(&c_service_wrapper::control_handler));
 	if (m_handle_status == 0) return; // ERROR
 
-	m_service_status.dwCurrentState             = SERVICE_RUNNING;
+	m_service_status.dwCurrentState = SERVICE_RUNNING;
 	SetServiceStatus(m_handle_status, &m_service_status);
 	while (m_service_status.dwCurrentState == SERVICE_RUNNING) {
 		run_child_process();
@@ -85,17 +74,22 @@ void c_service_wrapper::run_child_process() {
 	GetModuleFileNameW(hModule, &path[0], path.size());
 	auto pos = path.find_last_of(L"\\");
 	path.erase(pos);
-	// running process
+	path += L"\\";
+	path += m_name;
+	// running wrapped process
 	STARTUPINFOW startup_info;
 	ZeroMemory(&startup_info, sizeof(startup_info));
 	startup_info.cb = sizeof(startup_info);
 	ZeroMemory(&m_process_info, sizeof(m_process_info));
-	auto ret = CreateProcessW(&path[0], &path[0], nullptr, nullptr, false, 0, nullptr, nullptr, &startup_info, &m_process_info);
-	if (!ret) return;
+	CreateProcessW(&path[0], &path[0], nullptr, nullptr, false, 0, nullptr, nullptr, &startup_info, &m_process_info);
 }
 
+SERVICE_STATUS c_service_wrapper::m_service_status;
+SERVICE_STATUS_HANDLE c_service_wrapper::m_handle_status;
+PROCESS_INFORMATION c_service_wrapper::m_process_info;
+std::wstring c_service_wrapper::m_name;
+
 int main() {
-	c_service_wrapper service_wrapper(L"tunserver.elf.exe");
-	service_wrapper.run_child();
+	c_service_wrapper::run_as_service(L"tunserver.elf.exe"); // run tunserver.elf.exe as service
 	return 0;
 }
