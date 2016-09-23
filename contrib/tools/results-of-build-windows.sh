79f51c0fd89f1d55e934706cc00768ab4bc1e39a
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-09-23 12:50:58 +0000

    Gitian print checksum fixes

--- contrib/tools/results-of-build-windows.sh
+++ contrib/tools/results-of-build-windows.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-sha256sum $( find  /home/ubuntu/build/boost/mingw_build/lib/*   /home/ubuntu/build/libsodium/*/bin/   -name  '*.dll' -o -name '*.exe' ; ls -1 /home/ubuntu/build/galaxy42/*exe | sort )
+
+# outputs sum of files for windows
+# first line is the main checksum
+
+
+mapfile -t all_file < <( find  /home/ubuntu/build/boost/mingw_build/lib/*   /home/ubuntu/build/libsodium/*/bin/   -name  '*.dll' -o -name '*.exe' ; ls -1 /home/ubuntu/build/galaxy42/*exe | sort )
+
+#mapfile -t all_file < <( find  test/lib/*      -name  '*.dll' -o -name '*.exe' | sort ) # test
+if (( "${#all_file[@]}" )) ; then
+	mapfile -t all_hash < <( sha256sum "${all_file[@]}")
+else
+	all_hash=()
+fi
+# printf "f %s\n" "${all_file[@]}"
+# printf "%s\n" "${all_hash[@]}" | cat -
+hash_of_hash=$( printf "%s\n" "${all_hash[@]}" | sha256sum )
+
+printf "%s\n" "$hash_of_hash"
+
+printf "%s\n" "${all_hash[@]}"
+
