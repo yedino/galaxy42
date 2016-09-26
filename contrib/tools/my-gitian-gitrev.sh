0c96f36008ac6491719ff4342154e42376a7d858
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-09-26 13:58:22 +0000

    Gitian: checksum report update

--- contrib/tools/my-gitian-gitrev.sh
+++ contrib/tools/my-gitian-gitrev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd "${BASH_SOURCE%/*}" || exit # cd into the bundle and use relative paths; http://mywiki.wooledge.org/BashFAQ/028
+cd "${BASH_SOURCE%/*}" || { echo "(error: can not cd into dir)" ; exit 20 ; } # cd into the bundle and use relative paths; http://mywiki.wooledge.org/BashFAQ/028
 git log --format="%H" -1 || { echo "(error: can not detect gitrev. pwd=$PWD)" ; exit 1; }
