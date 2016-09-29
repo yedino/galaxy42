e714dca06ba1486f35ed8ce99390e7a3869d6eed
Robert Oleksy <robertoleksy6@gmail.com>
2016-09-29 12:47:51 +0000

    add optins for /build_gettext.sh

--- depends/gettext/build_gettext.sh
+++ depends/gettext/build_gettext.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-
+# usage ./build_gettext i686-w64-mingw32 or ./build_gettext x86_64-w64-mingw32
 # libiconv
 wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
 tar xf libiconv-1.14.tar.gz
 libiconvdir=`pwd`/libiconv-1.14
 cd $libiconvdir
-./configure --disable-shared --host=i686-w64-mingw32 --prefix="$libiconvdir/install_dir"
+./configure --disable-shared --host="$1" --prefix="$libiconvdir/install_dir"
 make install
 cd ..
-#echo "******************$libiconvdir/install_dir/"
-#exit 1
+
 # gettext
 wget http://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.tar.gz
 tar xf gettext-0.19.8.tar.gz
 cd gettext-0.19.8
-./configure --disable-shared --host=i686-w64-mingw32 --prefix=`pwd`/install_dir --with-libiconv-prefix="$libiconvdir/install_dir/"
+./configure --disable-shared --host="$1" --prefix=`pwd`/install_dir --with-libiconv-prefix="$libiconvdir/install_dir/"
 make install
