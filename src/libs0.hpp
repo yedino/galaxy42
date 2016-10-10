32fa30e08ed88ff72d2cc349f3c4c46b8937bc64
Krystain Mazela <krystian.mazela@onet.pl>
2016-10-10 16:50:55 +0200

    fake gettext for msvc

--- src/libs0.hpp
+++ src/libs0.hpp
@@ -243,4 +243,8 @@ throw std::runtime_error(oss.str()); \
 } while(0)
 
 #endif
-
+#if defined(_WIN32) || defined(__CYGWIN__)
+const char * gettext(const char * s);
+char * bindtextdomain(const char * domainname, const char * dirname);
+char * textdomain(const char * domainname);
+#endif
