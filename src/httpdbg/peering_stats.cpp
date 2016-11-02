2a353b08bd0e49fae95865bf6c2ed1f441852d97
Ryszard Mikulec <mikurys@gmail.com>
2016-11-02 10:05:11 +0100

    Data buffer added to stats

--- src/httpdbg/peering_stats.cpp
+++ src/httpdbg/peering_stats.cpp
@@ -1,8 +1,59 @@
 #include "peering_stats.hpp"
 #include <sstream>
+#include <iostream>
 #include <iomanip>
 
 using namespace std;
+
+c_data_tramsmission_buffer::c_data_tramsmission_buffer(int buffer_size, int interval_in_milisec):
+    m_data_sent_buffer(buffer_size, 0)
+  ,m_data_read_buffer(buffer_size, 0)
+  ,m_packets_sent_buffer(buffer_size, 0)
+  ,m_packets_read_buffer(buffer_size, 0)
+  ,m_last_sent(0)
+  ,m_last_read(0)
+  ,m_interval(interval_in_milisec)
+  ,m_reference_time(chrono::system_clock::now())
+{}
+
+void c_data_tramsmission_buffer::update_sent_buffer(size_t data_size){
+    auto now = chrono::system_clock::now();
+    long units = chrono::duration<double, milli>(now - m_reference_time).count() / m_interval;
+    long distance = units - m_last_sent;
+    m_last_sent = units;
+    for(int i=0; i<distance; i++)
+    {
+        m_data_sent_buffer.push_back(0);
+        m_packets_sent_buffer.push_back(0);
+    }
+    m_data_sent_buffer.back() += data_size;
+    m_packets_sent_buffer.back()++;
+}
+
+void c_data_tramsmission_buffer::update_read_buffer(size_t data_size){
+    auto now = chrono::system_clock::now();
+    long units = chrono::duration<double, milli>(now - m_reference_time).count() / m_interval;
+    long distance = units - m_last_read;
+    m_last_read = units;
+    for(int i=0; i<distance; i++)
+    {
+        m_data_read_buffer.push_back(0);
+        m_packets_read_buffer.push_back(0);
+    }
+    m_data_read_buffer.back() += data_size;
+    m_packets_read_buffer.back()++;
+}
+
+string c_data_tramsmission_buffer::get_data_buffer_as_string() const{
+    ostringstream out;
+    for(int i=0; i<m_data_read_buffer.size(); i++)
+    {
+        out << "['" << i << "', " << m_data_read_buffer[i] << ", " << m_data_sent_buffer[i] << "],";
+    }
+    string str = out.str();
+    str.pop_back();
+    return str;
+}
 
 c_peering_stats::c_peering_stats():
     m_size_of_sent_data(0)
@@ -10,18 +61,17 @@ c_peering_stats::c_peering_stats():
   ,m_number_of_sent_packets(0)
   ,m_number_of_read_packets(0)
   ,m_connection_time(chrono::system_clock::now())
-  ,m_read_data_stats(60, 0)
-  ,m_sent_data_stats(60, 0)
-  ,m_time_read_data(chrono::system_clock::now())
-  ,m_time_sent_data(chrono::system_clock::now())
+  ,m_data_buffer(120, 1000)
  {}
 
 void c_peering_stats::update_sent_stats(size_t size_of_sent_data){
+    m_data_buffer.update_sent_buffer(size_of_sent_data);
     m_size_of_sent_data += size_of_sent_data;
     m_number_of_sent_packets++;
 }
 
 void c_peering_stats::update_read_stats(size_t size_of_read_data){
+    m_data_buffer.update_read_buffer(size_of_read_data);
     m_size_of_read_data += size_of_read_data;
     m_number_of_read_packets++;
 }
--- src/httpdbg/peering_stats.hpp
+++ src/httpdbg/peering_stats.hpp
@@ -5,6 +5,26 @@
 #include <ctime>
 #include <chrono>
 #include <string>
+#include <boost/circular_buffer.hpp>
+
+class c_data_tramsmission_buffer
+{
+public:
+    c_data_tramsmission_buffer(int buffer_size, int interval_in_milisec);
+    void update_sent_buffer(std::size_t data_size);
+    void update_read_buffer(std::size_t data_size);
+    std::string get_data_buffer_as_string() const;
+
+private:
+    boost::circular_buffer<int> m_data_sent_buffer;
+    boost::circular_buffer<int> m_data_read_buffer;
+    boost::circular_buffer<int> m_packets_sent_buffer;
+    boost::circular_buffer<int> m_packets_read_buffer;
+    long m_last_sent;
+    long m_last_read;
+    int m_interval;
+    std::chrono::time_point<std::chrono::system_clock> m_reference_time;
+};
 
 class c_peering_stats
 {
@@ -18,6 +38,7 @@ public:
     long get_number_of_read_packets() const;
     std::string get_connection_time() const;
     void reset_connection_time();
+    c_data_tramsmission_buffer m_data_buffer;
 
 private:
     std::size_t m_size_of_sent_data;
@@ -25,10 +46,7 @@ private:
     long m_number_of_sent_packets;
     long m_number_of_read_packets;
     std::chrono::time_point<std::chrono::system_clock> m_connection_time;
-    boost::circular_buffer<int> m_read_data_stats;
-    boost::circular_buffer<int> m_sent_data_stats;
-    long m_time_read_data;
-    long m_time_sent_data;
+
 };
 
 #endif
