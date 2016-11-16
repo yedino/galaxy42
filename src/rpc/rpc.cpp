32f42c876240ad36154d058f515a4e8d585673bd
Robert Oleksy <robertoleksy6@gmail.com>
2016-11-16 13:36:40 +0000

    remove session from vector

--- src/rpc/rpc.cpp
+++ src/rpc/rpc.cpp
@@ -24,14 +24,20 @@ void c_rpc_sever::accept_handler(const boost::system::error_code &error) {
 	});
 }
 
+void c_rpc_sever::remove_session_from_vector(const size_t index) {
+	if (index >= m_session_vector.size()) throw std::out_of_range("Not found session with index " + std::to_string(index));
+	m_session_vector.erase(m_session_vector.begin() + index);
+}
 
-c_rpc_sever::c_session::c_session(size_t index_in_session_vector, const c_rpc_sever *rpc_server_ptr, boost::asio::ip::tcp::socket &&socket)
+
+c_rpc_sever::c_session::c_session(size_t index_in_session_vector, c_rpc_sever *rpc_server_ptr, boost::asio::ip::tcp::socket &&socket)
 :
 	m_index_in_session_vector(index_in_session_vector),
 	m_rpc_server_ptr(rpc_server_ptr),
 	m_socket(std::move(socket)),
 	m_received_data(1024, 0) // fill
 {
+	// start reading
 	m_socket.async_read_some(boost::asio::buffer(&m_received_data[0], m_received_data.size()),
 		[this](const boost::system::error_code &error, std::size_t bytes_transferred) {
 			read_handler(error, bytes_transferred);
@@ -39,5 +45,13 @@ c_rpc_sever::c_session::c_session(size_t index_in_session_vector, const c_rpc_se
 }
 
 void c_rpc_sever::c_session::read_handler(const boost::system::error_code &error, std::size_t bytes_transferred) {
-
+	if (error) {
+		m_rpc_server_ptr->remove_session_from_vector(m_index_in_session_vector);
+		return;
+	}
+	// continue reading
+	m_socket.async_read_some(boost::asio::buffer(&m_received_data[0], m_received_data.size()),
+		[this](const boost::system::error_code &error, std::size_t bytes_transferred) {
+			read_handler(error, bytes_transferred);
+	});
 }
