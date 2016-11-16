903f958668840ddb3783be4c01b762e7cdfb3c6e
Robert Oleksy <robertoleksy6@gmail.com>
2016-11-16 13:36:40 +0000

    remove session from vector

--- src/rpc/rpc.hpp
+++ src/rpc/rpc.hpp
@@ -19,13 +19,14 @@ class c_rpc_sever final {
 		std::vector<c_session> m_session_vector;
 
 		void accept_handler(const boost::system::error_code &error);
+		void remove_session_from_vector(const size_t index);
 
 		class c_session {
 			public:
-				c_session(size_t index_in_session_vector, c_rpc_sever const *rpc_server_ptr, boost::asio::ip::tcp::socket &&socket);
+				c_session(size_t index_in_session_vector, c_rpc_sever *rpc_server_ptr, boost::asio::ip::tcp::socket &&socket);
 			private:
-				size_t m_index_in_session_vector;
-				c_rpc_sever const *m_rpc_server_ptr;
+				size_t m_index_in_session_vector; // my index in m_session_vector
+				c_rpc_sever *m_rpc_server_ptr;
 				boost::asio::ip::tcp::socket m_socket;
 				std::string m_received_data;
 
