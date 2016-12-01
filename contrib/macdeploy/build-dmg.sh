833cecc9f598f4e0ee9540370efae82401761ba6
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-12-01 16:00:58 +0100

    build-dmg.sh reorganize tabs

--- contrib/macdeploy/build-dmg.sh
+++ contrib/macdeploy/build-dmg.sh
@@ -12,14 +12,12 @@ readonly GALAXY42_VERSION="$(git describe)"
 
 
 is_dir() {
-    local dir=$1
-
-    [[ -d $dir ]]
+	local dir=$1
+	[[ -d $dir ]]
 }
 is_file() {
-    local file=$1
-
-    [[ -f $file ]]
+	local file=$1
+	[[ -f $file ]]
 }
 
 clean_dirs() {
@@ -65,7 +63,7 @@ change_tun_dylib_loadpath () {
 	# check that required files exist
 	! is_file $tun_path \
 		&& echo "$tun_path not exist, something went wrong, exiting ..." && exit 1
-	
+
 	for dylib in "${dylib_list[@]}"; do
 		! is_file $dylib \
 			&& echo "missing library: $dylib. This library is required on your machine to create pkg, exiting ..." && exit 1
@@ -82,12 +80,12 @@ change_tun_dylib_loadpath () {
 create_tun_component () {
 	echo "creating tun.pkg component"
 	local tun_identifier="tunserver"
-    local tun_componenet_app="Tunserver.app"
+	local tun_componenet_app="Tunserver.app"
 	local tun_plist="tunserver.plist"
 	local tun_bin="tunserver.elf"
 	local tun_pkg="tunserver.pkg"
 
-	
+
 	pushd $SCRIPT_DIR
 		clean_dirs "$tun_componenet_app"
 
@@ -95,25 +93,25 @@ create_tun_component () {
 
 		cp -n "../../$tun_bin" $tun_componenet_app
 		change_tun_dylib_loadpath
-		
+
 		pkgbuild --analyze --root "$tun_componenet_app" $tun_plist
 		pkgbuild --identifier $tun_identifier \
 			--root $tun_componenet_app \
 			--component-plist $tun_plist \
-            --install-location "/Applications/Galaxy42.app" \
+			--install-location "/Applications/Galaxy42.app" \
 			$tun_pkg
-		
-                clean_dirs "$tun_componenet_app"
-                clean_files "$tun_plist"
-    popd
+
+		clean_dirs "$tun_componenet_app"
+		clean_files "$tun_plist"
+	popd
 }
 create_boost_component () {
 	echo "creating boost.pkg component"
 	local boost_identifier="boost_libs"
 	local boost_componenet_app="Boost.app"
-    local boost_plist="boost.plist"
+	local boost_plist="boost.plist"
 	local boost_pkg="boost.pkg"
-    
+
 	pushd $SCRIPT_DIR
 		clean_dirs "$boost_componenet_app"
 
@@ -123,64 +121,65 @@ create_boost_component () {
 		cp -n "/usr/local/opt/boost/lib/libboost_filesystem-mt.dylib" $boost_componenet_app
 		cp -n "/usr/local/opt/boost/lib/libboost_program_options-mt.dylib" $boost_componenet_app
 
-        pkgbuild --analyze --root "$boost_componenet_app/" $boost_plist
+		pkgbuild --analyze --root "$boost_componenet_app/" $boost_plist
 		pkgbuild --identifier $boost_identifier \
 			--root $boost_componenet_app \
-                        --install-location "/Applications/Galaxy42.app" \
+			--install-location "/Applications/Galaxy42.app" \
 			$boost_pkg
-		
+
 		clean_dirs "$boost_componenet_app"
 		clean_files "$boost_plist"
-    popd
+	popd
 }
 create_sodium_component () {
 	echo "creating sodium.pkg component"
 	local sodium_identifier="libsodium"
 	local sodium_componenet_app="Sodium.app"
-    local sodium_plist="sodium.plist"
+	local sodium_plist="sodium.plist"
 	local sodium_pkg="sodium.pkg"
-    
+
 	pushd $SCRIPT_DIR
 		clean_dirs "$sodium_componenet_app"
 
 		mkdir $sodium_componenet_app
 		cp -n "/usr/local/opt/libsodium/lib/libsodium.18.dylib" $sodium_componenet_app
 
-        pkgbuild --analyze --root "$sodium_componenet_app" $sodium_plist
+		pkgbuild --analyze --root "$sodium_componenet_app" $sodium_plist
 		pkgbuild --identifier $sodium_identifier \
 			--root $sodium_componenet_app \
-                        --install-location "/Applications/Galaxy42.app" \
+			--install-location "/Applications/Galaxy42.app" \
 			$sodium_pkg
-		
+
 		clean_dirs "$sodium_componenet_app"
 		clean_files "$sodium_plist"
-    popd
+	popd
 }
 create_galaxy_pkg() {
 	echo "creating galaxy42.pkg"
 	create_tun_component
 	create_boost_component
 	create_sodium_component
-    
-    pushd $SCRIPT_DIR
+
+	pushd $SCRIPT_DIR
 		productbuild --synthesize \
 				--package tunserver.pkg \
 				--package boost.pkg \
 				--package sodium.pkg \
 				Distribution.xml
+
 		productbuild --distribution ./Distribution.xml \
 				--package-path . \
 				$PKG_PATH
-		
+
 		clean_files "Distribution.xml" "tunserver.pkg" "boost.pkg" "sodium.pkg"
-    popd
+	popd
 }
 create_galaxy_dmg() {
 	local tmp_name="gal_tmp.dmg"
 	local vol_name="galaxy42_volume"
 	local vol_size="10000k"
-            
-    pushd $SCRIPT_DIR
+
+	pushd $SCRIPT_DIR
 		clean_files "$tmp_name" "${DMG_PATH}"
 
 		hdiutil create -srcfolder "${PKG_PATH}" \
@@ -190,11 +189,9 @@ create_galaxy_dmg() {
 			-format UDRW \
 			-size "${vol_size}" \
 			"${tmp_name}"
-		
-
 
 		device=$(hdiutil attach -readwrite -noverify -noautoopen "${tmp_name}" | egrep '^/dev/' | sed 1q | awk '{print $1}' )
-		
+
 		echo "sudo is necessary in this step to setting permissions properly:"
 		set -x
 		sudo chmod -Rf go-w "/Volumes/${vol_name}"
@@ -205,9 +202,9 @@ create_galaxy_dmg() {
 
 		hdiutil detach "${device}"
 		hdiutil convert "${tmp_name}" -format UDZO -imagekey zlib-level=9 -o "${DMG_PATH}"
-		
+
 		clean_files "${tmp_name}" "${PKG_PATH}"
-	popd 
+	popd
 }
 
 main() {
