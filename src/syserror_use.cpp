57fc6b0ccef624b85b65c4b487884c067cb5438a
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2016-12-05 18:18:18 +0000

    tun/tap errors: mac, windows errno

--- src/syserror_use.cpp
+++ src/syserror_use.cpp
@@ -1,7 +1,18 @@
 
+
+// need this to have the strerror_s - as in http://en.cppreference.com/w/c/string/byte/strerror point 3
+// this must be on top, must be first inclusion of <string> (in this compilation / translation unit)
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <string>
+#if (!defined(__STDC_LIB_EXT1__))
+	#warning "It seems __STDC_LIB_EXT1__ is not set by implementation/compiler - and we need it to have strerror_s"
+#endif
+
 #include <syserror_use.hpp>
+
 #include <cstring>
 #include <sstream>
+
 
 std::string errno_to_string(int errno_copy) {
 	// [02/12/2016 15:43] for http://man7.org/linux/man-pages/man3/strerror.3.html  how exactly to test in C program with #if should I call XSI-compilant, or GNU, version of strerror_r
@@ -13,16 +24,28 @@ std::string errno_to_string(int errno_copy) {
 	char buf[buflen];
 	std::memset(buf, 0, buflen); // extra guarantee no memleak
 
-	#if ( (_POSIX_C_SOURCE >= 200112L) && !  _GNU_SOURCE )  ||  (defined(__MACH__))
-		// XSI
-		//  int strerror_r(int errnum, char *buf, size_t buflen);
-		int result = strerror_r(errno_copy, buf, buflen);
-		// The XSI-compliant strerror_r() function returns 0 on success.  On
-		// error, a (positive) error number is returned (since glibc 2.13), or
-		// -1 is returned and errno is set to indicate the error (glibc versions
-		// before 2.13). -- according to http://man7.org/linux/man-pages/man3/strerror.3.html
+	#if ( \
+		( (_POSIX_C_SOURCE >= 200112L) && !  _GNU_SOURCE ) \
+		||  ( defined(__MACH__) ) \
+		||  ( defined(_WIN32) || defined(__CYGWIN__) ) \
+	)
+		// for linux in XSI mode
+		// for mac os x
+		// for windows - https://msdn.microsoft.com/en-us/library/51sah927.aspx
 
-		if (result != 0) {
+		#if ( defined(__linux__) || defined(__MACH__) )
+			// Linus XSI and Mac:
+			//  int strerror_r(int errnum, char *buf, size_t buflen);
+			int result = strerror_r(errno_copy, buf, buflen);
+		#elif ( defined(_WIN32) || defined(__CYGWIN__) )
+			// Windows:
+			// strerror_s( char *buffer, size_t numberOfElements, int errnum )
+			int result = strerror_s( buf , buflen , errno_copy );
+		#else
+			#error "Unknown platform for strerror_r / strerror_s - please add IF for this platform here"
+		#endif
+
+		if (result != 0) { // on linux, mac, windows
 			std::ostringstream oss;
 			oss << "(errno_copy=" << errno_copy<< ", can not convert this to error message because result="<<result <<")";
 			return oss.str();
