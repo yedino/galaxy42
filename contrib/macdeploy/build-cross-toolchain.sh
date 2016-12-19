5f643acf4a4a2eb785188012b7fe15b8d19ab57e
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-12-19 18:39:50 +0100

    [wip][gitian] cross-osx build

---
+++ contrib/macdeploy/build-cross-toolchain.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# prepare clang apple toolchain
+git clone https://github.com/tpoechtrager/osxcross
+pushd osxcross
+	
+	git checkout 2b3387844c1dccdd88be4cbc0de7ec955b05a630
+
+	local sdkfile="../MacOSX10.11.sdk.tar.gz"
+	[ -r "$sdkfile" ] 
+		|| echo "You need to provide file $sdkfile (PWD=$PWD). Read instruction in $dir_base_of_source/contrib/macdeploy/README_osx.md"
+
+	cp -v "${sdkfile}" tarballs/
+	UNATTENDED=1 ./build.sh
+
+	# configure built toolchain, set env variables PATH etc. 
+	eval "$(./tools/osxcross_conf.sh)"
+	
+popd
