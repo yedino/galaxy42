164f5000894a2f36ac519c27bef1baf7c40666e0
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-12-20 12:18:35 +0100

    fix syntax build-cross-toolchain.sh

--- contrib/macdeploy/build-cross-toolchain.sh
+++ contrib/macdeploy/build-cross-toolchain.sh
@@ -3,6 +3,8 @@
 set -o errexit
 set -o nounset
 
+# clean previous build
+rm -rf osxcross
 # prepare clang apple toolchain
 git clone https://github.com/tpoechtrager/osxcross
 pushd osxcross
@@ -10,7 +12,7 @@ pushd osxcross
 	git checkout 2b3387844c1dccdd88be4cbc0de7ec955b05a630
 
 	sdkfile="../MacOSX10.11.sdk.tar.gz"
-	[ -r "$sdkfile" ] 
+	[ -r "$sdkfile" ] \
 		|| echo "You need to provide file $sdkfile (PWD=$PWD). Read instruction in $dir_base_of_source/contrib/macdeploy/README_osx.md"
 
 	cp -v "${sdkfile}" tarballs/
