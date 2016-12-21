bc71fac1e8b114e0a93f4953e0977d18199f8943
Rysiek <mikurys@gmail.com>
2016-12-21 20:15:33 +0100

    Added tunserver port and rpc port option in tunserver.elf

--- src/test/utility.cpp
+++ src/test/utility.cpp
@@ -5,16 +5,16 @@
 
 TEST(utility, parse_ip_number) {
 	g_dbg_level_set(255, "start test");
-	c_tunserver myserver;
-	EXPECT_NO_THROW(myserver.add_peer_simplestring("192.168.2.62:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"));
-	EXPECT_THROW   (myserver.add_peer_simplestring(""), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("kjfahskdfhsh"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("23947934u43y"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62:9042fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("d42:10a9:4318:509b:80ab:8042:6275:609b-192.168.2.62:9042"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62-:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("562.486.489.426:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62:9042-xyz1:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
-	EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b 192.168.2.62:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
+    c_tunserver myserver(9042, 42000);
+    EXPECT_NO_THROW(myserver.add_peer_simplestring("192.168.2.62:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"));
+    EXPECT_THROW   (myserver.add_peer_simplestring(""), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("kjfahskdfhsh"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("23947934u43y"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62:9042fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("d42:10a9:4318:509b:80ab:8042:6275:609b-192.168.2.62:9042"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62-:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("562.486.489.426:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62:9042-xyz1:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
+    EXPECT_THROW   (myserver.add_peer_simplestring("192.168.2.62:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b 192.168.2.62:9042-fd42:10a9:4318:509b:80ab:8042:6275:609b"), std::invalid_argument);
 }
