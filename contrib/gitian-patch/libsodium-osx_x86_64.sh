71f2bce4039c114ec1d0532461229161766314f4
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-12-28 13:29:27 +0100

    [gitian][osx] gitian patch libsodium-osx_x86_64.sh
    [external]  script is based on build-aux/osx.sh script from libsodium project
        github.com/jedisct1/libsodium.git
    	release tag: 1.0.11
    	commit: 2f4f718cd94adab547c210a78050762cf667dfca

---
+++ contrib/gitian-patch/libsodium-osx_x86_64.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+printf "\nApple/darwin11 osx 64bit crossbuild script for Libsodium (with fixes)\n\n"
+
+function fail() {
+	printf "\nError in $0. (see above)\n\n"
+	exit 1
+}
+
+export PREFIX="$(pwd)/libsodium-osx"
+export OSX_VERSION_MIN=${OSX_VERSION_MIN-"10.8"}
+export OSX_CPU_ARCH=${OSX_CPU_ARCH-"core2"}
+
+mkdir -p $PREFIX || fail
+
+export CFLAGS="-arch x86_64 -mmacosx-version-min=${OSX_VERSION_MIN} -march=${OSX_CPU_ARCH} -O2 -g -flto"
+export LDFLAGS="-arch x86_64 -mmacosx-version-min=${OSX_VERSION_MIN} -march=${OSX_CPU_ARCH} -flto"
+
+make distclean > /dev/null
+
+./configure --enable-minimal \
+            --prefix="$PREFIX" \
+            --host=x86_64-apple-darwin11 || fail
+
+make -j3 install || fail
+
+# Cleanup
+make distclean > /dev/null
+
+printf "\nApple/darwin11 osx 64bit crossbuild script for Libsodium (with fixes) - DONE\n\n"
