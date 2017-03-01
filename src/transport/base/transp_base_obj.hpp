c64f897ed6708e8734f9e35f40f617c075f2c299
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-03-01 15:48:47 +0000

    Send chunks to fake transport

--- src/transport/base/transp_base_obj.hpp
+++ src/transport/base/transp_base_obj.hpp
@@ -8,7 +8,7 @@ class c_transport_base_obj {
 		c_transport_base_obj();
 		virtual ~c_transport_base_obj() = default;
 
-		virtual void send_data(boost::any dest, const char *data, size_t size_of_data); ///< block function
+		virtual void send_data(boost::any dest, const unsigned char *data, size_t size_of_data); ///< block function
 		// virtual void send_data_async(boost::any dest, const char *data, size_t size_of_data); // TODO
 };
 
--- src/transport/udp/transport_udp.cpp
+++ src/transport/udp/transport_udp.cpp
@@ -3,7 +3,7 @@
 
 using namespace boost::asio;
 
-void c_transport_udp::send_data(boost::any dest, const char *data, size_t size_of_data) {
+void c_transport_udp::send_data(boost::any dest, const unsigned char *data, size_t size_of_data) {
 	c_transport_udp_addr destination_endpoint;
 	destination_endpoint = boost::any_cast<decltype(destination_endpoint)>(dest);
 	ip::udp::socket socket(m_io_service);
