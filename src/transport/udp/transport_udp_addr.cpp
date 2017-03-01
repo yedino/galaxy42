cd3beb40dda1992529aeee606ea7c9736e870558
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-01 15:31:41 +0000

    string constructor for c_transport_udp_addr

--- src/transport/udp/transport_udp_addr.cpp
+++ src/transport/udp/transport_udp_addr.cpp
@@ -1,9 +1,17 @@
 #include "transport_udp_addr.hpp"
+#include "../../galaxy42_utils.hpp"
+
+using namespace boost::asio;
 
 c_transport_udp_addr::c_transport_udp_addr(const boost::asio::ip::udp::endpoint &endpoint)
 :
 	m_endpoint(endpoint)
 {
+}
+
+c_transport_udp_addr::c_transport_udp_addr(const std::string &ip_string) {
+	std::pair<std::string, int> endpoint = tunserver_utils::parse_ip_string(ip_string);
+	m_endpoint = ip::udp::endpoint(ip::address::from_string(endpoint.first), static_cast<unsigned short>(endpoint.second));
 }
 
 void c_transport_udp_addr::print(std::ostream &ostr) const {
