a3fa748cd2fd2e1f095893ae62c9e366499a8c70
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-03 10:24:11 +0000

    [wip] transport shm

--- src/cable/shm/cable_shm_addr.hpp
+++ src/cable/shm/cable_shm_addr.hpp
@@ -1,11 +1,13 @@
 #ifndef CABLE_SHM_ADDR_HPP
 #define CABLE_SHM_ADDR_HPP
 
+#include "../base/cable_base_addr.hpp"
 
-class cable_shm_addr
-{
+class cable_shm_addr final : c_cable_base_addr {
 	public:
-		cable_shm_addr();
+		typedef std::string t_addr; ///< actuall raw address. Instace of this will be in m_addrdata
+		cable_shm_addr() = default;
+		cable_shm_addr(const std::string &address);
 };
 
 #endif // CABLE_SHM_ADDR_HPP
