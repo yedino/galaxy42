f15b14b13400e8e9b544827c15c5869ec83e36eb
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-03 10:55:52 +0000

    c_cable_udp: receive_from and listen_on

--- src/cable/udp/cable_udp_obj.cpp
+++ src/cable/udp/cable_udp_obj.cpp
@@ -3,6 +3,13 @@
 
 using namespace boost::asio::ip;
 
+c_cable_udp::c_cable_udp()
+:
+	m_io_service(),
+	m_listen_socket(m_io_service)
+{
+}
+
 void c_cable_udp::send_to(const c_cable_base_addr & dest, const unsigned char *data, size_t size) {
 	auto destination_endpoint = (boost::any_cast<c_cable_udp_addr::t_addr>( dest.get_addrdata() ));
 	udp::socket socket(m_io_service);
@@ -10,9 +17,13 @@ void c_cable_udp::send_to(const c_cable_base_addr & dest, const unsigned char *d
 }
 
 size_t c_cable_udp::receive_from(c_cable_base_addr &source, unsigned char *const data, size_t size) {
-	return 0;
+	udp::endpoint source_endpoint;
+	size_t readed_bytes = m_listen_socket.receive_from(boost::asio::buffer(data, size), source_endpoint);
+	source.init_addrdata(source_endpoint);
+	return readed_bytes;
 }
 
 void c_cable_udp::listen_on(c_cable_base_addr &local_address) {
-
+	udp::endpoint local_endpoint = boost::any_cast<c_cable_udp_addr::t_addr>(local_address.get_addrdata());
+	m_listen_socket.bind(local_endpoint);
 }
