e6a3f482cb25ca260de238f79c9a58a724fcbecc
Rysiek <mikurys@gmail.com>
2017-03-03 19:31:55 +0100

    [bad] operators for cable addr

--- src/cable/shm/cable_shm_addr.hpp
+++ src/cable/shm/cable_shm_addr.hpp
@@ -8,6 +8,11 @@ class cable_shm_addr final : c_cable_base_addr {
 		typedef std::string t_addr; ///< actuall raw address. Instace of this will be in m_addrdata
 		cable_shm_addr() = default;
 		cable_shm_addr(const std::string &address);
+		/// is my address the same (cable type, and address) to another.
+		bool is_same(const c_cable_base_addr &other) const override;
+
+		/// return -1 if I am smaller, 0 if same, +1 if bigger, then the other address. Compares also across different cable-types
+		int compare(const c_cable_base_addr &other) const override;
 };
 
 #endif // CABLE_SHM_ADDR_HPP
--- src/cable/udp/cable_udp_addr.hpp
+++ src/cable/udp/cable_udp_addr.hpp
@@ -13,6 +13,11 @@ class c_cable_udp_addr final : public c_cable_base_addr {
 		c_cable_udp_addr(const t_addr &endpoint);
 		c_cable_udp_addr(const std::string &ip_string); ///< Converts string like "100.200.50.50:32000"
 		void print(std::ostream & ostr) const override;
+		/// is my address the same (cable type, and address) to another.
+		bool is_same(const c_cable_base_addr &other) const override;
+
+		/// return -1 if I am smaller, 0 if same, +1 if bigger, then the other address. Compares also across different cable-types
+		int compare(const c_cable_base_addr &other) const override;
 };
 
 #endif // cable_UDP_ADDR_HPP
