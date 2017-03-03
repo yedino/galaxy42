5524c1f62127d228a5b7f336c092fa6c8bfe2be8
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-03 12:59:23 +0000

    [wip] acync cable functions

--- src/cable/base/cable_base_obj.hpp
+++ src/cable/base/cable_base_obj.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <boost/any.hpp>
+#include <functional>
 #include "cable/base/cable_base_addr.hpp"
 
 class c_cable_base_obj {
@@ -14,6 +15,19 @@ class c_cable_base_obj {
 		virtual void send_to(const c_cable_base_addr & dest, const unsigned char *data, size_t size)=0; ///< block function
 
 		/**
+		 * @brief async_send_to
+		 * @param dest Destination address
+		 * @param data Pointer to buffer of data to send
+		 * @param size Size of buffer
+		 * @param handler The handler to be called when the send operation completes. The function signature of the handler must be:\n
+		 * void handler(\n
+		 * 	const unsigned char *data, // send data pointer, address is equal async_send_to data parameter\n
+		 * 	size_t size // Number of sended bytes\n
+		 * );
+		 */
+		virtual void async_send_to(const c_cable_base_addr & dest, const unsigned char *data, size_t size, std::function<void(const unsigned char *, std::size_t)> handler)=0;
+
+		/**
 		 * @brief receive_from block function
 		 * @param source address of sender
 		 * @param data pointer to prealocated buffer
@@ -21,6 +35,20 @@ class c_cable_base_obj {
 		 * @return number of readed bytes
 		 */
 		virtual size_t receive_from(c_cable_base_addr & source, unsigned char * const data, size_t size)=0;
+
+		/**
+		 * @brief async_receive_from
+		 * @param source address of sender
+		 * @param data pointer to prealocated buffer
+		 * @param size size of prealocated buffer
+		 * @param handler The handler to be called when the receive operation completes. The function signature of the handler must be:\n
+		 * void handler(\n
+		 *	const unsigned char *data, // receive data pointer, address is equal async_receive_from data parameter\n
+		 * 	size_t size // Number of sended bytes\n
+		 * );
+		 */
+		virtual void async_receive_from(unsigned char * const data, size_t size,
+			std::function<void(const unsigned char *, std::size_t, c_cable_base_addr source)> handler)=0;
 
 		virtual void listen_on(c_cable_base_addr & local_address)=0;
 };
