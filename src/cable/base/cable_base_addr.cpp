c24f3fa7a9c533681211d7af0e805d46c315fd16
Rysiek <mikurys@gmail.com>
2017-03-06 18:40:38 +0100

    operators for cable addr

--- src/cable/base/cable_base_addr.cpp
+++ src/cable/base/cable_base_addr.cpp
@@ -1,6 +1,8 @@
 
 #include "libs1.hpp"
 #include "cable/base/cable_base_addr.hpp"
+#include "cable/udp/cable_udp_addr.hpp"
+#include "cable/shm/cable_shm_addr.hpp"
 
 const boost::any & c_cable_base_addr::get_addrdata() const { return m_addrdata ; }
 
@@ -24,42 +26,48 @@ std::ostream & operator<<(std::ostream & ostr , c_cable_base_addr & obj) {
 }
 
 unique_ptr<c_cable_base_addr> c_cable_base_addr::cable_make_addr(const string & str) {
-	size_t pos1 = str.find(':');
-	if (pos1 == string::npos) { // must be udp
-		return std::make_unique<c_cable_udp_addr>(str);
+	try{
+		size_t pos1 = str.find(':');
+		if (pos1 == string::npos) { // try if is udp (format 192.166.218.58)
+			return std::make_unique<c_cable_udp_addr>(join_string(str, ":", get_default_galaxy_port()));
+		}
+		else {
+			_try_user(pos1>0); // string::substr is safe, but anyway test against user doing --peer "-foo"
+			string part1 = str.substr(0,pos1);
+			string part2 = str.substr(pos1+1);
+			if (part1=="auto" || part1=="udp" || part1=="tcp") { //udp for those types of cables
+				return std::make_unique<c_cable_udp_addr>(part2);
+			}
+			else if(part1=="shm"){
+				auto iter = find_if(part2.begin(), part2.end(), [](char c) { return !isalnum(c); });
+				if( iter == part2.end() )
+					return std::make_unique<cable_shm_addr>(part2);
+				else
+					_throw_error( std::invalid_argument(mo_file_reader::gettext("L_bad_peer_format")) );
+			}
+			else // try if is udp (format 192.166.218.58:9042)
+			{
+				return std::make_unique<c_cable_udp_addr>(str);
+			}
+		}
 	}
-	else {
-		_try_user(pos1>0); // string::substr is safe, but anyway test against user doing --peer "-foo"
-		string part1 = str.substr(0,pos1);
-		string part2 = str.substr(pos1, st);
-		if (part)
+	catch (const std::exception &e) {
+		_erro(mo_file_reader::gettext("L_failed_adding_peer_simple_reference") << e.what());
+		_throw_error( std::invalid_argument(mo_file_reader::gettext("L_bad_peer_format")) );
 	}
+
 	unique_ptr<c_cable_base_addr> x;
 	return x;
 }
 
 bool operator==(const c_cable_base_addr & obj1, const c_cable_base_addr & obj2) {
-	// TODO@mik
-	_UNUSED(obj2);
 	return obj1.is_same(obj2);
 }
 
 bool operator<(const c_cable_base_addr & obj1, const c_cable_base_addr & obj2) {
-	// TODO@mik
-	_UNUSED(obj2);
 	return obj1.compare(obj2) < 0;
 }
-/*
-bool c_cable_base_addr::is_same(const c_cable_base_addr &other) const {
-	_UNUSED(other);
-	_NOTREADY();
-	// TODO@mik - replace with abstract =0, when implemented for all existing cables, please
-}
 
-int c_cable_base_addr::compare(const c_cable_base_addr &other) const {
-	_UNUSED(other);
-	_NOTREADY();
-	// TODO@mik - replace with abstract =0, when implemented for all existing cables, please
+int get_default_galaxy_port(){
+	return 9042;
 }
-*/
-
--- src/cable/shm/cable_shm_addr.hpp
+++ src/cable/shm/cable_shm_addr.hpp
@@ -3,11 +3,12 @@
 
 #include "../base/cable_base_addr.hpp"
 
-class cable_shm_addr final : c_cable_base_addr {
+class cable_shm_addr final : public c_cable_base_addr {
 	public:
 		typedef std::string t_addr; ///< actuall raw address. Instace of this will be in m_addrdata
 		cable_shm_addr() = default;
 		cable_shm_addr(const std::string &address);
+		std::string cable_type_name() const override; ///< return name of type of this cable, e.g.: "tcp" "udp" "ETH" "shm"
 		/// is my address the same (cable type, and address) to another.
 		bool is_same(const c_cable_base_addr &other) const override;
 
