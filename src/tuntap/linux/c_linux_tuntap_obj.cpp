ea972dc8d7ad6ca57cd38ccaf9ceb03c661b6a9e
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-09 13:49:19 +0000

    add IFF_NO_PI flag

--- src/tuntap/linux/c_linux_tuntap_obj.cpp
+++ src/tuntap/linux/c_linux_tuntap_obj.cpp
@@ -36,7 +36,7 @@ void c_linux_tuntap_obj::async_receive_from_tun(unsigned char *const data, size_
 
 void c_linux_tuntap_obj::set_tun_parameters(const std::array<uint8_t, 16> &binary_address, int prefix_len, uint32_t mtu) {
 	as_zerofill< ifreq > ifr; // the if request
-	ifr.ifr_flags = IFF_TUN;
+	ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
 	strncpy(ifr.ifr_name, "galaxy%d", IFNAMSIZ);
 	int errcode_ioctl =  ioctl(m_tun_fd, TUNSETIFF, static_cast<void *>(&ifr));
 	_check_sys(errcode_ioctl != -1);
