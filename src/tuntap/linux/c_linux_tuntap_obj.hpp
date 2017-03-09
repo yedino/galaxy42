02272c250527095d6ad897244474b9f047496312
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-09 13:26:16 +0000

    linux tun write and read

--- src/tuntap/linux/c_linux_tuntap_obj.hpp
+++ src/tuntap/linux/c_linux_tuntap_obj.hpp
@@ -1,6 +1,8 @@
 #ifndef C_LINUX_TUNTAP_OBJ_HPP
 #define C_LINUX_TUNTAP_OBJ_HPP
 
+#ifdef __linux__
+
 #include "../base/tuntap_base.hpp"
 #include <libs0.hpp>
 
@@ -9,7 +11,7 @@ class c_linux_tuntap_obj final : public tuntap_base_obj {
 		c_linux_tuntap_obj();
 		size_t send_to_tun(const unsigned char *data, size_t size) override;
 		size_t read_from_tun(unsigned char * const data, size_t size) override;
-		size_t async_receive_from_tun(unsigned char * const data, size_t size, read_handler handler) override;
+		void async_receive_from_tun(unsigned char * const data, size_t size, read_handler handler) override;
 		void set_tun_parameters
 			(const std::array<uint8_t, 16> &binary_address, int prefix_len, uint32_t mtu) override;
 
@@ -19,5 +21,7 @@ class c_linux_tuntap_obj final : public tuntap_base_obj {
 		boost::asio::posix::stream_descriptor m_tun_stream;
 
 };
+
+#endif // __linux__
 
 #endif // C_LINUX_TUNTAP_OBJ_HPP
