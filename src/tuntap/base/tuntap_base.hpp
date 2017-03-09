cac4ea75010db31dfc4ad4dfced2177351c55109
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-09 13:16:15 +0000

    [wip] c_linux_tuntap_obj

--- src/tuntap/base/tuntap_base.hpp
+++ src/tuntap/base/tuntap_base.hpp
@@ -9,11 +9,11 @@ class tuntap_base_obj {
 	public:
 		using read_handler = const std::function<void(const unsigned char *, std::size_t)> &;
 		virtual ~tuntap_base_obj() = default;
-		virtual size_t send_to_tun(const unsigned char *data, size_t size) = 0;
-		virtual size_t read_from_tun(unsigned char * const data, size_t size) = 0;
+		virtual size_t send_to_tun(const unsigned char *data, size_t size) = 0; ///< block function
+		virtual size_t read_from_tun(unsigned char * const data, size_t size) = 0; ///< block function
 		virtual size_t async_receive_from_tun(unsigned char * const data, size_t size, read_handler handler) = 0;
 		virtual void set_tun_parameters
-			(const std::array<uint8_t, 16> &binary_address, int prefixLen, uint32_t mtu) = 0;
+			(const std::array<uint8_t, 16> &binary_address, int prefix_len, uint32_t mtu) = 0;
 };
 
 #endif // TUNTAP_BASE_HPP
---
+++ src/tuntap/linux/c_linux_tuntap_obj.hpp
@@ -0,0 +1,23 @@
+#ifndef C_LINUX_TUNTAP_OBJ_HPP
+#define C_LINUX_TUNTAP_OBJ_HPP
+
+#include "../base/tuntap_base.hpp"
+#include <libs0.hpp>
+
+class c_linux_tuntap_obj final : public tuntap_base_obj {
+	public:
+		c_linux_tuntap_obj();
+		size_t send_to_tun(const unsigned char *data, size_t size) override;
+		size_t read_from_tun(unsigned char * const data, size_t size) override;
+		size_t async_receive_from_tun(unsigned char * const data, size_t size, read_handler handler) override;
+		void set_tun_parameters
+			(const std::array<uint8_t, 16> &binary_address, int prefix_len, uint32_t mtu) override;
+
+	private:
+		const int m_tun_fd;
+		boost::asio::io_service m_io_service;
+		boost::asio::posix::stream_descriptor m_tun_stream;
+
+};
+
+#endif // C_LINUX_TUNTAP_OBJ_HPP
