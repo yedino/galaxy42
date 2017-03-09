685db6abef733651369547d658b5da1f39db423f
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-03-09 16:27:02 +0000

    cable: [doc] and error code in handler

--- src/tuntap/base/tuntap_base.hpp
+++ src/tuntap/base/tuntap_base.hpp
@@ -4,14 +4,27 @@
 #include <array>
 #include <cstddef>
 #include <functional>
+#include <boost/system/system_error.hpp> // for the error_code
 
+/// Abstract class for API of tuntap driver, that allows to set own IP, and then send data to TUN/TAP, and read from it
+/// Errors are either thrown, or reported to callere-provided handler (if the error happens async.)
 class tuntap_base_obj {
 	public:
-		using read_handler = const std::function<void(const unsigned char *, std::size_t)> &;
+		// the callback function that caller can provide
+		using read_handler = std::function<void(const unsigned char *, std::size_t, const boost::system::error_code& error)>;
+
 		virtual ~tuntap_base_obj() = default;
-		virtual size_t send_to_tun(const unsigned char *data, size_t size) = 0; ///< block function
-		virtual size_t read_from_tun(unsigned char * const data, size_t size) = 0; ///< block function
-		virtual void async_receive_from_tun(unsigned char * const data, size_t size, read_handler handler) = 0;
+
+		virtual size_t send_to_tun(const unsigned char *data, size_t size) = 0; ///< blocking function
+		// it seems that async send was slow.
+
+		virtual size_t read_from_tun(unsigned char * const data, size_t size) = 0; ///< blocking function
+
+		// receive data to provided buffer, and then callback. Buffer must be valid up untill callback is called
+		// then the callback is allowed to e.g. deallocate (or reuse) it.
+		virtual void async_receive_from_tun(unsigned char * const data, size_t size, const read_handler & handler) = 0;
+
+		// sets the parameters of our tuntap
 		virtual void set_tun_parameters
 			(const std::array<uint8_t, 16> &binary_address, int prefix_len, uint32_t mtu) = 0;
 };
