4dcec1fc1d98d6aed747a278d72c5d03de476ff0
Robert Oleksy <robertoleksy6@gmail.com>
2017-03-16 13:10:14 +0000

    copy private function into c_tuntap_windows

--- src/tuntap/windows/c_tuntap_windows.hpp
+++ src/tuntap/windows/c_tuntap_windows.hpp
@@ -17,6 +17,40 @@ class c_tuntap_windows final : c_tuntap_base_obj {
 
 		void set_tun_parameters
 			(const std::array<unsigned char, 16> &binary_address, int prefix_len, uint32_t mtu) override;
+
+	private:
+		boost::asio::io_service m_ioservice;
+		boost::asio::windows::stream_handle m_stream_handle; ///< boost handler to the TUN device
+		std::wstring m_guid;
+		std::array<uint8_t, 6> m_mac_address;
+
+		std::vector<std::wstring> get_subkeys(HKEY hKey); ///< for windows registry
+		std::wstring get_device_guid(); ///< technical name of the device
+		std::wstring get_human_name(const std::wstring &guid);
+		NET_LUID get_luid(const std::wstring &human_name);
+		HANDLE get_device_handle();
+		HANDLE open_tun_device(const std::wstring &guid); ///< returns opened handle for guid or INVALID_HANDLE_VALUE
+		std::array<uint8_t, 6> get_mac(HANDLE handle); ///< get handle to opened device (returned by get_device_handle())
+
+		class hkey_wrapper final {
+			public:
+				/**
+				 * hkey must have been opened by the RegCreateKeyEx, RegCreateKeyTransacted,
+				 * RegOpenKeyEx, RegOpenKeyTransacted, or RegConnectRegistry function.
+				 */
+				hkey_wrapper(HKEY hkey);
+				~hkey_wrapper();
+				HKEY &get();
+				/**
+				 * hkey must have been opened by the RegCreateKeyEx, RegCreateKeyTransacted,
+				 * RegOpenKeyEx, RegOpenKeyTransacted, or RegConnectRegistry function.
+				 */
+				void set(HKEY new_hkey);
+				void close();
+			private:
+				HKEY m_hkey;
+				bool m_is_open;
+  };
 };
 
 #endif // _WIN32 || __CYGWIN__
