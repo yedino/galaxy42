43f6fa5996e24bcce1548ef6454412f79c65a34b
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-03-20 16:48:48 +0000

    Receive UDP packets, into TUN

--- src/cable/udp/cable_udp_obj.cpp
+++ src/cable/udp/cable_udp_obj.cpp
@@ -11,14 +11,16 @@ c_cable_udp::c_cable_udp(shared_ptr<c_asioservice_manager> iomanager)
 	m_read_socket(get_io_service(), boost::asio::ip::udp::v4()),
 	m_write_socket(get_io_service(), boost::asio::ip::udp::v4())
 {
-	_note("Created UDP card");
+	_note("Created UDP card: \n"
+		<< "  Read socket:  open="<< m_read_socket.is_open() << " native="<<m_read_socket.native_handle() << "\n"
+		<< "  Write socket: open="<< m_write_socket.is_open() << " native="<<m_write_socket.native_handle());
 }
 
 void c_cable_udp::send_to(const c_cable_base_addr & dest, const unsigned char *data, size_t size) {
-	_dbg3("Seding UDP size=" << size); // TODO print dest
+	_dbg3("Seding (blocking) UDP size=" << size << " dest=" << dest);
 	try {
 		udp::endpoint destination_endpoint = (boost::any_cast<c_cable_udp_addr::t_addr>( dest.get_addrdata() ));
-		_dbg3("UDP to " << destination_endpoint);
+		_dbg4("UDP to " << destination_endpoint);
 		m_write_socket.send_to(boost::asio::buffer(data, size), destination_endpoint);
 	} catch(...) {
 		_warn("Can not send UDP");
@@ -27,24 +29,38 @@ void c_cable_udp::send_to(const c_cable_base_addr & dest, const unsigned char *d
 }
 
 void c_cable_udp::async_send_to(const c_cable_base_addr &dest, const unsigned char *data, size_t size, write_handler handler) {
-	udp::endpoint destination_endpoint = (boost::any_cast<c_cable_udp_addr::t_addr>( dest.get_addrdata() ));
-	m_write_socket.async_send_to(boost::asio::buffer(data, size), destination_endpoint,
-		// handler cannot be moved in lambda capture list (MSVC compilation problems)
-		[handler, data](const boost::system::error_code& error, std::size_t bytes_transferred) {
-			_UNUSED(error);
-			handler(data, bytes_transferred);
-		} // lambda
-	);
+	_dbg3("Seding (asyncblocking) UDP size=" << size << " dest=" << dest);
+	try {
+		udp::endpoint destination_endpoint = (boost::any_cast<c_cable_udp_addr::t_addr>( dest.get_addrdata() ));
+		m_write_socket.async_send_to(boost::asio::buffer(data, size), destination_endpoint,
+			// handler cannot be moved in lambda capture list (MSVC compilation problems)
+			[handler, data](const boost::system::error_code& error, std::size_t bytes_transferred) {
+				_UNUSED(error);
+				handler(data, bytes_transferred);
+			} // lambda
+		);
+	} catch(...) {
+		_warn("Can not send UDP");
+		throw;
+	}
 }
 
 size_t c_cable_udp::receive_from(c_cable_base_addr &source, unsigned char *const data, size_t size) {
-	udp::endpoint source_endpoint;
-	size_t readed_bytes = m_read_socket.receive_from(boost::asio::buffer(data, size), source_endpoint);
-	source.init_addrdata(source_endpoint);
-	return readed_bytes;
+	_dbg3("Receive (blocking) UDP");
+	try {
+		udp::endpoint source_endpoint;
+		size_t readed_bytes = m_read_socket.receive_from(boost::asio::buffer(data, size), source_endpoint);
+		source.init_addrdata(source_endpoint);
+		return readed_bytes;
+	} catch(...) {
+		_warn("Can not receive UDP");
+		throw;
+	}
 }
 
 void c_cable_udp::async_receive_from(unsigned char *const data, size_t size, read_handler handler) {
+	_dbg3("Receive (asyn) UDP");
+
 	auto endpoint_iterator = [this] {
 		std::unique_lock<std::mutex> lock(m_enpoint_list_mutex);
 		m_endpoint_list.emplace_back();
@@ -57,7 +73,7 @@ void c_cable_udp::async_receive_from(unsigned char *const data, size_t size, rea
 		[this, handler, data, endpoint_iterator]
 		(const boost::system::error_code& error, std::size_t bytes_transferred)
 		{
-			_UNUSED(error);
+			_UNUSED(error);	// TODO: handler error - do not run handler then?
 			std::unique_ptr<c_cable_base_addr> source_addr_cable = std::make_unique<c_cable_udp_addr>( *endpoint_iterator );
 			std::unique_lock<std::mutex> lock(m_enpoint_list_mutex);
 			m_endpoint_list.erase(endpoint_iterator);
@@ -69,6 +85,7 @@ void c_cable_udp::async_receive_from(unsigned char *const data, size_t size, rea
 }
 
 void c_cable_udp::listen_on(c_cable_base_addr &local_address) {
+	_note("Listening on " << local_address );
 	udp::endpoint local_endpoint = boost::any_cast<c_cable_udp_addr::t_addr>(local_address.get_addrdata());
 	m_read_socket.bind(local_endpoint);
 }
