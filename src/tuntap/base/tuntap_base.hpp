e69c3b879bdb7830e5370e1d52b0035ad96d72ba
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-03-21 13:27:24 +0100

    [add] tuntap and NetPlatform custom errors
    to tuntap_base files

--- src/tuntap/base/tuntap_base.hpp
+++ src/tuntap/base/tuntap_base.hpp
@@ -6,6 +6,23 @@
 #include <functional>
 #include <boost/system/system_error.hpp> // for the error_code
 
+#include "error_subtype.hpp"
+
+
+std::string errno_to_string(int errno_copy); ///< Convert errno from C-lib into a string. Thread-safe function.
+
+class tuntap_error : public runtime_error_subtype {};
+class tuntap_error_devtun : public tuntap_error {};
+class tuntap_error_ip     : public tuntap_error {};
+class tuntap_error_mtu    : public tuntap_error {};
+
+/// @return error-code-netplatform as a human (developer) string (not translated, it's tech detail)
+/// this translates the field t_syserr.my_code only, see other function for more
+std::string NetPlatform_error_code_to_string(int err);
+
+/// @return error-code-netplatform as a human (developer) string (not translated, it's tech detail)
+std::string NetPlatform_syserr_to_string(t_syserr syserr);
+
 /// Abstract class for API of tuntap driver, that allows to set own IP, and then send data to TUN/TAP, and read from it
 /// Errors are either thrown, or reported to callere-provided handler (if the error happens async.)
 class c_tuntap_base_obj {
