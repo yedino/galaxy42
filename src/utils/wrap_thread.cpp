b47edd9e4d9877bc26efba05b40562a15b1fb7a2
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-03-24 11:51:50 +0100

    noexcept specifier

--- src/utils/wrap_thread.cpp
+++ src/utils/wrap_thread.cpp
@@ -1,14 +1,14 @@
 #include "wrap_thread.hpp"
 
-wrap_thread::wrap_thread() { }
-
-wrap_thread::wrap_thread(wrap_thread &&rhm) {
-	if(m_thr.joinable())
-		m_thr.join();
-	m_thr = std::move(rhm.m_thr);
+wrap_thread::wrap_thread(wrap_thread &&rhm) noexcept {
+	if(&rhm != this) {
+		if(m_thr.joinable())
+			m_thr.join();
+		m_thr = std::move(rhm.m_thr);
+	}
 }
 
-wrap_thread &wrap_thread::operator=(wrap_thread &&rhm) {
+wrap_thread &wrap_thread::operator=(wrap_thread &&rhm) noexcept {
 	if(&rhm == this) {
 		return *this;
 	}
@@ -18,11 +18,11 @@ wrap_thread &wrap_thread::operator=(wrap_thread &&rhm) {
 	return *this;
 }
 
-bool wrap_thread::joinable() const {
+bool wrap_thread::joinable() const noexcept {
 	return m_thr.joinable();
 }
 
-std::thread::id wrap_thread::get_id() const {
+std::thread::id wrap_thread::get_id() const noexcept {
 	return m_thr.get_id();
 }
 
@@ -30,7 +30,7 @@ void wrap_thread::join() {
 	m_thr.join();
 }
 
-void wrap_thread::swap(wrap_thread &other) {
+void wrap_thread::swap(wrap_thread &other) noexcept {
 	m_thr.swap(other.m_thr);
 }
 
