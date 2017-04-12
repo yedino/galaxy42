9b86f4fdc467a1d9d947d9073ea2c8c1a279144a
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-04-12 18:07:47 +0200

    wrap_thread changed

--- src/utils/wrap_thread.cpp
+++ src/utils/wrap_thread.cpp
@@ -38,22 +38,57 @@ void wrap_thread::join() {
 	if(!m_future.valid())
 		return;
 	m_time_stopped = t_clock::now();
-	if (m_destroy_timeout == std::chrono::seconds(0))
-		m_future.get();
+	_info(info());
+	if (m_destroy_timeout == std::chrono::seconds(0)) {
+		_erro("wrap_thread with not set time should be joined manualy using try_join()!");
+		std::abort();
+	}
 	else {
 		std::future_status status = m_future.wait_for(m_destroy_timeout);
 		if (status != std::future_status::ready) {
 			_erro("can not end thread in given time");
-			_info(info());
 			std::abort();
 		}
 		m_future.get();
 	}
+}
 
+bool wrap_thread::try_join(std::chrono::duration<double> duration) {
+	std::chrono::seconds dest_time = std::chrono::duration_cast<std::chrono::seconds>(duration);
+
+	if(!m_future.valid())
+		return true;
+
+	std::future_status status = m_future.wait_for(dest_time);
+	if (status != std::future_status::ready) {
+		_warn("try_join: can not end thread in given time");
+		return true;
+	} else {
+		m_time_stopped = t_clock::now();
+		_info("Successfull joined wrap_thread");
+		_info(info());
+		m_future.get();
+		return false;
+	}
+	return true;
 }
 
 wrap_thread::~wrap_thread()  {
-	join();
+
+	m_time_stopped = t_clock::now();
+	_info(info());
+
+	if(!m_future.valid())
+		return;
+
+	if (m_destroy_timeout != std::chrono::seconds(0)) {
+		std::future_status status = m_future.wait_for(m_destroy_timeout);
+		if (status != std::future_status::ready) {
+			_erro("can not end thread in given time");
+			std::abort();
+		}
+		m_future.get();
+	}
 }
 
 std::string wrap_thread::info() const {
