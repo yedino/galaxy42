dc57af89386c01a0a81ebe584e30355c6a4eabfc
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-04-18 12:15:08 +0200

    unit_tess for wrap_thread

--- src/test/utils_wrap_thread.cpp
+++ src/test/utils_wrap_thread.cpp
@@ -5,7 +5,8 @@
 
 #include "../utils/wrap_thread.hpp"
 #include "../libs0.hpp"
-/*
+
+
 TEST(utils_wrap_thread, simple_thread_replace) {
 	// we create an empty thread and immediately replace it with another one
 	wrap_thread wt;
@@ -24,19 +25,25 @@ TEST(utils_wrap_thread, thread_at_throws_in_middle_of_move) {
 	std::atomic<bool> endflag{false};
 	_mark("Thread ...");
 
-	EXPECT_DEATH( vec.at(1) = std::move(wrap_thread(std::chrono::seconds(2), [&](){
+	EXPECT_DEATH( vec.at(1) = wrap_thread(std::chrono::seconds(2), [&](){
 		while(!endflag){}
-	} )), ""); // catch std::abort
+	} ), ""); // catch std::abort
 
 	endflag=true;
 	EXPECT_THROW( vec.at(1) , std::out_of_range );
-	EXPECT_THROW( vec.at(1).join() , std::out_of_range );
+	EXPECT_THROW( vec.at(1).try_join(std::chrono::seconds(1)) , std::out_of_range );
 	// we would have a problem here with normal std::thread, but here it will join itself
 }
 
-void fun0() {}
-void fun1(int) {}
-void fun2(int, int) {}
+void fun0() {
+	_info("fun0");
+}
+void fun1(int i) {
+	_info("fun1 arg: " << i);
+}
+void fun2(int i, int j) {
+	_info("fun2 args: " << i << ',' << j);
+}
 
 TEST(utils_wrap_thread, different_construct) {
 
@@ -47,18 +54,40 @@ TEST(utils_wrap_thread, different_construct) {
 	wrap_thread local_wrap_thread3(std::move(local_wrap_thread2)); // move constr
 	// from now local_wrap_thread2 is empty!
 
+	// try_join
+	local_wrap_thread.try_join(std::chrono::seconds(1));
+	local_wrap_thread3.try_join(std::chrono::seconds(1));
 }
 
-TEST(utils_wrap_thread, empty_functions) {
+// should not abort
+TEST(utils_wrap_thread, destruct_join) {
+
+	wrap_thread local_wrap_thread1(std::chrono::seconds(3),fun1, 3);
+}
+
+TEST(utils_wrap_thread, try_join) {
+
+	wrap_thread local_wrap_thread1(fun1, 3);
+	local_wrap_thread1.try_join(std::chrono::seconds(1));
+}
+
+TEST(utils_wrap_thread, simple_functions) {
 
 	wrap_thread local_wrap_thread1(fun0);
 	wrap_thread local_wrap_thread2(fun1, 3);
 	wrap_thread local_wrap_thread3(fun2, 4, 7);
+
+	local_wrap_thread1.try_join(std::chrono::seconds(1));
+	local_wrap_thread2.try_join(std::chrono::seconds(1));
+	local_wrap_thread3.try_join(std::chrono::seconds(1));
 }
 
-TEST(utils_wrap_thread, manual_join) {
+TEST(utils_wrap_thread, abort_not_joined) {
 
-	wrap_thread local_wrap_thread1(fun1, 3);
-	local_wrap_thread1.join();
+	EXPECT_DEATH({
+		{
+			wrap_thread local_wrap_thread1(fun0);
+		}
+	}
+	, ""); // catch std::abort
 }
-*/
