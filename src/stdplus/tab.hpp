58b3f78cde53ffaebf31207f1b222fc455cc48c2
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-04-19 19:12:53 +0000

    draft copy_safe_apart + some tests

--- src/stdplus/tab.hpp
+++ src/stdplus/tab.hpp
@@ -9,8 +9,47 @@ stdplus/tab focuses on containers support
 @todo TODO@testcase - test cases for all functions here
 */
 
+#include <memory>
+#include <utils/check.hpp>
+#include <utils/misc.hpp>
+
 
 namespace stdplus {
+
+
+/*
+Copies n elements from tab_src container (starting from offset_src)
+into tab_dst table (starting in it at index offset_dst).
+Both containers must be apart (that is: they must not overlap  at least in the range being copied).
+Both containers must be SequenceContainer and also ContiguousIterator(as defined for C++17) - e.g. std::vector<> will work.
+If conditions are not meet, then function at most throws exception (hard _check error),
+how ever it can also do nothing if given invalid condition was not needed anyway (e.g. for n==0).
+*/
+template <class T1, class T2>
+typename T2::iterator copy_safe_apart(size_t n, const T1 & tab_src, T2 & tab_dst, size_t offset_src=0, size_t offset_dst=0) {
+	if (n<1) return tab_dst.begin();
+
+	// source
+	auto src_rb = tab_src.begin() + offset_src; // rb = range begin
+	_check_input( offset_src < tab_src.size() );
+	_check_input( n <= tab_src.size() - offset_src ); // subtracting offset_src is valid since above
+	auto src_re = tab_src.begin() + offset_src + n; // range end
+	_check_abort( src_re <= tab_src.end() );
+
+	// dest
+	auto dst_rb = tab_dst.begin() + offset_dst; // rb = range begin
+	_check_input( offset_dst < tab_dst.size() );
+	_check_input( n <= tab_dst.size() - offset_dst ); // subtracting offset_dst is valid since above
+	auto dst_re = tab_dst.begin() + offset_dst + n; // range end
+	_check_abort( dst_re <= tab_dst.end() );
+
+	bool overlap = ranges_overlap_oc_ne_asserted(src_rb, src_re,  dst_rb, dst_re);
+	_check_input(!overlap);
+
+	copy_and_assert_no_overlap_size( src_rb, src_re,  dst_rb, n );
+
+	return dst_rb;
+}
 
 
 
--- src/test/stdplus_tab.cpp
+++ src/test/stdplus_tab.cpp
@@ -1,17 +1,51 @@
 #include "gtest/gtest.h"
+
 #include <iostream>
-#include <stdplus/tab.hpp>
 #include <vector>
 #include <map>
 #include <list>
+#include <string>
+#include <array>
 
-TEST(stdplus_tab, basicuse) {
+#include <stdplus/tab.hpp>
 
-	std::vector<int> tab1;
+template <class TAB1, class TAB2>
+void testcase_vectorlike() {
+	TAB1 tab1;
 	tab1.push_back(42);
 	tab1.push_back(100);
-	vector<int> tab2;
+	TAB2 tab2;
+	tab2.resize(2);
+
+	stdplus::copy_safe_apart(2, tab1, tab2);
+	auto it1=tab1.begin();
+	auto it2=tab2.begin();
+	for (size_t i=0; i<2; ++i) {
+		EXPECT_EQ( *it1, *it2 );
+		++it1;
+		++it2;
+	}
+}
+
+template <class TAB1, class TAB2>
+void testcase_arraylike(const TAB1& tab1, TAB2& tab2) {
+	stdplus::copy_safe_apart(2, tab1, tab2);
+	auto it1=tab1.begin();
+	auto it2=tab2.begin();
+	for (size_t i=0; i<2; ++i) {
+		EXPECT_EQ( *it1, *it2 );
+		++it1;
+		++it2;
+	}
+}
 
 
+
+TEST(stdplus_tab, basicuse) {
+	// testcase< std::vector<int> , std::vector<int> > ( ) ;
+	testcase_vectorlike<  std::vector<int> , std::vector<int>  > ( ) ;
+
+	std::array<int,2> arr1,arr2;
+	testcase_arraylike(arr1,arr2);
 }
 
