fc85d1eb246b64675f70f755e0b2b2dc3e5d75fb
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-04-19 18:00:51 +0000

    [wip] unit tests for stdplus/tab

---
+++ src/test/stdplus_tab.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include <iostream>
+#include <stdplus/tab.hpp>
+#include <vector>
+#include <map>
+#include <list>
+
+TEST(stdplus_tab, basicuse) {
+
+	std::vector<int> tab1;
+	tab1.push_back(42);
+	tab1.push_back(100);
+	vector<int> tab2;
+
+
+}
+
