e7b67878f03434c6757fb5136e7807135a193dc3
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-04-20 13:06:36 +0000

    [ok] tests for the stdplus::copy_safe_apart

--- src/test/stdplus_tab.cpp
+++ src/test/stdplus_tab.cpp
@@ -4,27 +4,46 @@
 #include <vector>
 #include <map>
 #include <list>
+#include <set>
 #include <string>
 #include <array>
 
 #include <stdplus/tab.hpp>
 
 template <class TAB1, class TAB2>
-void testcase_vectorlike() {
+void testcase_vectorlike_copyall(size_t n) {
 	TAB1 tab1;
-	tab1.push_back(42);
-	tab1.push_back(100);
-	TAB2 tab2;
-	tab2.resize(2);
+	for (size_t i=0; i<n; ++i) tab1.push_back(1000+i);
 
-	stdplus::copy_safe_apart(2, tab1, tab2);
+	TAB2 tab2;
+	tab2.resize(n);
+
+	stdplus::copy_safe_apart(n, tab1, tab2);
 	auto it1=tab1.begin();
 	auto it2=tab2.begin();
-	for (size_t i=0; i<2; ++i) {
+	for (size_t i=0; i<n; ++i) {
 		EXPECT_EQ( *it1, *it2 );
 		++it1;
 		++it2;
 	}
+}
+
+template <class TAB1, class TAB2>
+void testcase_vectorlike_offset2(size_t size1, size_t size2, size_t n, size_t off1, size_t off2) {
+	_info("Test size1="<<std::setw(4)<<size1<<" size2="<<size2<<" n="<<n<<" off1="<<off1<<" off2="<<off2);
+	TAB1 tab1;
+	typedef typename TAB1::value_type t_value;
+	const t_value test1=1000, test2=2000; // test values
+	for (size_t i=0; i<size1; ++i) { t_value iv = static_cast<t_value>(i);  tab1.push_back(test1+iv); }
+
+	TAB2 tab2;
+	for (size_t i=0; i<size2; ++i) { t_value iv = static_cast<t_value>(i);  tab2.push_back(test2+iv); }
+
+	stdplus::copy_safe_apart(n, tab1, tab2, off1, off2);
+	for (size_t i=0; i<size1; ++i) { t_value iv = static_cast<t_value>(i);  EXPECT_EQ( tab1.at(i) , test1+iv ); } // source is unchanged
+	for (size_t i=0; i<off2; ++i) { t_value iv = static_cast<t_value>(i);  EXPECT_EQ( tab2.at(i) , test2+iv ); } // dest part before - is unchanged
+	for (size_t i=off2; i<n; ++i) { t_value iv = static_cast<t_value>(i);  EXPECT_EQ( tab2.at(i) , tab1.at(i-off2+off1) );  UNUSED(iv); } // dest overwritten
+	for (size_t i=off2+n; i<size2; ++i) { t_value iv = static_cast<t_value>(i);  EXPECT_EQ( tab2.at(i) , test2+iv ); } // dest part after - is unchanged
 }
 
 template <class TAB1, class TAB2>
@@ -40,12 +59,53 @@ void testcase_arraylike(const TAB1& tab1, TAB2& tab2) {
 }
 
 
+template<class T>
+T fuzzy_plus_minus(typename T::value_type n, const T & tab) {
+	T ret;
+	for (const auto v: tab) {
+		for (long long int i=-n; i<+n; i++) {
+			ret.insert( v+i );
+		}
+	}
+	return ret;
+}
+
 
 TEST(stdplus_tab, basicuse) {
 	// testcase< std::vector<int> , std::vector<int> > ( ) ;
-	testcase_vectorlike<  std::vector<int> , std::vector<int>  > ( ) ;
+	testcase_vectorlike_copyall<  std::vector<int> , std::vector<int>  > ( 10 ) ;
 
 	std::array<int,2> arr1,arr2;
 	testcase_arraylike(arr1,arr2);
+}
+
+
+template <class TTab1, class TTab2>
+void tests_loop() {
+	size_t test_count=0;
+	for (const auto size1 : std::set<long int>{ 0, 1, 2, 3, 4, 5, 10 }) {
+		for (const auto size2 : fuzzy_plus_minus(2, std::set<long int>{ 0, 1, 2, 3, 4, 5, 10, size1/2, size1 } ) ) {
+			if (! (size2>=0)) continue;
+			for (const auto num : fuzzy_plus_minus(2, std::set<long int>{ 0, 1, 5, 10, size1/2, size1 } ) ) {
+				if (! (num>=0)) continue;
+				for (const auto off1 : fuzzy_plus_minus(2, std::set<long int>{ 0, 1, 5, 10, size1/2, size1 } ) ) {
+					if (! (off1>=0)) continue;
+					if (! ((num+off1)<=size1)) continue; // we use a valid range inside source container
+					for (const auto off2 : fuzzy_plus_minus(2, std::set<long int>{ 0, 1, 5, 10, size1/2, size1, off1, off1/2 } ) ) {
+						if (! (off2>=0)) continue;
+						if (! ((num+off2)<=size2)) continue; // we use a valid range inside destination container
+						++test_count;
+						testcase_vectorlike_offset2<  TTab1 , TTab2  > ( size1 , size2, num,  off1 , off2 ) ;
+					}
+				}
+			}
+		}
+	}
+	_fact("Executed tests count: " << test_count );
+}
+
+TEST(stdplus_tab, loop_vector) {
+	tests_loop< std::vector<int> , std::vector<int> >();
+	// tests_loop< std::vector<int> , std::vector<long int> >(); // TODO@rfree
 }
 
