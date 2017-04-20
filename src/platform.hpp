a6840efa31758603d799b3c23a72e6c8c710cac5
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-04-20 14:33:38 +0200

    update ATTR_NODISCARD in platform.hpp - (msvc and osx-clang)

--- src/platform.hpp
+++ src/platform.hpp
@@ -1,4 +1,8 @@
 #pragma once
+
+// comment
+// in c++17 ATTR_NODISCARD is in standard: [[nodiscard]]
+
 #if __linux__
 	#define ANTINET_linux
 	#define ATTR_NODISCARD __attribute__((warn_unused_result))
@@ -6,6 +10,7 @@
 	#define ANTINET_windows
 	#define UNICODE
 	#define _UNICODE
+	#define ATTR_NODISCARD _Check_return_
 	#if defined(__CYGWIN__)
 		//http://www.boost.org/doc/libs/1_61_0/doc/html/boost_asio/using.html
 		#ifndef __USE_W32_SOCKETS
@@ -14,6 +19,7 @@
 	#endif
 #elif (__MACH__)
 	#define ANTINET_macosx
+	#define ATTR_NODISCARD __attribute__((warn_unused_result))
 #else
 	#error Platform is not supported
 #endif
