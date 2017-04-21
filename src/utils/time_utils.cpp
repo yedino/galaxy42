541babefccffa0beb4179dcc50ab5d545df665b7
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-04-21 13:27:20 +0200

    update time_utils, add exact tests

--- src/utils/time_utils.cpp
+++ src/utils/time_utils.cpp
@@ -3,25 +3,74 @@
 #include <iomanip>
 #include <ctime>
 
-std::string time_utils::timepoint_to_readable(const time_utils::t_timepoint &tp) {
-	std::time_t time = std::chrono::system_clock::to_time_t(tp);
-	return timepoint_to_readable(time);
+
+time_t time_utils::gen_exact_date(int year,
+                      int month,
+                      int day,
+                      int hour,
+                      int min,
+                      int sec,
+                      int isdst) {
+
+	time_t ret;
+	std::tm tm;
+
+	// -1900 because tm::tm_year starts from 1900
+	tm.tm_year = year - 1900;
+	// -1 because tm::tm_mon is in range [0-11]
+	tm.tm_mon = month -1;
+	tm.tm_mday = day;
+
+	// hour is shifted by 1
+	tm.tm_hour = hour + 1;
+	tm.tm_min = min;
+	tm.tm_sec = sec;
+
+	// Daylight Saving Time flag. The value is positive if DST is in effect
+	tm.tm_isdst=isdst;
+	ret = mktime(&tm);
+	return ret;
 }
 
-std::string time_utils::timepoint_to_readable(const std::time_t &time) {
+std::string time_utils::timepoint_to_readable(const time_utils::t_timepoint &tp, const std::string &zone) {
+	std::time_t time = std::chrono::system_clock::to_time_t(tp);
+	return time_t_to_readable(time, zone);
+}
+
+std::string time_utils::time_t_to_readable(const std::time_t &time, const std::string &zone) {
 	std::stringstream ss;
 	ss << std::put_time(std::gmtime(&time), "%FT%T");
 
-	ss << time_utils::get_zone_iso8601(time);
+	ss << time_utils::get_zone_offset(time, zone);
 
 	return ss.str();
 }
 
-std::string time_utils::get_zone_iso8601(const std::time_t &time) {
-	std::stringstream zone;
-	zone << std::put_time(std::localtime(&time), "%z");
+std::string time_utils::get_zone_offset(const std::time_t &time, const std::string &zone) {
 
-	std::string zone_str = zone.str();
+	// save actual // char* because getenv could return null
+	char* ctz(getenv("TZ"));
+
+	// set variable zone
+	if(zone.empty()) {
+		setenv("TZ", "", 1);
+	} else {
+		setenv("TZ", zone.c_str(), 1);
+	}
+	tzset();
+
+	std::stringstream zone_stream;
+	zone_stream << std::put_time(std::localtime(&time), "%z");
+
+	// back to actual
+	if(ctz != NULL) {
+		setenv("TZ", ctz, 1);
+	} else {
+		unsetenv("TZ");
+	}
+	tzset();
+
+	std::string zone_str = zone_stream.str();
 	zone_str.insert(zone_str.end()-2,':');
 	return zone_str;
 }
--- src/utils/time_utils.hpp
+++ src/utils/time_utils.hpp
@@ -9,16 +9,43 @@ namespace time_utils {
 	using t_sysclock = std::chrono::system_clock;  ///< clock I will use for my timing
 	using t_timepoint = std::chrono::time_point<t_sysclock>; ////< timepoint I will use for my timing
 
+	/**
+	 * generate exact date, with optional daylight time saving (isdst)
+	 * final arguments values:
+	 * @param year years from 0, could be negative
+	 * @param month months – [1, 12]
+	 * @param day day of the month – [1, 31]
+	 * @param hour hours since midnight – [0, 23]
+	 * @param min minutes after the hour – [0, 59]
+	 * @param sec seconds after the minute – [0, 61](until C++11) / [0, 60] (since C++11)
+	 */
+	time_t gen_exact_date(int year,
+	                      int month,
+	                      int day,
+	                      int hour,
+	                      int min,
+	                      int sec,
+	                      int isdst = 0);
+
 	/// convert t_timepoint to readable date in accordance with ISO 8601 - Combined date and time in UTC
-	std::string timepoint_to_readable(const t_timepoint &tp);
+	std::string timepoint_to_readable(const t_timepoint &tp, const std::string &zone = "");
 
-	/// timepoint_to_readable overload for std::time_t
-	std::string timepoint_to_readable(const std::time_t &time);
+	/// convert time_t to readable date in accordance with ISO 8601 - Combined date and time in UTC
+	std::string time_t_to_readable(const std::time_t &time, const std::string &zone = "");
 
-	/// change zone offset format accordinig to ISO 8601:
-	/// +0200->+02:00
-	/// -0430->-04:00
-	std::string get_zone_iso8601(const time_t &time);
+	/**
+	 * change zone offset format accordinig to ISO 8601:
+	 * +0200->+02:00
+	 * -0430->-04:00
+	 * because of zone setting, gen_exact_date uses C getenv, setenv, tzset function
+	 * that are not thread safe
+	 *
+	 * available zones could be find on:
+	 *   https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+	 * or on linux in file:
+	 *   /usr/share/zoneinfo/zone.tab
+	 */
+	std::string get_zone_offset(const time_t &time, const std::string &zone = "");
 
 }
 
