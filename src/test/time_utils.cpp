f3c60d9527037bb2f995a259bac73d99b3610eaf
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-04-21 13:27:20 +0200

    update time_utils, add exact tests

--- src/test/time_utils.cpp
+++ src/test/time_utils.cpp
@@ -8,38 +8,23 @@ using namespace time_utils;
 
 // TODO write unit test that meet "Leap second" exception
 
-std::string gen_formated_date(int year, int month, int day) {
-	std::time_t t = std::time(NULL);
-	std::tm tm = *std::localtime(&t);
-
-	// -1900 because year start from 1900
-	tm.tm_year = year - 1900;
-	// -1 because [0-11]
-	tm.tm_mon = month-1;
-	tm.tm_mday = day;
-
-	// Daylight Saving Time flag. The value is positive if DST is in effect
-	tm.tm_isdst=1;
-	//tm.tm_gmtoff = 3600;
-
-	std::time_t test_time = mktime( &tm );
-	return timepoint_to_readable(test_time);
-}
 
 TEST(time_utils, check_readable_time_length) {
 
-	std::string formated = gen_formated_date(1023,6,1);
+	std::string formated = time_t_to_readable(gen_exact_date(1023,6,1,1,1,1));
 	EXPECT_TRUE(formated.length() == 25);
 
-	formated = gen_formated_date(1923,12,30);
+	formated = time_t_to_readable(gen_exact_date(1923,12,30,2,2,2));
 	EXPECT_TRUE(formated.length() == 25);
 
-	formated = gen_formated_date(2923,10,20);
+	formated = time_t_to_readable(gen_exact_date(2923,10,20,3,3,3));
 	EXPECT_TRUE(formated.length() == 25);
+
 }
 
 TEST(time_utils, check_readable_time_regex) {
-
+	//setenv("TZ", "UTC", 1);
+	//tzset();
 	//example match: 2017-01-02T09:10:53+00:00
 	const std::string year_regexp(R"((?:[1-9]\d{3})");
 	const std::string month_regexp(R"((?:(?:0[1-9]|1[0-2]))");
@@ -50,19 +35,19 @@ TEST(time_utils, check_readable_time_regex) {
 	std::smatch result;
 
 
-	std::string formated = gen_formated_date(1123,6,1);
+	std::string formated = time_t_to_readable(gen_exact_date(1023,6,1,1,1,1));
 	_mark("time: " << formated << ", formated_length: " << formated.length());
 	EXPECT_TRUE(std::regex_match(formated, result, re));
 
-	formated = gen_formated_date(1923,12,30);
+	formated = time_t_to_readable(gen_exact_date(1923,12,30,2,2,2));
 	EXPECT_TRUE(std::regex_match(formated, result, re));
 	_mark("time: " << formated << ", formated_length: " << formated.length());
 
-	formated = gen_formated_date(2923,10,20);
+	formated = time_t_to_readable(gen_exact_date(2923,10,20,3,3,3));
 	EXPECT_TRUE(std::regex_match(formated, result, re));
 	_mark("time: " << formated << ", formated_length: " << formated.length());
 
-	formated = gen_formated_date(2017,4,20);
+	formated = time_t_to_readable(gen_exact_date(2017,4,20,5,30,15));
 	EXPECT_TRUE(std::regex_match(formated, result, re));
 	_mark("time: " << formated << ", formated_length: " << formated.length());
 }
@@ -71,4 +56,19 @@ TEST(time_utils, t_timepoint_overload) {
 	t_timepoint now_time = t_timepoint::clock::now();
 	std::string formated = timepoint_to_readable(now_time);
 	EXPECT_TRUE(formated.length() == 25);
+}
+
+TEST(time_utils, exact_data_check) {
+
+	std::string formated = time_t_to_readable(gen_exact_date(2017,10,21,12,1,3), "Europe/Warsaw");
+	_mark("time Europe/Warsaw: " << formated << ", formated_length: " << formated.length());
+	EXPECT_EQ(formated, "2017-10-21T12:01:03+02:00");
+
+	formated = time_t_to_readable(gen_exact_date(2017,10,1,0,0,0), "Asia/Pyongyang");
+	_mark("time Asia/Pyongyang: " << formated << ", formated_length: " << formated.length());
+	EXPECT_EQ(formated, "2017-10-01T00:00:00+08:30");
+
+	formated = time_t_to_readable(gen_exact_date(2022,12,15,15,0,0), "Pacific/Galapagos");
+	_mark("time Pacific/Galapagos: " << formated << ", formated_length: " << formated.length());
+	EXPECT_EQ(formated, "2022-12-15T15:00:00-06:00");
 }
