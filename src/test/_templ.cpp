b726ba30cb959352d1a31ac46f8dde083c84cb59
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-04-24 12:41:21 +0000

    unit tests template fixed

--- src/test/_templ.cpp
+++ src/test/_templ.cpp
@@ -6,7 +6,7 @@
 
 TEST(_templ, _unnamed_) {
 
-	EXPECT_THROW( { vector<int> v(5); _info( v.at(5) ); } , std::out_of_range );
+	EXPECT_THROW( { vector<int> v(5); auto x = v.at(5); _dbg1(x); } , std::out_of_range );
 	EXPECT_EQ(2+2,4);
 	EXPECT_NE(2+2,5);
 
