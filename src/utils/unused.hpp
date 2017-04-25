698ad82342a547ca2aaead2bc851e0c5dfc1f856
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-04-25 15:16:25 +0000

    [style] comments; option to quiet warnings on missing return non-void for false-positives

--- src/utils/unused.hpp
+++ src/utils/unused.hpp
@@ -15,12 +15,20 @@ template<int n> bool volatile_always_true() { volatile int x=n; volatile int y=x
 /// @brief This macro will silence warnings about given variable being unused
 #define UNUSED(expr) do { (void)(expr); } while (0)
 
-/* @brief This macro could be placed at end of function to guarantee that in unexpected case where that code is reached,
+/** @brief This macro could be placed at end of function to guarantee that in unexpected case where that code is reached,
  * the program will abort (instead e.g. UB of returning without return expression from a non-void function).
  * Compiler can not prove that this code will always abort here, therefore this should not stop compiler
  * from issuing warnings about function possibly reaching end without return.
 */
 #define DEAD_RETURN() do { if (volatile_always_true<0>()) { _erro("Reached unexpected end of function (DEAD_RETURN), aborting.");  std::abort(); } } while (0)
+
+/**
+ * @brief much like #DEAD_RETURN it will abort if program reaches this spot (usually at very end of function returning non-void),
+ * but it says that COMPILER is MISTAKEN and fails to understand that this code is unreachable,
+ * so it quiets the compiler warning about this mistake.
+ * @warningUse only if you are sure that this code is unreachable, because it silences warnings.
+ */
+#define DEAD_RETURN_I_AM_SURE() do { { _erro("Reached unexpected end of function (DEAD_RETURN), aborting.");  std::abort(); } } while (0)
 
 #define TODOCODE { std::stringstream oss; oss<<"Trying to use a not implemented/TODO code, in " \
 <<__func__<<" (line "<<__LINE__<<")"; \
