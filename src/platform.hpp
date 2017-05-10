8560eb9533e081ba2445a834c6aeb506817214f4
Robert Oleksy <robertoleksy6@gmail.com>
2017-05-10 12:27:03 +0200

    compilation warnings on cygwin

--- src/platform.hpp
+++ src/platform.hpp
@@ -11,8 +11,6 @@
 	#define ATTR_NODISCARD __attribute__((warn_unused_result))
 #elif defined(_WIN32) || defined(__CYGWIN__)
 	#define ANTINET_windows
-	#define UNICODE
-	#define _UNICODE
 	#define ATTR_NODISCARD _Check_return_
 	#if defined(__CYGWIN__)
 		//http://www.boost.org/doc/libs/1_61_0/doc/html/boost_asio/using.html
