7e95285d56e78faf619527ad31b14938dc3dd1c4
Microsoft Corporation <>
2017-05-24 10:39:18 +0200

    check in user is admin, source https://msdn.microsoft.com/en-us/library/windows/desktop/aa376389(v=vs.85).aspx

--- src/tuntap/windows/c_tuntap_windows.cpp
+++ src/tuntap/windows/c_tuntap_windows.cpp
@@ -26,7 +26,36 @@
 #define TAP_IOCTL_SET_MEDIA_STATUS		TAP_CONTROL_CODE (6, METHOD_BUFFERED)
 
 c_is_user_admin::c_is_user_admin() {
+	/*++
+		Routine Description: This routine returns TRUE if the caller's
+		process is a member of the Administrators local group. Caller is NOT
+		expected to be impersonating anyone and is expected to be able to
+		open its own process and process token.
+		Arguments: None.
+		Return Value:
+			TRUE - Caller has Administrators local group.
+			FALSE - Caller does not have Administrators local group. --
+	*/
+	BOOL b;
+	SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+	PSID AdministratorsGroup;
+	b = AllocateAndInitializeSid(
+		&NtAuthority,
+		2,
+		SECURITY_BUILTIN_DOMAIN_RID,
+		DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&AdministratorsGroup);
+	if (b)
+	{
+		if (!CheckTokenMembership(NULL, AdministratorsGroup, &b))
+		{
+			b = FALSE;
+		}
+		FreeSid(AdministratorsGroup);
+	}
 
+	return(b);
 }
 
 c_tuntap_windows_obj::c_tuntap_windows_obj()
