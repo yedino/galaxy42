89a81351cec2f9220ea4532bb3025c413b88e78e
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2017-05-25 15:37:18 +0200

    update time_utils, add unit_test for all_zones

--- src/utils/time_utils.cpp
+++ src/utils/time_utils.cpp
@@ -2,7 +2,8 @@
 
 #include <iomanip>
 #include <ctime>
-#include "../platform.hpp"
+#include "platform.hpp"
+#include "tnetdbg.hpp"
 
 time_t time_utils::gen_exact_date(int year,
                       int month,
@@ -11,6 +12,15 @@ time_t time_utils::gen_exact_date(int year,
                       int min,
                       int sec,
                       int isdst) {
+
+	std::chrono::system_clock::time_point local_timepoint = std::chrono::system_clock::now();
+	std::time_t local_time = std::chrono::system_clock::to_time_t(local_timepoint);
+	std::istringstream iss(time_utils::get_zone_offset_local(local_time));
+	std::tm *local_tm = std::localtime(&local_time);
+
+	// parsion string in ISO format to hour and minutes variables example -12:30 -> -12 hours, 30 minutes
+	short int l_hours, l_minutes; char ch;
+	iss >> l_hours >> ch >> l_minutes;
 
 	time_t ret;
 	std::tm tm;
@@ -21,13 +31,21 @@ time_t time_utils::gen_exact_date(int year,
 	tm.tm_mon = month -1;
 	tm.tm_mday = day;
 
-	tm.tm_hour = hour;
-	tm.tm_min = min;
+	tm.tm_hour = hour + l_hours;
+	tm.tm_min = min + l_minutes;
 	tm.tm_sec = sec;
 
-	// Daylight Saving Time flag. The value is positive if DST is in effect
-	tm.tm_isdst=isdst;
+	// isdst less than zero means that the information is not available.
+	// setting local dst
+	if (isdst == -1) {
+		tm.tm_isdst=local_tm->tm_isdst;
+	} else {
+		// Daylight Saving Time flag. The value is positive if DST is in effect
+		tm.tm_isdst=isdst;
+	}
 	ret = std::mktime(&tm);
+
+
 	return ret;
 }
 
@@ -37,6 +55,12 @@ std::string time_utils::timepoint_to_readable(const time_utils::t_timepoint &tp,
 }
 
 std::string time_utils::time_t_to_readable(const std::time_t &time, const std::string &zone) {
+
+	// Pacific/Marquesas and America/St_Johns exception
+	// I have no idea why this two zones do not works properly.
+	if(zone == "Pacific/Marquesas" || zone == "America/St_Johns") {
+		_warn("Time for " << zone << " zone could not works properly!");
+	}
 
 	std::string full_date = get_date_str(time);
 
