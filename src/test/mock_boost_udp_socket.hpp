bf9997544efb9f6d84940d9c503b0cd3ccc82eec
Robert Oleksy <robertoleksy6@gmail.com>
2017-05-30 12:33:41 +0000

    create udp socket mock class

---
+++ src/test/mock_boost_udp_socket.hpp
@@ -0,0 +1,37 @@
+#ifndef MOCK_BOOST_UDP_SOCKET_HPP
+#define MOCK_BOOST_UDP_SOCKET_HPP
+
+#include "../../depends/googletest/googlemock/include/gmock/gmock.h"
+#include <boost/asio.hpp>
+#include <libs0.hpp>
+
+namespace mock {
+
+class mock_boost_udp_socket final {
+	public:
+		mock_boost_udp_socket(boost::asio::io_service &io_service,
+		                      const boost::asio::ip::udp::endpoint &endpoint);
+		MOCK_METHOD0(is_open, bool());
+		MOCK_METHOD1(set_option, void(const int &));
+		MOCK_METHOD2(send_to, std::size_t(const boost::asio::const_buffer &,
+		             const boost::asio::ip::udp::endpoint &endpoint));
+		MOCK_METHOD3(async_send_to, void(
+		                                 const boost::asio::const_buffer &,
+		                                 const boost::asio::ip::udp::endpoint &endpoint,
+		                                 std::function<void(const boost::system::error_code&, size_t)>));
+		MOCK_METHOD2(receive_from, std::size_t(
+		                                       const boost::asio::mutable_buffer &,
+		                                       boost::asio::ip::udp::endpoint &));
+		MOCK_METHOD0(native_handle, int());
+		MOCK_METHOD3(async_receive_from, void(
+		                                      const boost::asio::mutable_buffer &,
+		                                      boost::asio::ip::udp::endpoint &,
+		                                      std::function<void(const boost::system::error_code&, size_t)>));
+		MOCK_METHOD1(bind, void(const boost::asio::ip::udp::endpoint &));
+		MOCK_METHOD1(shutdown, void(boost::asio::ip::udp::socket::shutdown_type));
+		MOCK_METHOD0(close, void());
+};
+
+} // namespace
+
+#endif // MOCK_BOOST_UDP_SOCKET_HPP
