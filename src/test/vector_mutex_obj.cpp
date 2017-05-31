f6023baf4035050799da0e8e6604cdc9e802edf1
Rysiek <mikurys@gmail.com>
2017-05-31 15:40:35 +0200

    unit test for vector_mutex_obj

---
+++ src/test/vector_mutex_obj.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+#include <iostream>
+#include <exception>
+
+#include <libs0.hpp> // for debug etc
+#include <galaxysrv_engine.hpp>
+
+TEST(vector_mutex_obj_test, grow_to_test) {
+/*
+	vector_mutexed_obj<unsigned char> vec();
+
+	std::thread t1([&](){for (size_t i : {1, 3, 8, 2, 7, 2}) vec.grow_to(i); });
+	std::thread t2([&](){for (size_t i : {9, 10, 2, 1, 3, 1}) vec.grow_to(i); });
+	std::thread t3([&](){for (size_t i : {6, 5, 4, 3, 2, 1}) vec.grow_to(i); });
+	std::thread t4([&](){for (size_t i : {0, 1, 2, 3, 4, 5}) vec.grow_to(i); });
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+	*/
+//	EXPECT_EQ(vec.size(), 10);
+
+}
+
+
