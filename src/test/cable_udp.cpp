c771c5e0295a482914960a1ebbf97a6d9553348f
Robert Oleksy <robertoleksy6@gmail.com>
2017-06-01 11:12:33 +0000

    create mock for c_asioservice_manager

--- src/test/cable_udp.cpp
+++ src/test/cable_udp.cpp
@@ -1,7 +1,11 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../cable/udp/cable_udp_obj.hpp"
 
 using testing::Return;
 using testing::_;
 using testing::Invoke;
 
+TEST(cable_udp, constructor) {
+
+}
