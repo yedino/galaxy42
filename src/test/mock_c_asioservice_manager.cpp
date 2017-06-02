bb4b2fe1e65f4fd4a7ca1a1ecad19777b5508d38
Robert Oleksy <robertoleksy6@gmail.com>
2017-06-02 11:32:04 +0000

    segfault in test

--- src/test/mock_c_asioservice_manager.cpp
+++ src/test/mock_c_asioservice_manager.cpp
@@ -1,5 +1,5 @@
 #include "mock_c_asioservice_manager.hpp"
 
-mock_c_asioservice_manager::mock_c_asioservice_manager(size_t size_) {
+mock::mock_c_asioservice_manager::mock_c_asioservice_manager(size_t size_) {
 	_UNUSED(size_);
 }
