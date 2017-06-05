15c4652775c0f52d2428efaed6f9ebfe8df7c7d3
Robert Oleksy <robertoleksy6@gmail.com>
2017-06-05 09:09:15 +0000

    [unit test] cable_udp constructor

--- src/test/cable_udp.cpp
+++ src/test/cable_udp.cpp
@@ -9,8 +9,10 @@ using testing::_;
 using testing::Invoke;
 
 TEST(cable_udp, constructor) {
-	//std::shared_ptr<c_asioservice_manager_base> empty_ptr = std::make_shared<mock::mock_c_asioservice_manager>(1);
 	std::shared_ptr<c_asioservice_manager_base> empty_ptr;
 	mock::mock_c_card_selector card_selector;
 	EXPECT_THROW(c_cable_udp(empty_ptr, card_selector), std::invalid_argument);
+
+	std::shared_ptr<c_asioservice_manager_base> asioservice_manage_ptr = std::make_shared<mock::mock_c_asioservice_manager>(1);
+	EXPECT_NO_THROW(c_cable_udp(asioservice_manage_ptr, card_selector));
 }
