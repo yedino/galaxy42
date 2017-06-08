732092cb904f4f9480b2a2e2c9e07165a752373b
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-06-08 14:38:21 +0000

    Started fixes on cap lib

--- src/test/privileges.cpp
+++ src/test/privileges.cpp
@@ -3,6 +3,7 @@
 #include "my_tests_lib.hpp"
 
 #include "../utils/privileges.hpp"
+#include "../utils/capmodpp.hpp"
 
 #include <exception>
 #include <cmath>
@@ -11,7 +12,11 @@
 
 TEST( privileges , create_print_capmodpp ) {
 
-	//my_cap::
+	capmodpp::cap_state_map state_map;
+
+	_goal( state_map );
+
+	EXPECT_EQ(2,2);
 
 }
 
--- src/utils/capmodpp.hpp
+++ src/utils/capmodpp.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+#include <map>
+#include <sstream>
+
 namespace capmodpp {
 
 /**
