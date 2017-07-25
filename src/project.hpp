564454706c39d27e9a9e12a012135eab11d62d58
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-07-25 16:18:02 +0000

    Version 0.3.2b (rc)

--- src/project.hpp
+++ src/project.hpp
@@ -73,6 +73,6 @@ std::string project_version_info();
 constexpr int project_version_number_major{0};
 constexpr int project_version_number_minor{3};
 constexpr int project_version_number_sub{2};
-constexpr char project_version_number_patch{'a'};
+constexpr char project_version_number_patch{'b'};
 constexpr int project_version_number_progress{1};
 
