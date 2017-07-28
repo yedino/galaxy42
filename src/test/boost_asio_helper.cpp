aad778b6a0b567dcaceede2c42f0fb4794064439
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-07-28 13:18:39 +0000

    boost asio ipv6 address: reviewed, noop conversion removed from UT (commit without mo)

--- src/test/boost_asio_helper.cpp
+++ src/test/boost_asio_helper.cpp
@@ -10,8 +10,6 @@ TEST(boost_asio_helper, make_ipv6_address_from_bytes) {
 	auto addr2bytes = std::array<unsigned char,16>{ { 0xfd,0x42, 0x92,0x88, 0xdb,0x10, 0xf1,0xa8,
 	0x29,0xee, 0x28,0xbf, 0x17,0xbf, 0xa6,0x30 } };
 
-	addr1.from_string("fd42:9288:db10:f1a8:29ee:28bf:17bf:a630");
-
 	boost::asio::ip::address_v6 addr2{ make_ipv6_address( addr2bytes ) };
 
 	EXPECT_EQ(addr1, addr2);
--- src/utils/misc.hpp
+++ src/utils/misc.hpp
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include "libs0.hpp"
