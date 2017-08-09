425c1fa3b798c28912a3ea81b4591874d31cd8d3
Rysiek <mikurys@gmail.com>
2017-08-09 15:22:33 +0200

    Tool to test galaxy transfer speed

--- qa/test_tools/transfer_test.py
+++ qa/test_tools/transfer_test.py
@@ -3,7 +3,7 @@ import sys, subprocess, os, time
 from subprocess import PIPE, Popen
 from threading  import Thread
 
-from queue import Queue, Empty  # python 3.x
+from queue import Queue, Empty
 
 program='../../build/tunserver.elf'
 program_options={}
@@ -12,69 +12,57 @@ program_options['--rpc-port']='45000'
 
 ON_POSIX = 'posix' in sys.builtin_module_names
 
+galaxy_env = os.environ.copy()
+galaxy_env["LANG"] = "en_US.UTF-8"
+
 def enqueue_output(out, queue):
     for line in iter(out.readline, b''):
         queue.put(line)
+        #if 'ERROR' in str(line):
+        #    print(line.decode('utf-8', end=''))
     out.close()
 
 def run_galaxy():
-    if '-s' in sys.argv:
-        args = list(sum(program_options.items(), ()))
-        args.insert(0, program)
-        galaxy = Popen(args , stderr=PIPE, stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
-        q = Queue()
-        t = Thread(target=enqueue_output, args=(galaxy.stdout, q))
-        t.daemon = True # thread dies with the program
-        t.start()
-
-        # read line without blocking
-        while True:
-            #try:  line = q.get_nowait()
-            try: line = q.get(timeout=0.1)
-            except Empty:
-                pass
-                #print('no output yet')
-            else: # got line
-                if 'Your hash-IPv6 address is :' in str(line):
-                    galaxy_ip = str(line).split(' : ')[1][:-3]
-                    print('Should run on the other computer this program, with following options: ')
-                    print(os.path.basename(__file__), '-c', 'YOURIP-'+galaxy_ip, '[ options ]')
-                    break
-    elif '-c' in sys.argv:
-        peer = sys.argv[sys.argv.index('-c')+1]
-        print('-'.join([':', program_options['--port']]))
-        peer.replace('-', ':'.join([program_options['--port'], '-']))
-        program_options['--peer'] = peer
-        print(program_options['--peer'])
-        args = list(sum(program_options.items(), ()))
-        args.insert(0, program)
-        galaxy = Popen(args , stderr=PIPE, stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
-        q = Queue()
-        t = Thread(target=enqueue_output, args=(galaxy.stdout, q))
-        t.daemon = True # thread dies with the program
-        t.start()
-
-        # read line without blocking
-        while True:
-            #try:  line = q.get_nowait()
-            try: line = q.get(timeout=0.1)
-            except Empty:
-                pass
-                #print('no output yet')
-            else: # got line
-                print(line)
-                if 'OK: Started virtual network card interface (TUN) with proper IPv6 and other settings' in str(line):
-                    break
-    else:
+    if '-c' not in sys.argv and '-s' not in sys.argv:
         print("Use with option -s (to run as server) or -c [YOURIP-GALAXYIP] (to run as client)")
+        exit()
+    if '-c' in sys.argv:
+        peer = sys.argv[sys.argv.index('-c')+1].split('-')
+        galaxy_peer = '{}:{}-{}'.format(peer[0], program_options['--port'], peer[1])
+        program_options['--peer'] = galaxy_peer
+
+    args = list(sum(program_options.items(), ()))
+    args.insert(0, program)
+    galaxy = Popen(args, env=galaxy_env, stderr=PIPE, stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
+    print('Running galaxy with command: ', ''.join([arg+' ' for arg in args ] ))
+    q = Queue()
+    t = Thread(target=enqueue_output, args=(galaxy.stdout, q))
+    t.daemon = True # thread dies with the program
+    t.start()
+
+    # read line without blocking
+    while True:
+        #try:  line = q.get_nowait()
+        try: line = q.get(timeout=0.1)
+        except Empty:
+            pass
+            #print('no output yet')
+        else: # got line
+            if 'Your hash-IPv6 address is :' in str(line) and '-s' in sys.argv:
+                galaxy_ip = str(line).split(' : ')[1][:-3]
+                print('You should run on the other computer this program, with following options: ')
+                print('./'+os.path.basename(__file__), '-c', 'YOURIP-'+galaxy_ip, '[ iperf options (see man iperf)]')
+                break
+            elif 'OK: Started virtual network card interface (TUN) with proper IPv6 and other settings' in str(line):
+                break
 
 def run_iperf():
     sys.argv[0] = 'iperf'
     sys.argv.insert(1, '-V')
     if '-c' in sys.argv:
         index = sys.argv.index('-c')+1
-        sys.argv[index] = sys.argv[:sys.argv[index].index('-')]
-        print(sys.argv[index])
+        sys.argv[index] = sys.argv[index][sys.argv[index].index('-')+1:]
+    print('Running iperf with command: ', ''.join([arg+' ' for arg in sys.argv ] ))
     subprocess.call(sys.argv)
 
 if __name__ == "__main__":
