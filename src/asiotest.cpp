57b7477debdce839ece1b1aa7537e8a2293c612e
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-08-18 11:18:13 +0000

    example UDP server sync

--- src/asiotest.cpp
+++ src/asiotest.cpp
@@ -12,23 +12,40 @@ void asiotest()
   int port_num = 3456;
 
 	asio::io_service ios;
+	asio::ip::tcp protocol_tcp = asio::ip::tcp::v4();
+  asio::ip::udp protocol_udp = asio::ip::udp::v4();
 
-  asio::ip::address ip_address = asio::ip::address_v4::any();
-  asio::ip::tcp::endpoint ep(ip_address, port_num);
+	if (1) { // UDP server/client - anyway active socket
+		char inbuf_data[64];
+		auto inbuf = asio::buffer( inbuf_data , std::extent<decltype(inbuf_data)>::value );
+		asio::ip::udp::socket mysocket(ios); // active udp
+		_note("bind");
+		mysocket.open( protocol_udp );
+		mysocket.bind( asio::ip::udp::endpoint( asio::ip::address_v4::any() , 9000 ) );
+		asio::ip::udp::endpoint remote_ep;
+		_note("receive");
+		size_t read_size = mysocket.receive_from( inbuf , remote_ep ); // ***
+		_note("got data from remote " << remote_ep);
+		_note("read: ["<<std::string(&inbuf_data[0],read_size)<<"]");
+	}
 
-	asio::ip::tcp protocol = asio::ip::tcp::v4();
-	asio::ip::tcp::socket mysocket(ios);
+	if (0) { // TCP server - part
+		asio::ip::address ip_address = asio::ip::address_v4::any();
+		asio::ip::tcp::endpoint ep(ip_address, port_num);
 
-	asio::ip::tcp::acceptor myacceptor(ios);
+		asio::ip::tcp::socket mysocket(ios);
 
-	mysocket.open(protocol);
-	myacceptor.open(protocol);
+		asio::ip::tcp::acceptor myacceptor(ios);
 
-	if (1) { // TCP client
+		mysocket.open(protocol_tcp);
+		myacceptor.open(protocol_tcp);
+	}
+
+	if (0) { // TCP client
 		asio::ip::tcp::endpoint ep_dst_tcp( asio::ip::address::from_string("127.0.0.1") , 80 );
 		asio::ip::tcp::socket tcpsend( ios );
 		_note("open");
-		tcpsend.open(protocol);
+		tcpsend.open(protocol_tcp);
 		_note("connect");
 		tcpsend.connect( ep_dst_tcp ); // waits for ACK here
 		asio::write( tcpsend , asio::buffer( std::string("GET /index.html HTTP/1.1\r\n\r\n") ) );
