387dfd4fecfe993f7b920e729582bffebb5c0a1a
Robert Oleksy <robertoleksy6@gmail.com>
2017-12-08 15:42:01 +0000

    make bitcoin_node_cli::get_balance() thread safe

--- src/bitcoin_node_cli.hpp
+++ src/bitcoin_node_cli.hpp
@@ -6,11 +6,14 @@
 class bitcoin_node_cli final {
 	public:
 		bitcoin_node_cli(const std::string &ip_address="127.0.0.1", unsigned short port=18332);
+		/**
+		 * @brief get_balance this funstion is thread safe
+		 * @return amount of BTC (in satoshi)
+		 */
 		uint32_t get_balance();
 	private:
 		boost::asio::io_service m_io_service;
 		const boost::asio::ip::tcp::endpoint m_btc_rpc_endpoint;
-		boost::asio::ip::tcp::socket m_socket;
 };
 
 #endif // BITCOIN_NODE_CLI_HPP
