1c0f377d9688b255358c4901f66da5af8ef9afc3
Robert Oleksy <robertoleksy6@gmail.com>
2017-12-11 14:48:39 +0000

    bitcoin_node_cli::get_new_address()

--- src/bitcoin_node_cli.cpp
+++ src/bitcoin_node_cli.cpp
@@ -11,6 +11,9 @@ bitcoin_node_cli::bitcoin_node_cli(const std::string &ip_address, unsigned short
 uint32_t bitcoin_node_cli::get_balance() const {
 	const std::string get_balance_request = [this] {
 		std::string get_balance_request = generate_request_prototype();
+		//get_balance_request += "Authorization: Basic X19jb29raWVfXzowMmNmYzBlYjE2YWU1OTQzMDIxMWVlZTc0NTc0NGVhNTVkZTk4MjM2ZDkzNTU2YzE5MGMxNzEyNjgyNTQwNmNh\r\n";
+		get_balance_request += "Authorization: Basic X19jb29raWVfXzoyNzYxMTIwMDU1ZWM0ZDBhYTQ0NTQwOGQ2OGJkYzcwOGZhZTk3ODYzN2E4MTU0MDk0NTViY2JjNTdmZTY3ZWUw\r\n";
+		get_balance_request += "Content-Length: 48\r\n\r\n";
 		get_balance_request += R"({"method":"getbalance","params":["*",0],"id":1})";
 		get_balance_request += "\n";
 		return get_balance_request;
@@ -19,8 +22,7 @@ uint32_t bitcoin_node_cli::get_balance() const {
 	std::string receive_data = send_request_and_get_response(get_balance_request);
 	_mark("Receive data " << receive_data);
 
-	auto it = std::find(receive_data.begin(), receive_data.end(), '{'); // find begin of json data
-	receive_data.erase(receive_data.begin(), it); // remove http POST data
+	remove_POST_data(receive_data);
 	std::setlocale(LC_ALL, "en_US.UTF-8");
 	nlohmann::json json = nlohmann::json::parse(receive_data.c_str());
 
@@ -28,13 +30,29 @@ uint32_t bitcoin_node_cli::get_balance() const {
 	return btc_amount * 100'000'000. ; // return balance in satoshi
 }
 
+std::string bitcoin_node_cli::get_new_address() const {
+	const std::string request = [this] {
+		std::string request = generate_request_prototype();
+		request += "Authorization: Basic X19jb29raWVfXzoyNzYxMTIwMDU1ZWM0ZDBhYTQ0NTQwOGQ2OGJkYzcwOGZhZTk3ODYzN2E4MTU0MDk0NTViY2JjNTdmZTY3ZWUw\r\n";
+		request += "Content-Length: 46\r\n\r\n";
+		request += R"({"method":"getnewaddress","params":[],"id":1})";
+		request += "\n";
+		return request;
+	}();
+
+	std::string receive_data = send_request_and_get_response(request);
+	_mark("Receive data " << receive_data);
+	remove_POST_data(receive_data);
+
+	nlohmann::json json = nlohmann::json::parse(receive_data);
+	return json.at("result").get<std::string>();
+}
+
 std::string bitcoin_node_cli::generate_request_prototype() const {
 	std::string request;
 	request += "POST / HTTP/1.1\r\n";
 	request += "Host: ::1\r\n";
 	request += "Connection: close\r\n";
-	request += "Authorization: Basic X19jb29raWVfXzowMmNmYzBlYjE2YWU1OTQzMDIxMWVlZTc0NTc0NGVhNTVkZTk4MjM2ZDkzNTU2YzE5MGMxNzEyNjgyNTQwNmNh\r\n";
-	request += "Content-Length: 48\r\n\r\n";
 	return request;
 }
 
@@ -46,4 +64,9 @@ std::string bitcoin_node_cli::send_request_and_get_response(const std::string &r
 	size_t readed_bytes = socket.receive(boost::asio::buffer(&receive_buffer[0], receive_buffer.size()));
 	receive_buffer.resize(readed_bytes);
 	return receive_buffer;
+}
+
+void bitcoin_node_cli::remove_POST_data(std::string &data_with_POST) const {
+	auto it = std::find(data_with_POST.begin(), data_with_POST.end(), '{'); // find begin of json data
+	data_with_POST.erase(data_with_POST.begin(), it); // remove http POST data
 }
--- src/bitcoin_node_cli.hpp
+++ src/bitcoin_node_cli.hpp
@@ -11,11 +11,18 @@ class bitcoin_node_cli final {
 		 * @return amount of BTC (in satoshi)
 		 */
 		uint32_t get_balance() const;
+
+		/**
+		 * @brief get_new_address
+		 * @return new bitcoin address
+		 */
+		std::string get_new_address() const;
 	private:
 		mutable boost::asio::io_service m_io_service;
 		const boost::asio::ip::tcp::endpoint m_btc_rpc_endpoint;
 		std::string generate_request_prototype() const;
 		std::string send_request_and_get_response(const std::string &request) const;
+		void remove_POST_data(std::string &data_with_POST) const;
 };
 
 #endif // BITCOIN_NODE_CLI_HPP
