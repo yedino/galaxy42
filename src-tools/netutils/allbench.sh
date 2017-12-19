e086eb29c36cd39adfd40e377d3ba4dd993e2018
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-12-19 15:05:02 +0000

    allbench: cd into dir also as: bash foo.sh

--- src-tools/netutils/allbench.sh
+++ src-tools/netutils/allbench.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # Use a root dir for this script. http://mywiki.wooledge.org/BashFAQ/028
-dir_base_of_source="../../" # <--- this is how THIS ONE our script is bundled inside our repo
-echo "bash file: [$BASH_SOURCE]" ; cd_to="${BASH_SOURCE%/*}" ; cd "$cd_to" || { echo "Can not cd into my script dir [$cd_to]" ; exit 1 ; }
-echo "Script works in PWD [$PWD]"
-dir_base_of_source="${PWD}/${dir_base_of_source}/"
-[ -r "${dir_base_of_source}toplevel" ] || { echo "Can't find 'toplevel' using dir_base_of_source=[$dir_base_of_source]"; exit 1; }
-source "${dir_base_of_source}share/script/lib/fail.sh" || { echo "Can not load lib (in $0)"; exit 1 ; }
-echo "OK"
+relative_top="../../" # <--- this is how THIS ONE our script is bundled inside our repo
+echo "bash file: [$BASH_SOURCE]" ;
+
+my_src="${BASH_SOURCE}"
+echo "my_src [$my_src]"
+[[ "$my_src" == "" ]] && { echo "Unknown script file name. This script must be run from a file (because it is part of bundle)" ; exit 1; }
+echo "my_src [$my_src]"
+[[ $my_src && $my_src != */* ]] && my_src=./$my_src
+echo "my_src [$my_src]"
+cd_to="${my_src%/*}"
+echo "cd into [$cd_to]"
+cd "$cd_to" || { echo "Can not cd into my script dir [$cd_to]" ; exit 1 ; }
+bundle_dir="${PWD}/${relative_top}"
+[ -r "${bundle_dir}toplevel" ] || { echo "Can't find 'toplevel' using bundle_dir=[$bundle_dir]"; exit 1; }
+source "${bundle_dir}share/script/lib/fail.sh" || { echo "Can not load lib (in $0)"; exit 1 ; }
+echo "OK, working in bundle_dir [$bundle_dir]"
 
 exit
 
