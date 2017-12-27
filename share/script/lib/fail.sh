2499bc822ae14d4d4f5364e000f0f0028b68d91a
Rafal Crypto Tigusoft <rafal2@tigusoft.pl>
2017-12-27 12:25:26 +0000

    [wip] bench all - nicer

--- share/script/lib/fail.sh
+++ share/script/lib/fail.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 function fail() {
-       echo -e "\nERROR (in $0): " "$@"
-       exit 1
+	echo "$(tput setaf 1)"
+	echo -e "\nERROR (in $0): " "$@"
+	exit 1
+	echo "$(tput sgr0)"
 }
