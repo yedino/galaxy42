8fcc17718ffb8fb86dbc9c803f9718d1e64f3c7c
neodr <neodr@onet.pl>
2018-02-08 13:34:45 +0100

    bench: Option to choose format

--- src-tools/netmodel/netmodel.cpp
+++ src-tools/netmodel/netmodel.cpp
@@ -1053,6 +1053,8 @@ void asiotest_udpserv(std::vector<std::string> options) {
 	for (const string & arg : options) mycmdline.add(arg);
 	auto func_cmdline = [&mycmdline](const string &name) -> int { return get_from_cmdline(name,mycmdline); } ;
 	auto func_cmdline_def = [&mycmdline](const string &name, int def) -> int { return get_from_cmdline(name,mycmdline,def); } ;
+    auto func_cmdline_str = [&mycmdline](const string &name, string def) -> string { return get_from_cmdline(name,mycmdline,def); } ;
+
 
 	const int cfg_num_inbuf = func_cmdline("wire_buf"); // e.g. 32 ; this is also the number of flows (wire/p2p connections)
 	const int cfg_num_socket_wire = func_cmdline("wire_sock"); // 2 ; number of sockets - wire (p2p)
@@ -1100,6 +1102,11 @@ void asiotest_udpserv(std::vector<std::string> options) {
 	const int cfg_tuntap_ios_threads_per_one = func_cmdline("tuntap_ios_thr"); // for each ios of tuntap (if any ios are created for tuntap) how many threads to .run it in
 	const bool cfg_tuntap_use_real_tun = func_cmdline("tuntap_use_real"); // if true real tuntap is used
 	const bool cfg_tuntap_async = func_cmdline("tuntap_async");
+
+    const string cfg_stats_format = func_cmdline_str("stats_format", std::string(""));
+    _mark("stats_format configuration: \"" << cfg_stats_format);
+
+
 
 	cfg_tuntap_buf_sleep = func_cmdline("tuntap_weld_sleep");
 
@@ -1273,7 +1280,7 @@ void asiotest_udpserv(std::vector<std::string> options) {
 	// stop / show stats
 	_goal("The stop (and stats) thread"); // exit flag --> ios.stop()
 	std::thread thread_stop(
-		[&ios_general,&ios_wire,&ios_tuntap, &ios_general_work, &ios_wire_work, &ios_tuntap_work, &welds, &welds_mutex, &cfg_run_timeout] {
+        [&ios_general,&ios_wire,&ios_tuntap, &ios_general_work, &ios_wire_work, &ios_tuntap_work, &welds, &welds_mutex, &cfg_run_timeout,&cfg_stats_format] {
 
 			std::vector<double> speed_tab;
 
@@ -1297,9 +1304,18 @@ void asiotest_udpserv(std::vector<std::string> options) {
 				// [counter] read
 				std::ostringstream oss;
 				oss << "Loop. ";
-                                oss << "Wire: RECV={" << g_speed_wire_recv << "}";
-//				oss << "Wire: RECV={" << g_state_tuntap2wire_in_handler1.get_speed() << "Mbps} ";
-//				oss << "Wire: RECV={" << g_state_tuntap2wire_in_handler2.get_speed() << "Mbps} ";
+
+               // std::size_t find_w = cfg_stats_format.find('w');
+               // std::size_t find_t = cfg_stats_format.find('t');
+
+                if(cfg_stats_format.find('w')!=string::npos){
+                    oss << "Wire: RECV={" << g_speed_wire_recv << "} ";
+                }
+                if(cfg_stats_format.find('t')!=string::npos){
+                    oss << "Tuntap: RECV={" << g_state_tuntap2wire_in_handler1.get_speed() << "Mbps} ";
+                    oss << "Tuntap: RECV={" << g_state_tuntap2wire_in_handler2.get_speed() << "Mbps} ";
+                }
+
 				oss << "; ";
 				oss << "Tuntap: ";
 				oss << "start="<<g_state_tuntap2wire_started.load(std::memory_order_relaxed)<<' ';
